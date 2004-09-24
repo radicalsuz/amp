b63e332b10ba3450f36326ed57a73d6706681046
lattice <lattice@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2004-09-24 05:25:18 +0000

    Fixed handling of saves made by administrators.
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@18 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/Modules/UDM/AMP/save.inc.php
+++ include/Modules/UDM/AMP/save.inc.php
@@ -2,77 +2,94 @@
 
 function udm_amp_save ( &$udm, $options = null ) {
 
-	foreach ( $udm->fields as $fname => $fdef ) {
+    if ( isset( $options['admin'] ) && $options['admin'] ) {
 
-		if ( isset( $fdef['public'] ) && $fdef['public'] ) {
-			$publicFields[] = $fname;
-		}
+        $submitValues = $udm->form->exportValues();
+
+        foreach ( $udm->fields as $fname => $fdef ) {
+            if ( isset( $submitValues[ $fname ] ) )
+                $frmFieldValues[ $fname ] = $submitValues[ $fname ];
+        }
+
+    } else {
+        foreach ( $udm->fields as $fname => $fdef ) {
+            if ( isset( $fdef['public'] ) && $fdef['public'] ) {
+                $publicFields[] = $fname;
+            }
+        }
+
+        $frmFieldValues = $udm->form->exportValues( $publicFields );
+    }
+
+    $dbcon = $udm->dbcon;
+    // Insert or Update?
+    if (isset( $udm->uid )) {
+
+        $sql = "UPDATE userdata SET ";
+
+        foreach ( $frmFieldValues as $field => $value ) {
+
+            $elements[] = $field . "=" . $dbcon->qstr( $value );
+
+        }
+
+        $sql .= join( ", ", $elements );
+        $sql .= " WHERE id=" . $dbcon->qstr( $udm->uid );
+
+    } else {
+
+        $sql = "INSERT INTO userdata (";
+
+        $fields = array_keys( $frmFieldValues );
+        $values_noescape = array_values( $frmFieldValues );
+
+	foreach ( $values_noescape as $value ) {
+		$values[] = $dbcon->qstr( $value );
 	}
 
-	$frmFieldValues = $udm->form->exportValues( $publicFields );
+        $fields[] = "modin";
+        $values[] = $udm->instance;
 
-	// Insert or Update?
-	if (isset( $udm->uid )) {
+        $sql .= join( ", ", $fields );
+        $sql .= ") VALUES (";
+        $sql .= join( ", ", $values );
+        $sql .= ")";
 
-		$sql = "UPDATE userdata SET ";
+    }
 
-		foreach ( $frmFieldValues as $field => $value ) {
+    $rs = $dbcon->Execute( $sql ) or
+        die( "There was an error completing the request: " . $dbcon->ErrorMsg() );
 
-			$elements[] = $field . "='" . $value ."'";
+    if ( $rs ) {
 
-		}
+        $udm->showForm = false;
 
-		$sql .= join( ", ", $elements );
-		$sql .= " WHERE id='" . $udm->uid . "'";
+        if ( !isset( $udm->uid ) ) {
 
-	} else {
+            $udm->uid = $dbcon->Insert_ID();
 
-		$sql = "INSERT INTO userdata (";
+        }
 
-		$fields = array_keys( $frmFieldValues );
-		$values = array_values( $frmFieldValues );
+        // Run some default plugins. Plugins will not be run unless
+        // they are pre-registered.
+        //
+        // These plugins should provide output to the user to reflect
+        // their actions.
 
-		$sql .= join( ", ", $fields );
-		$sql .= ") VALUES ('";
-		$sql .= join( "', '", $values );
-		$sql .= "')";
+//      $udm->doAction( 'add_subscriber' );
 
-	}
+//      $udm->doPlugin( 'AMP', 'auto_approve' );
 
-	$dbcon = $udm->dbcon;
-	$rs = $dbcon->Execute( $sql ) or
-		die( "There was an error completing the request: " . $dbcon->ErrorMsg() );
+//      $udm->doPlugin( 'AMP', 'email_admin' );
+//      $udm->doPlugin( 'AMP', 'email_user' );
 
-	if ( $rs ) {
+    } else {
 
-		$udm->showForm = false;
+        $udm->errorMessage( "There was an error processing the request." );
 
-		if ( !isset( $udm->uid ) ) {
+    }
 
-			$udm->uid = $dbcon->Insert_ID();
-
-		}
-
-		// Run some default plugins. Plugins will not be run unless
-		// they are pre-registered.
-		//
-		// These plugins should provide output to the user to reflect
-		// their actions.
-
-//		$udm->doAction( 'add_subscriber' );
-
-//		$udm->doPlugin( 'AMP', 'auto_approve' );
-
-//		$udm->doPlugin( 'AMP', 'email_admin' );
-//		$udm->doPlugin( 'AMP', 'email_user' );
-
-	} else {
-
-		$udm->errorMessage( "There was an error processing the request." );
-
-	}
-
-	return true;
+    return true;
 
 }
 
