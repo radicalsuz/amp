a2051c88cc872e127e589678d51571a6501107d5
lattice <lattice@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2004-09-24 05:26:13 +0000

    Added output of udm stacked results.
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@19 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/Modules/UDM/Output/html.inc.php
+++ include/Modules/UDM/Output/html.inc.php
@@ -2,15 +2,26 @@
 
 function udm_output_html ( $udm, $options = null ) {
 
+    $out = '';
+
+    $results = $udm->getResults(); 
+
+    foreach ( $results as $result ) {
+        $out .= '<p class="' . $result['type'] . '">';
+        $out .= $result[ 'result' ] . '</p>';
+    }
+
     if ( !isset( $udm->showForm ) || $udm->showForm && !isset( $options['showForm'] ) ) {
 
         // Ensure we have a form built before proceeding.
         if ( !isset( $udm->form ) )
             $udm->doPlugin( 'QuickForm', 'build' );
 
-        return $udm->form->toHtml();
+        $out .= $udm->form->toHtml();
 
     }
+
+    return $out;
 
 }
 
