e62062fdc6dd4c979bbe2893b9758ad7ca7b1e34
lattice <lattice@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2004-09-28 05:43:43 +0000

    Removed uneccessary commented code
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@33 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/Modules/UDM/DIA/save.inc.php
+++ include/Modules/UDM/DIA/save.inc.php
@@ -4,10 +4,6 @@ require_once( 'Modules/diaRequest.inc.php' );
 
 function udm_DIA_save ( &$udm, $options = null ) {
 
-/*	foreach ( $udm->fields as $fname => $fdef ) {
-		$saveFields[] = $fname;
-	} */
-
 	$frmFieldValues = $udm->form->exportValues();
 
 	$diaRequest = new diaRequest( $options[ 'orgCode' ] );
