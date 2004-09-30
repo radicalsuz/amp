a9a40c50cd7dd1b971ff516c639740c75510bc31
lattice <lattice@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2004-09-30 00:13:31 +0000

    testing
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@58 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/Modules/UDM/AMP/save.inc.php
+++ include/Modules/UDM/AMP/save.inc.php
@@ -56,6 +56,7 @@ function udm_amp_save ( &$udm, $options = null ) {
         $sql .= ")";
 
     }
+    print $sql;
 
     $rs = $dbcon->Execute( $sql ) or
         die( "There was an error completing the request: " . $dbcon->ErrorMsg() );
