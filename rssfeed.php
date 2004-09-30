19a7a590e054a3f5f8fa8c17777ff52d5b988432
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2004-09-30 00:29:16 +0000

    rss feed generator
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@61 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ rssfeed.php
@@ -0,0 +1,81 @@
+<?php 
+header('Content-type: text/xml');
+print '<?xml version="1.0" encoding="ISO-8859-1"?>
+';
+#field list: title, description, limit, orderby, orderbyorder, where
+$sqllimit= 15;
+$orderby = "date";
+$orderbyorder ="desc";
+require_once( 'includes/base_db.php' );
+
+if ($_GET[feed]) {
+	$f =$dbcon->CacheExecute("select * from rssfeed where id = ".$_GET[feed]." ") or DIE($dbcon->ErrorMsg());
+	if ($f->Fields("description")) { $meta_description = $f->Fields("description");}
+	if ($f->Fields("title")) {$title = $f->Fields("title"); }
+	if ($f->Fields("sqllimit")) {$sqllimit = $f->Fields("sqllimit"); }
+	if ($f->Fields("orderby")) {$orderby = $f->Fields("orderby"); }
+	if ($f->Fields("orderbyorder")) {$orderbyorder = $f->Fields("orderbyorder"); }
+	if ($f->Fields("sqlwhere")) {$sqlwhere = $f->Fields("sqlwhere"); }
+}
+
+if ($sqlwhere) {$sqlwhere = " and ".$sqlwhere;}
+$sql = "SELECT distinct title, id, test, date, updated, shortdesc FROM articles Left Join articlereltype on articleid = id  WHERE publish=1 $sqlwhere ORDER BY $orderby $orderbyorder  limit $sqllimit" ;
+	
+$R =$dbcon->CacheExecute($sql) or DIE($sql.$dbcon->ErrorMsg());
+
+?>
+<rss version="2.0">
+   <channel>
+   	<title><?= $title ?></title>
+	<link><?= $Web_url ?></link>
+	<description><?= $meta_description ?></description>
+	<language>en-us</language>
+	<docs><?php echo $Web_url.'rssfeed.php?feed='.$_GET[feed];?></docs>
+	<pubDate><?php echo date(r); ?></pubDate>
+	<generator>Activist Mobilization Platform</generator>
+	<webMaster><?= $admEmail ?></webMaster>
+<?php 
+function makesmall($ttext) {
+	$aspace=" ";
+	$maxTextLenght=9000;
+	if(strlen($ttext) > $maxTextLenght ) {
+		$ttext = substr(trim($ttext),0,$maxTextLenght); 
+		$ttext = substr($ttext,0,strlen($ttext)-strpos(strrev($ttext),$aspace));
+		$ttext = $ttext.'...';
+	  }
+	return $ttext;
+}
+while (!$R->EOF) {
+$description = NULL;
+if ($R->Fields("title")) {
+	$link = $Web_url."article.php?id=".$R->Fields("id");
+	if ($R->Fields("shortdesc")) {
+		$description = makesmall($R->Fields("shortdesc"));
+	}
+	else {
+		//$description = makesmall($R->Fields("test"));
+	}
+	$description = htmlspecialchars(strip_tags($description));
+	if ($description == NULL) {$description= $R->Fields("title");}
+	//if ($R->Fields("date")) {
+	$date = date(r,$R->Fields("updated"));
+	$title = ereg_replace ("&", "and" ,$R->Fields("title"));
+
+	//}
+	//else {$date = rssdateformat();}
+?>
+		<item>
+			<title><?php echo $title?></title>
+			<description><?php echo $description?></description>
+			<link><?php echo $link ;?></link>
+			<pubDate><?php echo $date ;?></pubDate>
+			<guid><?php echo $link ?></guid>
+		</item>
+<?php
+	} 
+		$R->MoveNext();
+}
+?>
+
+</channel>
+</rss>
---
+++ system/rssfeed.php
@@ -0,0 +1,88 @@
+<?php
+#generic update page
+$modid = "";
+
+require("Connections/freedomrising.php");
+$buildform = new BuildForm;
+
+
+$table = "rssfeed";
+$listtitle ="RSS Feeds";
+$listsql ="select id, title title from $table  ";
+$orderby =" order by id desc  ";
+$fieldsarray=array('ID'=>'id',
+					'Title'=>'title');
+$filename="rssfeed.php";
+
+
+ob_start();
+// insert, update, delete
+if ((($_POST[MM_update]) && ($_POST[MM_recordId])) or ($_POST[MM_insert]) or (($_POST[MM_delete]) && ($_POST[MM_recordId]))) {
+    $MM_editTable  = $table;
+    $MM_recordId = $_POST[MM_recordId];
+    $MM_editRedirectUrl = $filename."?action=list";
+	if ($_POST["class"]) {$sqlwhere = " class = ".$_POST["class"]." ";}
+	if ($_POST["type"]) {$sqlwhere = " type = ".$_POST["type"]." ";}
+	if (($_POST["class"]) && ($_POST["type"])) {$sqlwhere = " class = ".$_POST["class"]." and type = ".$_POST["type"]." ";}
+
+	$MM_fieldsStr = "title|value|description|value|sqllimit|value|orderbysql|value|orderbyorder|value|sqlwhere|value";
+    $MM_columnsStr = "title|',none,''|description|',none,''|sqllimit|',none,''|orderbysql|',none,''|orderbyorder|',none,''|sqlwhere|',none,''";//|$delim,$altVal,$emptyVal|  |',none,''|
+	require ("../Connections/insetstuff.php");
+	require ("../Connections/dataactions.php");
+ob_end_flush();	
+}
+
+// build sql
+if (isset($_GET["id"])) {	$R__MMColParam = $_GET["id"]; }
+else {$R__MMColParam = "8000000";}
+$R=$dbcon->Execute("SELECT * FROM rssfeed WHERE id = $R__MMColParam") or DIE($dbcon->ErrorMsg());
+$classsql=$dbcon->Execute("SELECT id, class FROM class order by class asc") or DIE($dbcon->ErrorMsg());
+$section=$dbcon->Execute("SELECT id, type FROM articletype order by type asc") or DIE($dbcon->ErrorMsg());
+
+//declare form objects
+$rec_id = & new Input('hidden', 'MM_recordId', $_GET[id]);
+
+//build form
+$html  = $buildform->start_table('name');
+$html .= $buildform->add_header('Add/Edit '.$listtitle, 'banner');
+$html .= addfield('title','Feed Title','text',$R->Fields("title"));
+$html .= addfield('description','Feed Description','textarea',$R->Fields("description"));
+$html .= $buildform->add_header('Feed Conditions');
+
+
+$c_options = makelistarray($classsql,'id','class','Select Class');
+$csel = & new Select('class',$c_options);
+$html .=  $buildform->add_row('Feed from content in this class', $csel);
+
+$type_options = makelistarray($section,'id','type','Select Section');
+$typesel = & new Select('type',$type_options);
+$html .=  $buildform->add_row('Feed from content in this section', $typesel);
+
+$html .= addfield('sqlwhere','Where Statement','textarea',$R->Fields("sqlwhere"));
+
+$order2_options = array(''=>'','date'=>'Date','updated'=>'Date Updated','id'=>'ID','title'=>'Title');
+$order2 = & new Select('orderbysql',$order2_options,$R->Fields("orderbysql"));
+$html .=  $buildform->add_row('Order By', $order2);
+
+$order_options = array(''=>'','asc'=>'asc','desc'=>'desc');
+$order = & new Select('orderbyorder',$order_options,$R->Fields("orderbyorder"));
+$html .=  $buildform->add_row('Order By', $order);
+
+$html .= addfield('sqllimit','Number of Items','text',$R->Fields("sqllimit"));
+
+$html .= $buildform->add_content($buildform->add_btn() .'&nbsp;'. $buildform->del_btn().$rec_id->fetch());
+$html .= $buildform->end_table();
+$form = & new Form();
+$form->set_contents($html);
+
+include ("header.php");
+
+if ($_GET[action] == "list") {
+	listpage($listtitle,$listsql,$fieldsarray,$filename,$orderby,$sort);
+}
+else {
+	echo $form->fetch();
+}	
+
+include ("footer.php");
+?>
