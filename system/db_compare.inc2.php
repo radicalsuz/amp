840f79fd2cc907b695e2958ea4aca60ad5f43d2e
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2004-10-05 20:55:21 +0000

    db_compare utilities (coded for embers, need to be changed for other servers)
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@102 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ system/db_compare.inc2.php
@@ -0,0 +1,147 @@
+<?php
+
+class dbDefinition {
+	var $tableSet;
+	var $fieldSet;
+	var $fieldSet_extended;
+	var $dbOutput;
+	var $dbname;
+	var $dbCompareInfo;
+	var $tables_missing;
+	var $tables_found;
+	var $tables_common;
+	var $fields_missing;
+	var $fields_found;
+	var $fields_common;
+	var $fields_newtype;
+	var $recordcount;
+
+	function dbDefinition(){
+	}
+
+	function dbLoadDef($dbcon, $makename) {
+		$this->fieldSet=array();
+		$this->tableSet = $dbcon->MetaTables('TABLES');
+		foreach ($this->tableSet as $table) {
+			$this->fieldSet[$table] = $dbcon->MetaColumns( $table, FALSE );
+			foreach ($this->fieldSet[$table] as $fieldobj) {
+				$this->fieldSet_extended[$table][$fieldobj->name]=$fieldobj->type.$fieldobj->max_length;
+			}
+	#		$temp_recordset=$dbcon->Execute("SELECT * from $table");
+	#		$this->recordcount[$table]=$temp_recordset->RecordCount();
+		}
+		$this->dbname = $makename;
+		return $this;
+	}
+
+	function dbtable_Structure($currentTable, $dbdef) {
+		//display Database ->Table->All Fields & Characteristics
+		#$fieldSet=$dbcon->Metacolumns($currentTable, FALSE);
+		$fieldSet=$dbdef->fieldSet[$currentTable];
+		$this->dboutput['TablesNotFound'][$dbdef->dbname] .= "Non-template table found: <B>".$currentTable."</b> in database ".$dbdef->dbname." has <B>".count($fieldSet)."</B> fields &nbsp;&nbsp;&nbsp;No matching table in template<BR>\r\n";
+		$this->dboutput['TablesNotFound'][$dbdef->dbname] .="<table><tr><Td colspan=2>Fields from $currentTable</td></tr><TR><TD>FIELDNAME</TD><TD>TYPE</TD></TR>";
+		foreach ($fieldSet as $fieldobj){
+			$this->dboutput['TablesNotFound'][$dbdef->dbname] .="<tr><td>".$fieldobj->name."</td><td>".$fieldobj->type."</td></tr>";
+		}
+		$this->dboutput['TablesNotFound'][$dbdef->dbname] .="</table><BR>\r\n";
+	}
+
+	function dbFindField ($currentTable, $fieldobj, $dbdef) {
+		//Checks for a field in the master template and returns messages if field does not match master
+		$fieldfound=FALSE;
+		foreach ($this->fieldSet[$currentTable] as $master_fields){ 
+			if ($master_fields->name==$fieldobj->name){  
+			$fieldfound=TRUE ;
+				if (($master_fields->type.$master_fields->max_length) != ($fieldobj->type.$fieldobj->max_length)) {
+					$this->dboutput[$currentTable][$dbdef->dbname] .= "Field Type mismatch: <B>".$fieldobj->name."</B> in table <B>".$currentTable."</b> in database ".$dbdef->dbname." is of type <B>".$fieldobj->type."(".$fieldobj->max_length.")</B>&nbsp;&nbsp;&nbsp;Template field type: ".$master_fields->type."(".$master_fields->max_length.")<BR>\r\n";
+				}
+			}
+		}
+		if (!$fieldfound) {
+			$this->dboutput[$currentTable][$dbdef->dbname] .= "Non-template field found: <B>".$fieldobj->name."</B> in table <B>".$currentTable."</b> in database ".$dbdef->dbname." is of type <B>".$fieldobj->type."</B>&nbsp;&nbsp;&nbsp;No matching field in template<BR>\r\n";
+		}
+#		return $fieldfound;
+	}
+
+
+	function dbCompareTable ($currentTable, $dbdef) {
+	
+		#$fieldSet=$dbcon->Metacolumns($currentTable, FALSE);
+		$fieldSet=$dbdef->fieldSet[$currentTable];
+		$masterSet = $this->fieldSet[$currentTable];
+		//begin field loop
+		foreach ($fieldSet as $fieldobj){
+		//Look for field in master and check data type
+			$this->dbFindField($currentTable, $fieldobj, $dbdef);
+		} //end of field loop
+		//check for fields present in master but not in local
+		foreach($masterSet as $master_fields) {
+			$fieldfound=FALSE;
+			foreach ($fieldSet as $fieldobj) {
+				if ($fieldobj->name==$master_fields->name) {$fieldfound=TRUE;}
+			}
+			if (!$fieldfound){
+				$this->dboutput[$currentTable][$dbdef->dbname] .= "Template field missing: No copy of <B>".$master_fields->name."</b>, type <B>".$master_fields->type."</b>, in table $currentTable, exists in database ".$dbdef->dbname."<BR>\r\n";
+			}
+		}
+#		return $output;
+
+	}
+
+	function dbCheckTable ($currentTable, $dbdef) {
+		$tablefound=FALSE;
+		//begin table loop
+		foreach($this->tableSet as $tablename){
+			if ($tablename==$currentTable) {$tablefound=TRUE;}
+		}
+		if ($tablefound) {
+			$this->dbCompareTable($currentTable, $dbdef);
+		} else {
+			$this->dbtable_Structure($currentTable, $dbdef);	
+		}
+	}
+	
+	function dbCheckTables($dbdef) {
+		//Check for tables present in called set and not in secondary set
+		$this->tables_missing[$dbdef->dbname] =array_diff($this->tableSet, $dbdef->tableSet);
+		$this->tables_found[$dbdef->dbname]=array_diff($dbdef->tableSet, $this->tableSet);
+		/*foreach ($this->tables_found[$dbdef->dbname] as $debug_set){
+			echo $dbdef->dbname.": found table: ".$debug_set."<BR>";
+		}*/
+		
+		$this->tables_common[$dbdef->dbname] = array_intersect($this->tableSet, $dbdef->tableSet);
+		
+		foreach ($this->tables_common[$dbdef->dbname] as $currentTable) {
+			$master_fieldnameSet = array_keys($this->fieldSet_extended[$currentTable]);
+			$client_fieldnameSet = array_keys($dbdef->fieldSet_extended[$currentTable]);
+			$this->fields_missing[$dbdef->dbname][$currentTable]=array_diff($master_fieldnameSet, $client_fieldnameSet);
+			
+		
+			$this->fields_found[$dbdef->dbname][$currentTable]=array_diff($client_fieldnameSet, $master_fieldnameSet);
+			/*foreach ($this->fields_found[$dbdef->dbname][$currentTable] as $debug_set){
+				echo $dbdef->dbname.": found field: ".$debug_set. " in table: ".$currentTable."<BR>";
+			}*/
+			$this->fields_common[$dbdef->dbname][$currentTable]=array_intersect($client_fieldnameSet, $master_fieldnameSet);
+			$temp_fields_newtype = array_diff_assoc($dbdef->fieldSet_extended[$currentTable], $this->fieldSet_extended[$currentTable]);
+			$debug_flag=FALSE;
+			
+			foreach (array_keys($temp_fields_newtype) as $temp_field) {
+				if (in_array($temp_field, $this->fields_common[$dbdef->dbname][$currentTable])){
+					$this->fields_newtype[$dbdef->dbname][$currentTable][$temp_field]=$temp_fields_newtype[$temp_field];
+					#$debug_flag=TRUE;
+				}
+			}
+			/*if ($debug_flag) {
+			foreach (array_keys($this->fields_newtype[$dbdef->dbname][$currentTable]) as $debug_set) {
+				echo $dbdef->dbname.": field type non-match: ".$debug_set. "in table: ".$currentTable."is of type :".$this->fields_newtype[$dbdef->dbname][$currentTable][$debug_set]."<BR>";
+			}
+			} #end debug if*/
+		}
+
+	}
+	
+
+
+}
+
+?>
---
+++ system/db_compare.php
@@ -0,0 +1,193 @@
+<?php
+
+if($MM_haveMaster){
+	require_once("adodb/adodb.inc.php");
+	require_once( 'db_compare.inc2.php');
+
+	$master_template=new dbDefinition;
+	#set_time_limit(0);
+
+	##GET LIST OF DATABASES IN SYSTEM#
+	ADOLoadCode("mysql");
+	#$ampdbcon=&ADONewConnection("mysql");
+	$dbcon1=&ADONewConnection("mysql");
+	$dbcon2=&ADONewConnection("mysql");
+		
+   $dbcon2->Connect("localhost","david","havlan","amp");
+#	$dbcon2->Connect("localhost:3306","root","","amp_system");
+ 
+	$currentsql = "SELECT DISTINCT `database` FROM system where !IsNull(`database`)";
+	$AMPsql=$dbcon2->Execute($currentsql);
+	$dbcon2->Close;
+
+
+	##### LOAD MASTER TEMPLATE ####
+	$dbcon1->NConnect("localhost","david","havlan", $master_dbname);
+	$master_template->dbLoadDef($dbcon1, $master_dbname);
+	/*
+	echo "Master: ".$master_dbname."<BR>";
+	echo "Master: ".$master_template->dbname."<BR>";
+	foreach ($master_template->tableSet as $table){
+		echo "Master table: ".$table."<BR>";
+		foreach ($master_template->fieldSet[$table] as $fieldobj){
+			echo "field in master: ".$fieldobj->name;
+		}
+	}
+	*/
+	##### BEGIN COMPARISON LOOP ###
+	while (!$AMPsql->EOF){
+	 	$MM_DATABASE=$AMPsql->Fields("database");
+		#connect to Database
+		$dbcon=&ADONewConnection("mysql");
+		$dbcon->NConnect("localhost","david","havlan", $MM_DATABASE);
+		$currentDB[$MM_DATABASE] = new dbDefinition;
+		$currentDB[$MM_DATABASE]->dbLoadDef($dbcon, $MM_DATABASE);
+		$master_template->dbCheckTables($currentDB[$MM_DATABASE]);
+		$AMPsql->MoveNext();
+		$dbcon->Close();
+	
+	}
+	#####OUTPUT REPORT######
+	//HEADERDATA and JAVASCRIPT HIDE FUNCTIONS
+	$head_html= "<HTML><HEAD><TITLE=\"DB Compare Tool\"></HEAD><LINK rel=\"stylesheet\" href=\"db_compare.css\" type=\"text/css\"><script type=\"text/javascript\">\r\n 
+	
+	function hideClass(theclass, objtype) {
+	if (objtype=='') {objtype='div';}
+	for (i=0;i<document.getElementsByTagName(objtype).length; i++) {
+		if (document.getElementsByTagName(objtype).item(i).className == theclass){
+			document.getElementsByTagName(objtype).item(i).style.display = 'none';
+		}
+	}
+	}
+
+	function showClass(theclass, objtype) {
+	if (objtype=='') {objtype='div';}
+	for (i=0;i<document.getElementsByTagName(objtype).length; i++) {
+		if (document.getElementsByTagName(objtype).item(i).className == theclass){
+			document.getElementsByTagName(objtype).item(i).style.display = 'block';
+		}
+	}
+	}
+
+	function change(which, whatkind) {
+	if (whatkind!='') {hideClass(whatkind, '');}
+		if(document.getElementById(which).style.display == 'block' ) {
+			document.getElementById(which).style.display = 'none';
+		} else {
+		document.getElementById(which).style.display = 'block';
+		//alert(which+'/'+whatkind);
+		}
+	}
+	</script>";
+	$head_html.="<BODY BGCOLOR=\"#FFFFFF\"><div id=\"container\">";
+	 /*$AMPsql->MoveFirst();
+	 while (!$AMPsql->EOF) {
+		$head_html .= "document.getElementById('database_".$AMPsql->fields("database")."').style.display = 'none';\r\n";
+		$AMPsql->MoveNext();
+	 }*/	
+	$control_html="<div id=\"left_nav\"><form name=\"show_db\">";
+	$AMPsql->MoveFirst();
+	$control_html.=$AMPsql->GetMenu("db_control", "", FALSE, 0, 10, "onchange=\"change(('database_'+document.forms['show_db'].elements['db_control'].value), 'db_container'); change(('db_control_'+document.forms['show_db'].elements['db_control'].value), 'db_control_nav');\"");
+	$AMPsql->MoveFirst();
+
+	while(!$AMPsql->EOF){
+		$thisDB=$AMPsql->Fields("database");
+		//Parse table errors into categories
+		/*
+		foreach($master_template->tableSet as $currentTable) {
+			if ($master_template->dboutput[$currentTable][$AMPsql->Fields("database")]>"") {
+				$myOutput=explode("<BR>",  $master_template->dboutput[$currentTable][$AMPsql->Fields("database")]);
+				foreach ($myOutput as $thisLine) {
+					$problemtype=substr($thisLine, 0, strpos($thisLine, ":"));
+					if ($problemtype!=""){
+						#$debug_html.=$AMPsql->Fields("database").": ".$problemtype."<BR>";
+						$problemtype=str_replace(" ", "_", trim($problemtype));
+						$db_divided_output[trim($problemtype)].=$thisLine;
+						if (strpos($db_menu, trim($problemtype))===FALSE) {
+							$db_menu.=trim($problemtype).":";
+						}
+					}
+				}
+			}
+		}
+		if ($master_template->dboutput['TablesNotFound'][$AMPsql->Fields("database")]>"") {
+			$db_divided_output['New_Tables_Found'].= "<HR><H2>New Table(s) Found</H2><BR>".$master_template->dboutput['TablesNotFound'][$AMPsql->Fields("database")];
+			$db_menu.='New_Tables_Found:';
+		}
+		*/
+		$db_divided_output=array();
+		$db_menu ="";
+		//MOVE ERROR RECORD FROM master_template OBJECT into HTML array for OUTPUT
+		
+
+		foreach ($master_template->tables_missing[$thisDB] as $output_set){
+			$db_divided_output["Tables_Missing"].= $thisDB.": missing table: ".$output_set."<BR>\r\n";
+		}
+		foreach ($master_template->tables_found[$thisDB] as $output_set){
+			$db_divided_output["Tables_Found"].= $thisDB.": found table: ".$output_set."<BR>\r\n";
+		}
+		foreach($master_template->tables_common[$thisDB] as $currentTable) {
+			foreach ($master_template->fields_missing[$thisDB][$currentTable] as $output_set){
+					$db_divided_output["Fields_Missing"].= $thisDB.": in table: ".$currentTable." missing field: ".$output_set."<BR>\r\n";
+			}
+			foreach ($master_template->fields_found[$thisDB][$currentTable] as $output_set){
+					$db_divided_output["Fields_Found"].= $thisDB.": in table: ".$currentTable." found field: ".$output_set."<BR>\r\n";
+			}
+			if (count($master_template->fields_newtype[$thisDB][$currentTable])) {
+				foreach (array_keys($master_template->fields_newtype[$thisDB][$currentTable]) as $output_set) {
+					$db_divided_output["Field_Type_Changes"].= $thisDB.": in table: ".$currentTable." field type non-match: ".$output_set." is of type :".$master_template->fields_newtype[$thisDB][$currentTable][$output_set]."<BR>\r\n";
+				}
+			}	
+		}		
+		foreach ($currentDB[$thisDB]->tableSet as $currentTable) {
+			$db_divided_output["DB_Structure"].=$currentTable." has <A href=\"#\" onclick=\"change('field_list_".$currentTable."_".$thisDB."', '');\"> ".count($currentDB[$thisDB]->fieldSet[$currentTable])." fields</a><BR>";
+			#$db_divided_output["DB_Structure"].="<div id=\"field_list_".$currentTable."_".$thisDB."\" class=\"field_list\">";
+			#foreach (array_keys($currentDB[$thisDB]->fieldSet_extended[$currentTable]) as $currentField) {
+			#	$db_divided_output["DB_Structure"].="$currentField is of type ".$currentDB[$thisDB]->fieldSet_extended[$currentTable][$currentField]."<BR>";
+			#}
+			#$db_divided_output["DB_Structure"].="</div>";
+			$db_divided_output["DB_Structure"].="<ul id=\"field_list_".$currentTable."_".$thisDB."\" class=\"field_list\">";
+			foreach (array_keys($currentDB[$thisDB]->fieldSet_extended[$currentTable]) as $currentField) {
+				$db_divided_output["DB_Structure"].="<LI>$currentField is of type ".$currentDB[$thisDB]->fieldSet_extended[$currentTable][$currentField]."</LI>";
+			}
+			$db_divided_output["DB_Structure"].="</ul>";
+		}
+
+		//CREATE FINAL OUTPUT DATA
+		$content_html_header = "<div class=db_container id=\"database_".$AMPsql->fields("database")."\"><H1>".$AMPsql->fields("database")."</H1>";
+
+		$content_html_body="";
+		//echo errors out to display form by category
+		#$debug_html.=$AMPsql->Fields("database").": ".$db_menu."<BR>";
+		#echo $thisDB.": Menu :".$db_menu;
+		#$db_menu_set=explode(":",$db_menu);
+		$db_menu_set=array_keys($db_divided_output);
+		$control_html .= "<div class=db_control_nav id=\"db_control_$thisDB\"><select name =\"".$thisDB."_action\" size=\"6\" onchange=\"change(('database_".$thisDB."_'+document.forms['show_db'].elements['".$thisDB."_action'].value), 'list_entry');\">";
+		foreach($db_menu_set as $problemtype) {
+			if ($problemtype>""){
+				$control_html.="<option value=\"$problemtype\">".str_replace("_", " ", $problemtype)."</option>\r\n";
+				$content_html_header.="<a href=\"#\" title=\"show $problemtype\" onclick=\"change('database_".$thisDB."_$problemtype', 'list_entry');\" class=content_nav>".str_replace("_", " ", $problemtype)."</a>";
+				$content_html_body.="<div class=\"list_entry\" id=\"database_".$thisDB."_$problemtype\">";
+				$content_html_body.="<H2>".str_replace("_", " ",$problemtype).":</h2><BR>".$db_divided_output[$problemtype];
+				$content_html_body.="</div>";
+			}
+		}
+		$control_html.="</select></div>";
+		$content_html.= $content_html_header.$content_html_body."</div>";
+		$AMPsql->MoveNext();
+	}
+	$control_html .= "</form></div>";
+	$footer_html="</div></body></html>";
+	echo $head_html.$control_html.$content_html.$debug_html.$footer_html;
+ #####FORM FOR TEMPLATE DB####   
+} else {?>
+<HTML>
+<HEAD><TITLE="DB Compare Tool"></HEAD>
+<BODY BGCOLOR=#FFFFFF>
+<FORM name = "compare" action="db_compare_local3.php" method="GET">
+Enter master database template for comparison:<BR>
+<INPUT type="text" name="master_dbname" size="20" value="victoria">
+<INPUT type="hidden" name="MM_haveMaster" value="1">
+</FORM>
+</BODY></HTML>
+<?php } ?>
