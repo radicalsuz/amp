6fc26a3c133ab9a1371bb66b746617ab457ddcb2
lattice <lattice@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2004-10-20 03:31:21 +0000

    Restored modinput4_view.php
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@148 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ system/modinput4_view.php
@@ -0,0 +1,71 @@
+<?php
+
+/*****
+ *
+ * AMP UserData Form View
+ *
+ * (c) 2004 Radical Designs
+ * Written by Blaine Cook, blaine@radicaldesigns.org
+ *
+ *****/
+
+require_once( 'AMP/UserDataInput.php' );
+require_once( 'Connections/freedomrising.php' );
+require_once( 'utility.functions.inc.php' );
+
+// Fetch the form instance specified by submitted modin value.
+$udm = new UserDataInput( $dbcon, $_REQUEST[ 'modin' ] );
+$udm->admin = true;
+$udm->authorized = true;
+
+$modidselect=$dbcon->Execute("SELECT id from modules where userdatamodid=" . $udm->instance ) or DIE($dbcon->ErrorMsg());
+$modid=$modidselect->Fields("id");
+
+// User ID.
+$uid = (isset($_REQUEST['uid'])) ? $_REQUEST['uid'] : false;
+$udm->uid = $uid;
+
+// Was data submitted via the web?
+$sub = (isset($_REQUEST['btnUdmSubmit'])) ? $_REQUEST['btnUdmSubmit'] : false;
+
+// Fetch or save user data.
+if ( $sub ) {
+
+    // Save only if submitted data is present, and the user is
+    // authenticated, or if the submission is anonymous (i.e., !$uid)
+    $udm->saveUser();
+
+} elseif ( !$sub && $uid ) {
+
+    // Fetch the user data for $uid if there is no submitted data
+    // and the user is authenticated.
+    $udm->submitted = false;
+    $udm->getUser( $uid ); 
+
+}
+
+/* Now Output the Form.
+
+   Any necessary changes to the form should have been registered
+   before now, including any error messages, notices, or
+   complete form overhauls. This can happen either within the
+   $udm object, or from print() or echo() statements.
+
+   By default, the form will include AMP's base template code,
+   and any database-backed intro text to the appropriate module.
+
+*/
+
+$mod_id = $udm->modTemplateID;
+
+require_once( 'header.php' );
+
+print "<h2>Add/Edit " . $udm->name . "</h2>";
+
+$udm->showForm = true;
+print $udm->output();
+
+// Append the footer and clean up.
+require_once( 'footer.php' );
+
+?>
