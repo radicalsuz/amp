28be6d2980ad6c981535ac067bbd8c73b2573806
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2004-11-30 22:08:14 +0000

    formatting change
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@170 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- system/amp_alerts.php
+++ system/amp_alerts.php
@@ -4,12 +4,14 @@
 require("Connections/freedomrising.php");
 
 function display_alerts($q) {
+	echo "<table cellpadding=3><tr><td>";
 	while (!$q->EOF) {
 		echo "<h3>".$q->Fields("alert_title")."</h3>";
 		echo "<p><b>".$q->Fields("date")."</b><br>".$q->Fields("alert_text")."</p><br>";
 		
 		$q->MoveNext();
 	}
+	echo "</td></tr></table>";
 }
 $sql="select * from alerts where publish =1 order by date desc";
 $alerts_txt=$ampdbcon->Execute($sql) or DIE("15".$ampdbcon->ErrorMsg());
