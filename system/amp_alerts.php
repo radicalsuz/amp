5021619da817af096f230c6b3e4c3b8211106434
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2004-11-30 22:55:03 +0000

    more email detials
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@174 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- system/amp_alerts.php
+++ system/amp_alerts.php
@@ -4,21 +4,21 @@
 require("Connections/freedomrising.php");
 
 function display_alerts($q) {
-	echo "<table width=\"100%\" cellpadding=3><tr><td>";
 	while (!$q->EOF) {
 		echo "<h3>".$q->Fields("alert_title")."</h3>";
-		echo "<p class=\"name\"><b>".$q->Fields("date")."</b><br>".$q->Fields("alert_text")."</p><br>";
-		
+		echo "<p class=\"name\"><b>".$q->Fields("date")."</b><br>".$q->Fields("alert_text")."</p>";		
 		$q->MoveNext();
 	}
-	echo "</td></tr></table>";
-}
+	}
 $sql="select * from alerts where publish =1 order by date desc";
 $alerts_txt=$ampdbcon->Execute($sql) or DIE("15".$ampdbcon->ErrorMsg());
 include ("header.php");
+echo "<table width=\"100%\" cellpadding=3><tr><td>";
 echo "<h2>AMP SYSTEM UPDATES & NEWS</h2>";	
 display_alerts($alerts_txt);
 include ("amp_alerts_emails.php");
+echo "</td></tr></table>";
+
 include ("footer.php");
 
 ?>
--- system/amp_alerts_emails.php
+++ system/amp_alerts_emails.php
@@ -1,5 +1,6 @@
 <h3>AMP Email Alerts</h3>
-<form name="form" method="post" action="http://www.radicaldesigns.org/custom/amp_alerts.php" class="name">
+<?php if ($_GET["message"]) {echo $_GET["message"];} ?>
+<form name="form" method="post" action="http://www.radicaldesigns.org/custom/alert_emails.php" class="name">
 <input name="redirect" type="hidden" value="<?php echo $_SERVER["SCRIPT_URI"]."?".$_SERVER["QUERY_STRING"] ?>">
 <input name="user_ID" type="hidden" value="<?php echo $ID ?>">
 <input name="domain" type="hidden" value="<?php echo $_SERVER["HTTP_HOST"] ?>">
