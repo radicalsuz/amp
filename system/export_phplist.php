76070b3b99bbbb84848153fc15b578c59d887dbc
lattice <lattice@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2004-12-07 03:16:31 +0000

    Initial Checkin of PHPlist export code.
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@179 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ system/export_phplist.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once( "Connections/freedomrising.php" );
+require_once( "XML/Serializer.php" );
+
+$dbcon->SetFetchMode( ADODB_FETCH_ASSOC );
+
+$user_sql = "SELECT * FROM phplist_user_user";
+$attribute_sql = "SELECT * FROM phplist_user_user_attribute LEFT JOIN phplist_user_attribute ON phplist_user_user_attribute.attributeid = phplist_user_attribute.id";
+$list_sql = "SELECT * FROM phplist_listuser";
+
+$user_rs = $dbcon->Execute( $user_sql );
+$attr_rs = $dbcon->Execute( $attribute_sql );
+$list_rs = $dbcon->Execute( $list_sql );
+
+while ( $row = $user_rs->FetchRow() ) {
+    $subdata[ $row['id'] ] = $row;
+}
+
+while ( $row = $attribute_rs->FetchRow() ) {
+    $attrname = preg_replace( " ", "-", $row['name'] );
+    $subdata[ $row[ 'userid' ] ][ $attrname ] = $row[ 'value' ];
+}
+
+while ( $row = $list_rs->FetchRow() ) {
+    $subdata[ $row[ 'userid' ] ]['lists'][] = $row[ 'listid' ];
+}
+
+$serializer = new XML_Serializer();
+
+$result = $serializer->serialize( $subdata );
+
+if ( $result === true ) {
+    echo $serializer->getSerializedData();
+}
+
+?>
