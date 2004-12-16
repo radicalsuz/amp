dc3cb8a7cedf3c2c1708371fad3169f016f485ff
lattice <lattice@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2004-12-16 23:02:15 +0000

    Cleanup for mod_heirarchy.
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@215 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- Connections/modhierarchy.php
+++ Connections/modhierarchy.php
@@ -1,16 +1,27 @@
 <?php 
+
 // find out hierarchy for module and assign hierarchy vars
-$modhierarchy=$dbcon->CacheExecute("SELECT templateid, name, type FROM moduletext WHERE id = $mod_id") or DIE($dbcon->ErrorMsg());  
-	$MM_type = $modhierarchy->Fields("type");
-	$mod_name = $modhierarchy->Fields("name");
-	$mod_template = $modhierarchy->Fields("templateid");
-include($base_path."Connections/menu.class.php");
+$modhierarchy=$dbcon->CacheExecute("SELECT templateid, name, type FROM moduletext WHERE id = $mod_id")
+    or die("Couldn't establish module rank: " . $dbcon->ErrorMsg());  
+
+$MM_type = $modhierarchy->Fields("type");
+$mod_name = $modhierarchy->Fields("name");
+$mod_template = $modhierarchy->Fields("templateid");
+
+require_once($base_path."Connections/menu.class.php");
 $obj = new Menu;
+
 // populate the module instance varaibles
-if (isset($modid)){
-$modinstance = $dbcon->CacheExecute("SELECT * from module_control where modid = $modid") or DIE($dbcon->ErrorMsg());
-while (!$modinstance->EOF) {
-$a = $modinstance->Fields("var");
-$$a = $modinstance->Fields("setting");
-$modinstance->MoveNext();} }
+if (isset($modid)) {
+
+    $modinstance = $dbcon->CacheExecute("SELECT * from module_control where modid = $modid")
+        or die("Couldn't find module instance data: " . $dbcon->ErrorMsg());
+
+    while (!$modinstance->EOF) {
+        $a = $modinstance->Fields("var");
+        $$a = $modinstance->Fields("setting");
+        $modinstance->MoveNext();
+    }
+}
+
 ?>
