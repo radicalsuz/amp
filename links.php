8a73223ad37ea974c9ff4b33709d723e438f25cb
margot <margot@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2004-12-16 21:31:01 +0000

    Just migrating some bug fixes back down from embers.
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@211 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- links.php
+++ links.php
@@ -12,38 +12,52 @@ $mod_id = 12;
 include("sysfiles.php");
 include("header.php"); ?>
 <?php 
-if ($_GET["name"]) 
-	 {$name_link = $_GET["name"];
-  		header ("Location: links.php#$name_link");  }
-		if ($_GET[linktype] ) {
-$sql = "SELECT l. * , t.name FROM links l, linksreltype r, linktype t WHERE l.id = r.linkid AND l.linktype = t.id AND r.typeid =$_GET[linktype] order by t.name asc, l.linkname asc";}
-else  { $sql = "SELECT l. * , t.name FROM links l, linktype t WHERE  l.linktype = t.id  order by t.name asc, l.linkname asc";}
+if ($_GET["name"]) {
+	 $name_link = $_GET["name"];
+  	header ("Location: links.php#$name_link");  
+}
 
-   $links=$dbcon->CacheExecute($sql) or DIE($dbcon->ErrorMsg());
+if ($_GET[linktype] ) {
+	$sql = "SELECT l. * , t.name FROM links l, linksreltype r, linktype t WHERE l.id = r.linkid AND l.linktype = t.id AND r.typeid =$_GET[linktype] order by t.name asc, l.linkname asc";
+	}
+	else  { 
+	$sql = "SELECT l. * , t.name FROM links l, linktype t WHERE  l.linktype = t.id  order by t.name asc, l.linkname asc";
+}
 
-if ($_GET[linktype] ) { $linkt=$dbcon->CacheExecute("select  type from articletype where id = $_GET[linktype]") or DIE($dbcon->ErrorMsg());
-echo " <p class=title>".$linkt->Fields("type")."</p><br> <table border=0>"; }
+$links=$dbcon->CacheExecute($sql) or DIE($dbcon->ErrorMsg());
+
+if ($_GET[linktype]) { 
+	$linkt=$dbcon->CacheExecute("select  type from articletype where id = $_GET[linktype]") or DIE($dbcon->ErrorMsg());
+	echo "<p class=title>".$linkt->Fields("type")."</p><br>";
+}
  
- while  (!$links->EOF) 
-   { 
-   
-   if (strtolower(trim($links->Fields("name"))) != $curType){
-   echo "<p class=listtitle><a name=\"".$links->Fields("typeid")."\"></a>".$links->Fields("name")."</p><table width=100% border=0 cellpadding=3 cellspacing=0>";}
+while  (!$links->EOF) { 
+   if (strtolower(trim($links->Fields("name"))) != $curType) {
+   		echo "<p class=listtitle><a name=\"".$links->Fields("linktype")."\"></a>".$links->Fields("name")."</p><table width=100% border=0 cellpadding=3 cellspacing=0>";
+   	}
+	$curType = strtolower(trim($links->Fields("name")));
+?>
+	<tr><td>
+	<p class="text">
+	<a href="<?php echo $links->Fields("url")?>" target="_blank">
 
-   $curType = strtolower(trim($links->Fields("name")));
-   ?>
-<tr><td>
-<p class="text">
-<a href="<?php echo $links->Fields("url")?>" target="_blank">
-
-<? if (($links->Fields("image"))!= ($null)) {  ?>
-    <img name="thumbnail" src="img/thumb/<?php echo $links->Fields("image")?>" alt="thumbnail" hspace="8" vspace="1" border="0" align="left"><br><?php 
+<? if ($links->Fields("image")) {  ?>
+    	<img name="thumbnail" src="img/thumb/<?php echo $links->Fields("image")?>" alt="thumbnail" hspace="8" vspace="1" border="0" align="left"><br><?php 
     }  ?>
 
-<?php echo $links->Fields("linkname")?></a> &nbsp;&nbsp; <?php echo $links->Fields("description")?></td></tr>
-<?php  $links->MoveNext();}
+<?php 
+	echo $links->Fields("linkname") ?>
+	</a> &nbsp;&nbsp; <?php echo $links->Fields("description")?></td></tr>
+<?php  
+	$links->MoveNext();
+	if (strtolower(trim($links->Fields("name"))) != $curType) {
+		echo '</table>';
+	}
+}
+
+
 ?>
-</table>
+
 <p>&nbsp;</p>
 
 <?php include("footer.php"); ?>
