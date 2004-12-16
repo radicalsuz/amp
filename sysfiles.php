41e5bd91419ca787ab7a64eaf112baa1bff4e367
lattice <lattice@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2004-12-16 23:32:51 +0000

    Cleanup for sysfiles.php
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@220 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- sysfiles.php
+++ sysfiles.php
@@ -1,6 +1,8 @@
 <?php
-  require_once("adodb/adodb.inc.php");
-  require_once("Connections/freedomrising.php");
-  require_once("Connections/modhierarchy.php");  
-  require_once("Connections/templateassign.php");
-  ?>
+
+require_once("adodb/adodb.inc.php");
+require_once("Connections/freedomrising.php");
+require_once("Connections/modhierarchy.php");  
+require_once("Connections/templateassign.php");
+
+?>
