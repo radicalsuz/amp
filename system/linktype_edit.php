5d0b077f223273f972fc7855e254e949ae371097
margot <margot@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2004-12-17 00:00:47 +0000

    Added Functionality: You can now publish/hide entire types of links Changes: also rewrote linktype_list for the proper formatting and made linktype_edit function as both the edit and add page.
    This change requires these DB aditions:
    
    ALTER TABLE `linktype` ADD `publish` int DEFAULT NULL 
    
    UPDATE 'modules' set navhtml = '
     <a href=\"link_list.php\" class=\"side\">View/Edit Links</a><br>
    <a href=\"link_edit.php\" class=\"side\">Add Links</a><br>
    <a href=\"linktype_list.php\" class=\"side\">View/Edit Link Type</a><br>
    <a href=\"linktype_edit.php\" class=\"side\">Add Link Type</a> <br>
    <a href=\"module_control_list.php?modid=11\" class=\"side\">Settings</a>' 
    WHERE id = 11
    
    
    
    
    
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@223 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- system/linktype_edit.php
+++ system/linktype_edit.php
@@ -1,8 +1,10 @@
 <?php
+
 $modid=11;
-  require("Connections/freedomrising.php");
-?><?php
-  // *** Edit Operations: declare Tables
+
+require("Connections/freedomrising.php");
+
+// *** Edit Operations: declare Tables
   $MM_editAction = $PHP_SELF;
   if ($QUERY_STRING) {
     $MM_editAction = $MM_editAction . "?" . $QUERY_STRING;
@@ -11,7 +13,7 @@ $modid=11;
   $MM_abortEdit = 0;
   $MM_editQuery = "";
   ob_start();
-?><?php
+
   // *** Update Record: set variables
   
   if (isset($MM_update) && (isset($MM_recordId))) {
@@ -21,8 +23,8 @@ $modid=11;
     $MM_editColumn = "id";
     $MM_recordId = "" . $MM_recordId . "";
     $MM_editRedirectUrl = "linktype_list.php";
-    $MM_fieldsStr = "name|value";
-    $MM_columnsStr = "name|',none,''";
+    $MM_fieldsStr = "name|value|publish|value";
+    $MM_columnsStr = "name|',none,''|publish|none,1,0";
   
     // create the $MM_fields and $MM_columns arrays
    $MM_fields = Explode("|", $MM_fieldsStr);
@@ -38,11 +40,28 @@ $modid=11;
     $MM_editRedirectUrl .= ((strpos($MM_editRedirectUrl, '?') == false)?"?":"&") . $QUERY_STRING;
     }
   }
-  ?>
-<?php
-  // *** Delete Record: declare variables
-  if (isset($MM_delete) && (isset($MM_recordId))) {
-//    $MM_editConnection = $MM__STRING;
+  
+  if (isset($MM_insert)){
+
+   // $MM_editConnection = MM__STRING;
+   $MM_editTable  = "linktype";
+   $MM_editRedirectUrl = "linktype_list.php";
+   $MM_fieldsStr = "name|value";
+   $MM_columnsStr = "name|',none,''";
+
+  // create the $MM_fields and $MM_columns arrays
+   $MM_fields = explode("|", $MM_fieldsStr);
+   $MM_columns = explode("|", $MM_columnsStr);
+  
+  // set the form values
+  for ($i=0; $i+1 < sizeof($MM_fields); ($i=$i+2)) {
+    $MM_fields[$i+1] = $$MM_fields[$i];
+ 	}
+ }
+ 
+ // *** Delete Record: declare variables
+ if (isset($MM_delete) && (isset($MM_recordId))) {
+	//    $MM_editConnection = $MM__STRING;
     $MM_editTable  = "linktype";
     $MM_editColumn = "id";
     $MM_recordId = "" . $MM_recordId . "";
@@ -54,45 +73,60 @@ $modid=11;
   }
   require ("../Connections/dataactions.php");
   ob_end_flush();
-?>
 
-<?php
-$Recordset1__MMColParam = "1";
-if (isset($HTTP_GET_VARS["id"]))
-  {$Recordset1__MMColParam = $HTTP_GET_VARS["id"];}
-?>
-<?php
-   $Recordset1=$dbcon->Execute("SELECT * FROM linktype WHERE id = " . ($Recordset1__MMColParam) . "") or DIE($dbcon->ErrorMsg());
-   $Recordset1_numRows=0;
-   $Recordset1__totalRows=$Recordset1->RecordCount();
-?>
-<?php include("header.php"); ?>
-<body bgcolor="#FFFFFF" text="#000000">
+$fields = array();
+
+if (isset($_REQUEST["id"])) {
+	$linkTypeRs = $dbcon->Execute("SELECT * FROM linktype WHERE id = " . $dbcon->qstr($_REQUEST['id']))
+		or DIE($dbcon->ErrorMsg());
+
+	$fields['id']      = $linkTypeRs->Fields('id');
+	$fields['name']    = $linkTypeRs->Fields('name');
+	$fields['publish'] = $linkTypeRs->Fields('publish');
+}
+
+include("header.php"); ?>
+
+<h2>Edit Link Type</h2>
 <form method="POST" action="<?php echo $MM_editAction?>" name="form1">
   <table border=0 cellpadding=2 cellspacing=0 align="center">
     <tr valign="baseline"> 
       <td nowrap align="right">Name:</td>
       <td> 
-        <input type="text" name="name" value="<?php echo $Recordset1->Fields("name")?>" size="32">
+        <input type="text" name="name" value="<?= $fields['name'] ?>" size="32">
+      </td>
+    </tr>
+    <tr valign="baseline"> 
+      <td nowrap align="right">Publish:</td>
+      <td> 
+        <input type="checkbox" name="publish" value="1" size="32" <?= ($fields['publish']==1) ? "CHECKED" : '' ?>>
       </td>
     </tr>
     <tr valign="baseline"> 
       <td nowrap align="right">&nbsp;</td>
       <td> 
-        <input type="submit" value="update Record" name="submit">
+        <input type="submit" value="Update Record" name="submit">
       </td>
     </tr>
   </table>
-  <input type="hidden" name="MM_update" value="true">
-  <input type="hidden" name="MM_recordId" value="<?php echo $Recordset1->Fields("id") ?>">
+<?php 
+  
+    if (!isset($_REQUEST['id'])) {
+		echo '<input type="hidden" name="MM_insert" value="true">';
+	} else {
+       	echo '<input type="hidden" name="MM_update" value="true">';
+	}
+	
+?>
+
+  <input type="hidden" name="MM_recordId" value="<?= $fields['id'] ?>">
 </form>
 <form name="form2" method="POST" action="<?php echo $MM_editAction?>">
   <input type="submit" name="Submit" value="delete">
   <input type="hidden" name="MM_delete" value="true">
-  <input type="hidden" name="MM_recordId" value="<?php echo $Recordset1->Fields("id") ?>">
+  <input type="hidden" name="MM_recordId" value="<?= $fields["id"] ?>">
 </form>
+
 <p>&nbsp;</p>
+
 <?php include("footer.php"); ?>
-<?php
-  $Recordset1->Close();
-?>
--- system/linktype_list.php
+++ system/linktype_list.php
@@ -1,78 +1,22 @@
 <?php
-$modid=11;
-  require("Connections/freedomrising.php");
-?><?php
-   $Recordset1=$dbcon->Execute("SELECT * FROM linktype") or DIE($dbcon->ErrorMsg());
-   $Recordset1_numRows=0;
-   $Recordset1__totalRows=$Recordset1->RecordCount();
-?><?php
-   $Repeat1__numRows = -1;
-   $Repeat1__index= 0;
-   $Recordset1_numRows = $Recordset1_numRows + $Repeat1__numRows;
-?><?php $MM_paramName = ""; ?><?php
-// *** Go To Record and Move To Record: create strings for maintaining URL and Form parameters
 
-// create the list of parameters which should not be maintained
-$MM_removeList = "&index=";
-if ($MM_paramName != "") $MM_removeList .= "&".strtolower($MM_paramName)."=";
-$MM_keepURL="";
-$MM_keepForm="";
-$MM_keepBoth="";
-$MM_keepNone="";
+$modid = "11";
 
-// add the URL parameters to the MM_keepURL string
-reset ($HTTP_GET_VARS);
-while (list ($key, $val) = each ($HTTP_GET_VARS)) {
-	$nextItem = "&".strtolower($key)."=";
-	if (!stristr($MM_removeList, $nextItem)) {
-		$MM_keepURL .= "&".$key."=".urlencode($val);
-	}
-}
+require("Connections/freedomrising.php");
 
-// add the URL parameters to the MM_keepURL string
-if(isset($HTTP_POST_VARS)){
-	reset ($HTTP_POST_VARS);
-	while (list ($key, $val) = each ($HTTP_POST_VARS)) {
-		$nextItem = "&".strtolower($key)."=";
-		if (!stristr($MM_removeList, $nextItem)) {
-			$MM_keepForm .= "&".$key."=".urlencode($val);
-		}
-	}
-}
+$table = "linktype";
+$listtitle ="Links";
+$listsql ="select id, name, publish from $table  ";
+$orderby =" order by name asc  ";
+$fieldsarray=array( 'Type Name'=>'name',
+					'ID'=>'id',
+					'Publish'=>'publish');
+$filename="linktype_edit.php";
 
-// create the Form + URL string and remove the intial '&' from each of the strings
-$MM_keepBoth = $MM_keepURL."&".$MM_keepForm;
-if (strlen($MM_keepBoth) > 0) $MM_keepBoth = substr($MM_keepBoth, 1);
-if (strlen($MM_keepURL) > 0)  $MM_keepURL = substr($MM_keepURL, 1);
-if (strlen($MM_keepForm) > 0) $MM_keepForm = substr($MM_keepForm, 1);
-?><?php include("header.php"); ?>
-<body bgcolor="#FFFFFF" text="#000000">
-<h2>Link Types</h2>
-<table width="90%" border="0" cellspacing="3" cellpadding="0">
-  <tr> 
-    <td height="18">id</td>
-    <td height="18">name</td>
-    <td height="18">&nbsp;</td>
-  </tr>
-  <?php while (($Repeat1__numRows-- != 0) && (!$Recordset1->EOF)) 
-   { 
+include ("header.php");
+
+listpage($listtitle,$listsql,$fieldsarray,$filename,$orderby,$sort);
+
+
+include ("footer.php");
 ?>
-  <tr> 
-    <td> 
-      <?php echo $Recordset1->Fields("id")?>
-    </td>
-    <td> 
-      <?php echo $Recordset1->Fields("name")?>
-    </td>
-    <td><A HREF="linktype_edit.php?<?php echo $MM_keepNone.(($MM_keepNone!="")?"&":"")."id=".$Recordset1->Fields("id") ?>">edit</A></td>
-  </tr>
-  <?php
-  $Repeat1__index++;
-  $Recordset1->MoveNext();
-}
-?>
-</table>
-
-<?php
-  $Recordset1->Close();
-?><?php include("footer.php"); ?>
