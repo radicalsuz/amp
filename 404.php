fe5540ce6e95bb83be48bc2d4c8f4adf73f58be5
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-01-13 21:50:15 +0000

    debug
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@242 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- 404.php
+++ 404.php
@@ -49,11 +49,11 @@ $go= false;
 $myURI = $dbcon->qstr(substr($_SERVER['REQUEST_URI'], 1));
 $R=$dbcon->Execute("select * from redirect where publish =1 and old=$myURI") or DIE('404 query'.$dbcon->ErrorMsg());
 
+die(AMP_LOCAL_PATH . "/custom/" . $myURI );
 
 if ( file_exists( AMP_LOCAL_PATH . "/custom/" . $myURI ) ) { 
 	include( AMP_LOCAL_PATH . "/custom/" . $myURI );
 }
-die(AMP_LOCAL_PATH . "/custom/" . $myURI );
 
 	else {
 	
