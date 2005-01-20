cc4187b0ebcb8c2da2a5b7cea8d7ef2fc247eb2f
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-01-20 21:41:35 +0000

    converted to use userdata table
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@249 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- endorse.php
+++ endorse.php
@@ -12,40 +12,53 @@ To Do:  make modular so that  it can be passed  data from new modules
 *********************/ 
 
 $modid = 6;
-$mod_id = 10;
-include("sysfiles.php");
-include("header.php"); 
+$modin = ($_REQUEST['modin'] && isset($_REQUEST['modin'])) ? $_REQUEST['modin']:1;
+$mod_id = ($_REQUEST['modtext'] && isset($_REQUEST['modtext'])) ? $_REQUEST['modtext']:10;
+include("AMP/BaseDB.php");
+include("AMP/BaseTemplate.php");
+include("AMP/BaseModuleIntro.php");  
  
- $areacalled= $_GET["area"];
-if  (($_GET["area"]) && ($_GET["area"] != "national")) {
-$areaq=$dbcon->CacheExecute("SELECT distinct states.*, moduserdata.State FROM states inner join moduserdata on states.id=moduserdata.State where moduserdata.publish=1 and moduserdata.modinid=1 and moduserdata.State = $areacalled and  moduserdata.field3 != 1 Order by states.statename asc") or DIE($dbcon->ErrorMsg());
+ 
+#clean up from udm conversion
+#also needs austin's instance code
+
+#this area stuff is old and will need to be rewritten
+#$areacalled= $_GET["area"];
+#if  (($_GET["area"]) && ($_GET["area"] != "national")) {
+#	$areaq=$dbcon->CacheExecute("SELECT distinct states.*, moduserdata.State FROM states inner join moduserdata on states.id=moduserdata.State where moduserdata.publish=1 and moduserdata.modinid=1 and moduserdata.State = $areacalled and  moduserdata.field3 != 1 Order by states.statename asc") or DIE($dbcon->ErrorMsg());
+#}
+
+#elseif  (($_GET["area"]) != "national") {
+#   $areaq=$dbcon->CacheExecute("SELECT distinct states.*, moduserdata.State FROM states, moduserdata where  states.id=moduserdata.State and  moduserdata.publish=1 and moduserdata.modinid=1  Order by states.statename asc") or DIE($dbcon->ErrorMsg());
+#}
+
+#$MM_linkid=$areaq->Fields("id"); 
+
+if (!
+
+$groups=$dbcon->CacheExecute("SELECT * FROM userdata  WHERE  publish = '1' and $modin and custom1 = 1 ORDER BY moduserdata.Comapny ASC") or DIE($dbcon->ErrorMsg());
+
+while (!$groups->EOF)   { 
+	echo '<span class ="eventtitle"><a ';
+	if (($groups->Fields("Web_Page") != (NULL)) and ($groups->Fields("Web_Page") != ("http://")))  {
+		echo "href=\"".$groups->Fields("Web_Page")."\"";
+	}
+	echo ' class ="eventtitle" target="_blank">'.  $groups->Fields("Company")  .  '</a></span><br>';
+	if (($groups->Fields("City") !=(NULL))  && ($groups->Fields("State") !=(NULL))) {
+		echo '<span class="eventsubtitle">'  .  $groups->Fields("City") . ', ' . $groups->Fields("State") . '</span><br>';
+	}
+	if (($groups->Fields("First_Name") !=(NULL))  or ($groups->Fields("Last_Name") !=(NULL))) { 
+		echo '<span class="bodygrey">'  .  $groups->Fields("First_Name") . '&nbsp;' . $groups->Fields("Last_Name") . '</span><br>';
+	}
+	if (($groups->Fields("Email") !=(NULL)) ) { 
+		echo '<span class="bodygrey"><a href="mailto:' . $groups->Fields("Email") . '">' . $groups->Fields("Email")  . '</a></span><br>';
+	 } 
+	if (($groups->Fields("Phone") !=(NULL)) ) { 
+		echo '<span class="bodygrey">' . $groups->Fields("Phone") . '</span><br>';
+	} 
+	echo '<br>';
+	$groups->MoveNext();
 }
 
-elseif  (($_GET["area"]) != "national") {
-   $areaq=$dbcon->CacheExecute("SELECT distinct states.*, moduserdata.State FROM states, moduserdata where  states.id=moduserdata.State and  moduserdata.publish=1 and moduserdata.modinid=1  Order by states.statename asc") or DIE($dbcon->ErrorMsg());
-   }
-
-   $MM_linkid=$areaq->Fields("id");
-   $groups=$dbcon->CacheExecute("SELECT moduserdata.*, states.state FROM moduserdata left join states on moduserdata.State= states.id   WHERE  moduserdata.publish = '1' and moduserdata.modinid=1 and moduserdata.field1 = 1 ORDER BY moduserdata.Organization ASC") or DIE($dbcon->ErrorMsg());
-
- while (!$groups->EOF)   { 
+include("AMP/BaseFooter.php");
 ?>
-<span class ="eventtitle"><a <?php 
- if (($groups->Fields("WebPage") != ($null)) and ($groups->Fields("WebPage") != ("http://")))  
- {echo "href=\"".$groups->Fields("WebPage")."\"";}?> class ="eventtitle" target="_blank"><?php echo $groups->Fields("Organization")?></a></span><br>
-<?php if (($groups->Fields("City") !=($null))  && ($groups->Fields("state") !=($null))) {?>
-	 <span class="eventsubtitle"><?php echo $groups->Fields("City")?>, <?php echo $groups->Fields("state")?></span><br><?php }?>
-	 
-<?php if (($groups->Fields("FirstName") !=($null))  or ($groups->Fields("LastName") !=($null))) { ?>
-<span class="bodygrey"><?php echo $groups->Fields("FirstName")?>&nbsp;<?php echo $groups->Fields("LastName")?></span><br><?php } ?>
-<?php if (($groups->Fields("EmailAddress") !=($null)) ) { ?>
-<span class="bodygrey"><a href="mailto:<?php echo $groups->Fields("EmailAddress")?>"><?php echo $groups->Fields("EmailAddress")?></a></span><br><?php } ?>
-<?php if (($groups->Fields("Phone") !=($null)) ) { ?>
-<span class="bodygrey"><?php echo $groups->Fields("Phone")?></span><br><?php } ?>
-
-<br>
-<?php
-
-  $groups->MoveNext();
-}
-include("footer.php"); ?>
