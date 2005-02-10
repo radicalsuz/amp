4ed7ea93c90658777843de117799bbe10b73b479
margot <margot@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-02-10 23:03:39 +0000

    removes tables and added new classes for styling the links module
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@326 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- links.php
+++ links.php
@@ -9,65 +9,59 @@ To Do:  user added option
 *********************/ 
 $modid=11;
 $mod_id = 12;
-include("AMP/BaseDB.php");
-include("AMP/BaseTemplate.php");
-include("AMP/BaseModuleIntro.php");  
-
-$mod_id = 12;
-
-
-
-
 if ($_GET["name"]) {
-	$name_link = $_GET["name"];
+	 $name_link = $_GET["name"];
   	header ("Location: links.php#$name_link");  
   	
 }
+include("AMP/BaseDB.php");
+include("AMP/BaseTemplate.php");
+include("AMP/BaseModuleIntro.php");
 
-
-if ($_GET["linktype"] ) {
+if ($_GET[linktype] ) {
 	$sql = "SELECT l. * , t.name FROM links l, linksreltype r, linktype t WHERE l.id = r.linkid AND l.linktype = t.id AND r.typeid =$_GET[linktype] AND l.publish = '1' order by t.name asc, l.linkname asc";
 	}
-else  { 
+	else  { 
 	$sql = "SELECT l. * , t.name FROM links l, linktype t WHERE  l.linktype = t.id  AND l.publish = '1' AND t.publish = '1' order by t.name asc, l.linkname asc";
 }
 
 $links=$dbcon->CacheExecute($sql) or DIE($dbcon->ErrorMsg());
 
-if ($_GET["linktype"]) { 
-	$linkt=$dbcon->CacheExecute("select  type from articletype where id = ".$_GET["linktype"]) or DIE($dbcon->ErrorMsg());
+if ($_GET[linktype]) { 
+	$linkt=$dbcon->CacheExecute("select type from articletype where id = $_GET[linktype]") or DIE($dbcon->ErrorMsg());
 	echo "<p class=title>".$linkt->Fields("type")."</p><br>";
 }
  
 while  (!$links->EOF) { 
    if (strtolower(trim($links->Fields("name"))) != $curType) {
-   		echo "<p class=listtitle><a name=\"".$links->Fields("linktype")."\"></a>".$links->Fields("name")."</p><table width=100% border=0 cellpadding=3 cellspacing=0>";
+   		echo '<p class="linktype">
+   				<a name="'.$links->Fields("linktype").'"></a>'.$links->Fields("name")."</p>";
    	}
 	$curType = strtolower(trim($links->Fields("name")));
-?>
-	<tr><td>
-	<p class="text">
+	
+	?>
+	<div class="links">
 	<a href="<?php echo $links->Fields("url")?>" target="_blank">
-
-<? if ($links->Fields("image")) {  ?>
-    	<img name="thumbnail" src="img/thumb/<?php echo $links->Fields("image")?>" alt="thumbnail" hspace="8" vspace="1" border="0" align="left"><br><?php 
-    }  ?>
-
-<?php 
+	<? 
+	if ($links->Fields("image")) {  ?>
+    	<img name="thumbnail" src="img/thumb/<?php echo $links->Fields("image")?>" alt="thumbnail" border="0">
+    	<?php 
+    }      
 	echo $links->Fields("linkname") ?>
-	</a> &nbsp;&nbsp; <?php echo $links->Fields("description")?></td></tr>
-<?php  
+	</a><p>
+    <?php echo $links->Fields("description")?></p> <br />
+	
+	<?php  echo '</div>';
 	$links->MoveNext();
 	if (strtolower(trim($links->Fields("name"))) != $curType) {
-		echo '</table>';
+		
 	}
-}
+} 
 
 
 ?>
 
 <p>&nbsp;</p>
 
-<?php 
-include("AMP/BaseFooter.php"); 
+<?php  include("AMP/BaseFooter.php");
 ?>
