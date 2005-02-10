0c745859ec623d3b871ac0abfb94a5a007c200ac
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-02-10 02:01:36 +0000

    added file BaseModuleIntro.php
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@297 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ include/AMP/BaseModuleIntro.php
@@ -0,0 +1,37 @@
+ <?php
+ /*********************
+11-26-2002  v3.01
+Module:  Module System
+Description:  calls module headers
+CSS: subtitle, text, title
+functions: converttext
+Called by: all modules
+To Do:  
+
+*********************/ 
+if (($mod_id) or ($intro_id)) {
+	if (!$intro_id) { $intro_id = $mod_id; }
+	$module=$dbcon->CacheExecute("SELECT * FROM moduletext WHERE id = $intro_id") or DIE("Couldn't retrieve module text: " . $dbcon->ErrorMsg());
+	if ($moduleintroreplace != NULL) { 
+		include ("$moduleintroreplace"); 
+	}
+	else {
+		echo '<table width="100%" border="0" cellpadding="0" cellspacing="0"><tr><td class="text">';
+		echo '<p class="title">'  .  $module->Fields("title")  .  '</p>';
+		if ($module->Fields("subtitile") != (NULL)) {
+			echo '<p class="subtitle">'  .  $module->Fields("subtitile")  .  '</p>';
+		}
+		if ($module->Fields("test") != (NULL)) {
+			echo '<p class="text">';
+			if  ($module->Fields("html")) {
+				echo $module->Fields("test");
+			}
+			else {
+				echo converttext($module->Fields("test"));
+			}
+			echo '</p>';
+		}
+		echo '</td></tr></table>';
+	}
+}
+?>
