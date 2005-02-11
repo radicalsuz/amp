f21c3b4647d691d83797e7fc892de156dbeb1d52
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-02-11 01:47:00 +0000

    deleted some useless fiels
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@335 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- system/dir.php
+++
@@ -1,9 +0,0 @@
-<?php
-$base_path = "/var/www/freedomrising/endtheoccupation";
-$dir = $base_path."test";
-//chown ($base_path, "www-data")or die("Couldn't chown");
-chmod ($base_path, 0777)or die("Couldn't chomd"); 
-mkdir ($dir,0777) or die("Couldn't create dir");
-chmod ($base_path, 0755); 
-echo "done";
-?>
