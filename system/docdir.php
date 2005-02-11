c18d207da47e6e83dcc44becc4576d8a68c25a58
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-02-11 01:39:01 +0000

    rewrote using ampclass
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@332 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- system/docdir.php
+++ system/docdir.php
@@ -1,51 +1,50 @@
 <?php
-  require("Connections/freedomrising.php");?>
-    <?php include("header.php");?>
-	<h2><?php echo helpme(""); ?>Documents</h2>
-	<?php
-	if (isset($actdel)){
+$modid = "19";
+
+require_once("Connections/freedomrising.php");
+
+$listtitle ="Documents";
+$filename="docdir.php";
+
+//delete file 
+if ( isset($_GET['actdel']) ){
 	$dir_name="".$base_path_amp."downloads/";
 	unlink($dir_name.$actdel);
-	}
+}
+
+// get file list
 $dir_name="".$base_path_amp."downloads";
-  echo "<table cellpadding=10>" ;
 $dir = opendir($dir_name);
 $basename = basename($dir_name);
 $fileArr = array();
 
-while ($file_name = readdir($dir))
-{
-  if (($file_name !=".") && ($file_name !=
-".."))
-  {
-    #Get file modification date...
-    #
-    $fName = "$dir_name/$file_name";
-    $fTime = filemtime($fName);
-    $fileArr[$file_name] = $fTime;    
-  }
+while ($file_name = readdir($dir)){
+	if (($file_name !=".") && ($file_name != "..")) {
+    #Get file modification date...<a href="calendar_type.php">calendar_type.php</a>
+        $fName = "$dir_name/$file_name";
+    	$fTime = filemtime($fName);
+    	$fileArr[$file_name] = $fTime;    
+  	}
 }
 
 # Use arsort to get most recent first
 # and asort to get oldest first
 arsort($fileArr);
-
 $numberOfFiles = sizeOf($fileArr);
-for($t=0;$t<$numberOfFiles;$t++)
-{
+
+for($t=0;$t<$numberOfFiles;$t++){
     $thisFile = each($fileArr);
     $thisName = $thisFile[0];
     $thisTime = $thisFile[1];
-    $thisTime = date("M d Y", $thisTime);?>
-	<tr>
-  <td>&nbsp;</td>
-	<td width='150'><b><a href="/downloads/<?php echo $thisName ?>"><?php echo $thisName ?></a></b></td>	
-		<td><?php echo $thisTime ?></td><td><a href="docdir.php?actdel=<?php echo $thisName ?>">delete</a></td>
-		
-		</tr>
-		<?php
+    $thisTime = date("M d Y", $thisTime);
+	
+	$fieldsarray[$t]['Document'] = '<a href="../downloads/'.$thisName.'" target="_blank">'.$thisName.'</a>';
+	$fieldsarray[$t]['Date'] = $thisTime;
+	$fieldsarray[$t]['Delete'] = '<a href="docdir.php?actdel='.$thisName.'">delete</a>';
 }
 closedir ($dir);
-  echo"</table>";
-  
-?><?php   include("footer.php");?>
+
+include ("header.php");
+listpage_basic($listtitle,$fieldsarray,$filename);
+include ("footer.php");
+?>
