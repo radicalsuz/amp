7b8166383ffd8b0977958663b66974a75675cf32
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-02-14 23:45:53 +0000

    added
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@362 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ system/Connections/system_navs.php
@@ -0,0 +1,152 @@
+<?php
+
+#calendar
+$sys_nav[1][0]['link'] ='calendar_gxlist.php';
+$sys_nav[1][0]['name'] ='View/Edit';
+$sys_nav[1][1]['link'] ='calendar_gxlist.php?old=1';
+$sys_nav[1][1]['name'] ='View/Edit Old Events';
+$sys_nav[1][2]['link'] ='calendar_gxedit.php';
+$sys_nav[1][2]['name'] ='Add Event';
+$sys_nav[1][3]['link'] ='calendar_type.php?acton=list';
+$sys_nav[1][3]['name'] ='Event Types';
+
+#faq
+$sys_nav[4][0]['link'] ='faq.php?action=list';
+$sys_nav[4][0]['name'] ='View/Edit';
+$sys_nav[4][1]['link'] ='faq.php';
+$sys_nav[4][1]['name'] ='Add';
+$sys_nav[4][2]['link'] ='faq_type.php?action=list';
+$sys_nav[4][2]['name'] ='FAQ Types';
+
+#petition
+$sys_nav[7][0]['link'] ='View/Edit';
+$sys_nav[7][0]['name'] ='petition.php?action=list';
+$sys_nav[7][1]['link'] ='Add';
+$sys_nav[7][1]['name'] ='petition.php';
+
+#photo gallery
+$sys_nav[8][0]['link'] ='photo_edit.php';
+$sys_nav[8][0]['name'] ='Add';
+$sys_nav[8][1]['link'] ='photo_list.php';
+$sys_nav[8][1]['name'] ='View/Edit';
+$sys_nav[8][2]['link'] ='photo_typelist.php';
+$sys_nav[8][2]['name'] ='Gallery Types';
+
+#links
+$sys_nav[11][0]['link'] ='links.php?action=list';
+$sys_nav[11][0]['name'] ='View/Edit';
+$sys_nav[11][1]['link'] ='links.php';
+$sys_nav[11][1]['name'] ='Add';
+$sys_nav[11][2]['link'] ='link_type.php?action=list';
+$sys_nav[11][2]['name'] ='Link Types';
+
+#content
+
+$sys_nav[19][0]['link'] ='articlelist.php';
+$sys_nav[19][0]['name'] ='View/Edit Content';
+$sys_nav[19][1]['link'] ='article_edit.php';
+$sys_nav[19][1]['name'] ='Add Content';
+$sys_nav[19][2]['link'] ='article_list.php?&class=2';
+$sys_nav[19][2]['name'] ='View/Edit Homepage Content';
+$sys_nav[19][3]['link'] ='article_fpedit.php';
+$sys_nav[19][3]['name'] ='Add Homepage Content';
+
+$sys_nav[19][4]['title'] ='Docs and Images';
+
+$sys_nav[19][5]['link'] ='docdir.php';
+$sys_nav[19][5]['name'] ='View Document';
+$sys_nav[19][6]['link'] ='imgdir.php';
+$sys_nav[19][6]['name'] ='View Images';
+$sys_nav[19][7]['link'] ='doc_upload.php';
+$sys_nav[19][7]['name'] ='Uplaoad Document';
+$sys_nav[19][8]['link'] ='imgup.php';
+$sys_nav[19][8]['name'] ='Uplaod Images';
+
+$sys_nav[19][9]['title'] ='Sections';
+
+$sys_nav[19][10]['link'] ='edittypes.php';
+$sys_nav[19][10]['name'] ='View/Edit Sections';
+$sys_nav[19][11]['link'] ='type_edit.php';
+$sys_nav[19][11]['name'] ='Add Section';
+
+#web actions
+$sys_nav[21][0]['link'] ='sendfax_list.php';
+$sys_nav[21][0]['name'] ='View/Edit';
+$sys_nav[21][1]['link'] ='sendfax_edit.php';
+$sys_nav[21][1]['name'] ='Add';
+
+# Comments
+$sys_nav[23][0]['link'] ='comments.php?action=list';
+$sys_nav[23][0]['name'] ='View/Edit';
+
+# Vol
+$sys_nav[40][0]['link'] ='';
+$sys_nav[40][0]['name'] ='';
+$sys_nav[40][1]['link'] ='';
+$sys_nav[40][1]['name'] ='';
+$sys_nav[40][2]['link'] ='';
+$sys_nav[40][2]['name'] ='';
+$sys_nav[40][3]['link'] ='';
+$sys_nav[40][3]['name'] ='';
+$sys_nav[40][4]['link'] ='';
+$sys_nav[40][4]['name'] ='';
+$sys_nav[40][5]['link'] ='';
+$sys_nav[40][5]['name'] ='';
+$sys_nav[40][6]['link'] ='';
+$sys_nav[40][6]['name'] ='';
+$sys_nav[40][7]['link'] ='';
+$sys_nav[40][7]['name'] ='';
+$sys_nav[40][8]['link'] ='';
+$sys_nav[40][8]['name'] ='';
+
+# Quotes
+$sys_nav[41][0]['link'] ='quotes.php?action=list';
+$sys_nav[41][0]['name'] ='View/Edit';
+$sys_nav[41][1]['link'] ='quotes.php';
+$sys_nav[41][1]['name'] ='Add';
+
+# RSS
+$sys_nav[45][0]['link'] ='rssfeed.php?action=list';
+$sys_nav[45][0]['name'] ='View/Edit Feeds';
+$sys_nav[45][1]['link'] ='rssfeep.php';
+$sys_nav[45][1]['name'] ='Add Feed';
+
+$sys_nav[45][2]['title'] ='RSS Agragator';
+
+$sys_nav[45][3]['link'] ='feeds_add.php';
+$sys_nav[45][3]['name'] ='Add/Edit';
+$sys_nav[45][4]['link'] ='feeds_update.php';
+$sys_nav[45][4]['name'] ='Update Feeds';
+$sys_nav[45][5]['link'] ='feeds_view.php?action=list';
+$sys_nav[45][5]['name'] ='View/Filter Feeds';
+
+# navigation
+$sys_nav[30][0]['link'] ='module_nav_edit.php?id=1';
+$sys_nav[30][0]['name'] ='Defualt Navigation Layout';
+$sys_nav[30][1]['link'] ='module_nav_edit.php?id=2';
+$sys_nav[30][1]['name'] ='Homepage Navigation Layout';
+$sys_nav[30][2]['link'] ='nav.php?action=list';
+$sys_nav[30][2]['name'] ='Veiw/Edit Navigation Files';
+$sys_nav[30][3]['link'] ='nav.php';
+$sys_nav[30][3]['name'] ='Add Navigation Files';
+
+#templates
+$sys_nav[31][0]['link'] ='template.php?action=list';
+$sys_nav[31][0]['name'] ='View/Edit Template';
+$sys_nav[31][1]['link'] ='template.php';
+$sys_nav[31][1]['name'] ='Add Template';
+$sys_nav[31][2]['link'] ='css_edit.php';
+$sys_nav[31][2]['name'] ='Edit Standard CSS';
+$sys_nav[31][3]['link'] ='css_list.php';
+$sys_nav[31][3]['name'] ='View/Edit Custom CSS';
+
+#
+#$sys_nav[][]['link'] ='';
+#$sys_nav[][]['name'] ='';
+
+
+#$nav_link .= '<div width= "100%"><fieldset   style="  border: 1px solid black;">';
+#$nav_link .= 'test';
+
+
+?>
