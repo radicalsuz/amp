ad6718520615f2e019094dc3bc8ad2dbcd8f000b
margot <margot@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-03-19 01:51:24 +0000

    cleaned this file and added support for a new body div and readmore class
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@523 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/AMP/Article/index.inc.php
+++ include/AMP/Article/index.inc.php
@@ -1,6 +1,6 @@
 <?php
 /*********************
-09-12-2003  v3.01
+03-16-2005  v3.01
 Module:  Index
 Description:  display index page content
 CSS: text, photocaption, hometitle,  subtitle, homebody
@@ -8,40 +8,98 @@ SYS VARS: $NAV_IMG_PATH
 functions  getimagesize
 To Do: 
 *********************/ 
-   $maintext=$dbcon->CacheExecute("SELECT *  FROM articles  WHERE class=2 and publish='1'  ORDER BY pageorder asc") or DIE($dbcon->ErrorMsg());
+$maintext=$dbcon->CacheExecute("SELECT *  FROM articles  
+                                WHERE class=2 and publish='1'  
+                                ORDER BY pageorder asc") 
+                                or DIE($dbcon->ErrorMsg());
 
-?>
-<table width="100%" class="text"><?php while  (!$maintext->EOF)
-   { ?><tr><td>
+
+print '<div class="home"><table width="100%" class="text">';
+
+while  ( !$maintext->EOF ) { 
+    print '<tr><td>';
+
+    if ( $maintext->Fields("picuse") == (1) ) { //start of picture 
+
+        $fpathtoimg = $base_path_amp.$NAV_IMG_PATH .$maintext->Fields("pselection")."/".$maintext->Fields("picture");
+        $pathtoimg = $NAV_IMG_PATH .$maintext->Fields("pselection")."/".$maintext->Fields("picture");
+        $imageInfo = getimagesize($fpathtoimg); 
+        $pwidth = $imageInfo[0]; 
+        $pheight = $imageInfo[1];
 
 
-<?php #################IMAGE IMAGE ###############
-if ($maintext->Fields("picuse") == (1)) { //start of picture 
+    print   '<table width="'. 
+            $pwidth .
+            '" border="0" align="';
+    if ( $maintext->Fields( "alignment" ) == ( "left" ) ) {
+        echo "left";
+    } else {
+    echo "right";
+    }
 
-$fpathtoimg = $base_path_amp.$NAV_IMG_PATH .$maintext->Fields("pselection")."/".$maintext->Fields("picture");
-$pathtoimg = $NAV_IMG_PATH .$maintext->Fields("pselection")."/".$maintext->Fields("picture");
-$imageInfo = getimagesize($fpathtoimg); 
-$pwidth = $imageInfo[0]; 
-$pheight = $imageInfo[1];
-?>
-<table width="<?php echo $pwidth ?>" border="0" align="<?php if ($maintext->Fields("alignment") == ("left")) {echo "left";} else {echo "right";}?>" cellpadding="0" cellspacing="0"><tr><td><img src="<?php echo $pathtoimg; ?>" alt="<?php echo $maintext->Fields("alttag")?>" vspace="4" hspace="4" border="1" width="<?php echo $pwidth ?>" height="<?php echo $pheight ?>"></td></tr><tr align="center"><td width="<?php echo $pwidth ?>" class="photocaption"><?php echo $maintext->Fields("piccap")?></td></TR></table>
-<?php } //end of picture ?>
+    print '" cellpadding="0" cellspacing="0"><tr><td>';
+    print '<img src="' .
+            $pathtoimg .  
+            ' alt="' .
+            $maintext->Fields("alttag") .
+            '" vspace="4" hspace="4" border="1" width="' .
+            $pwidth .
+            '" height="' .
+            $pheight . 
+            '"></td></tr><tr align="center"><td width="' .
+            $pwidth .
+            '" class="photocaption">' .
+            $maintext->Fields("piccap") .
+            '</td></TR></table>';
+    } //end of picture
 
-<?php if ($maintext->Fields("title") != (NULL)) {?><p class="hometitle"><?php if ($maintext->Fields("usemore") == ('1')) { ?><a href="<?php echo $maintext->Fields("morelink")?>" class="hometitle"><?php } echo $maintext->Fields("title");?><?php if ($maintext->Fields("usemore") == ('1')) { echo "</a>"; }?></p><?php }?>
-<?php if ($maintext->Fields("subtitile") != (NULL)) { ?><span class="subtitle"><?php echo $maintext->Fields("subtitile")?></span><?php }  //end if for subtitle ?>
+    if ( $maintext->Fields( "title" ) != (NULL) ) {  // start of title
+        print '<p class="hometitle">';
+        
+        if ( $maintext->Fields( "usemore" ) == ('1') ) { 
+            print '<a href="' .
+                $maintext->Fields( "morelink" ) .
+                '" class="hometitle">';
+        } 
+        
+        print $maintext->Fields( "title" );
 
-<span class="homebody"> <p class="homebody"><?php  
-### BODY TEXT ###
- if ($maintext->Fields("html") == (0)) {   // start non html text
-     echo converttext($maintext->Fields("test")); }  //end of non html text
-   if ($maintext->Fields("html") == (1)) {  //start of html text 
-    echo $maintext->Fields("test"); } //end of html text ?>
-<?php if ($maintext->Fields("usemore") == ('1')) { ?>
-</span> <br><br><div align="right"><a href="<?php echo $maintext->Fields("morelink")?>">Read More&nbsp;&#187;</a>&nbsp;&nbsp; </div>
-<?php } ?>
-</p></span><br>
-  
+        if ( $maintext->Fields( "usemore" ) == ('1') ) { 
+            echo "</a>"; 
+        }
 
-</td></tr>
-<?php  $maintext->MoveNext();  }?>
-</table>
+        print '</p>'; 
+     } // end of title
+    
+    if ( $maintext->Fields( "subtitile" ) != (NULL) ) {  // start of subtitle 
+        print '<span class="subtitle">';
+        print $maintext->Fields("subtitile");
+        print '</span>'; 
+    }  //end if for subtitle
+
+    
+    print '<span class="homebody"> <p class="homebody">';
+      
+
+    if ( $maintext->Fields( "html" ) == (0) ) {   // start non html text
+        echo converttext( $maintext->Fields( "test" ) ); 
+    }  //end of non html text
+    
+    if ( $maintext->Fields( "html" ) == (1) ) {  //start of html text 
+        echo $maintext->Fields( "test" ); 
+    } //end of html text 
+
+    if ($maintext->Fields("usemore") == ('1')) {  // start of more link
+        print '</span> <span class="morelink">'; 
+        print '<a href="' .
+                $maintext->Fields( "morelink" ) .
+                '">Read More&nbsp;&#187;</a>&nbsp;&nbsp; </span>';
+     }
+
+    print '</p></span><br></td></tr>';
+
+    $maintext->MoveNext();  
+
+}
+
+print '</table></div>';
