1dce15e1d8af46774988314fcd83eb5edc8eaddd
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-04-04 17:59:23 +0000

    added section and content to drop down
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@574 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/AMP/Nav/nav.flooret.php
+++ include/AMP/Nav/nav.flooret.php
@@ -12,10 +12,30 @@ Touch: Margot
 *********************/ 
 
 
+function nav_sub_content($type) {
+	global $dbcon;
+	$html .= '<ul class= nav_sub_list>';
+	$sql = "select title,id, linktext from articles where type = $type and publish =1 and (class !=2 and class !=8 )";
+	$R=$dbcon->Execute($sql) or DIE('Could not load the sub navigation information'.$sql.$dbcon->ErrorMsg());	
+	while (!$R->EOF) {
+		if ($R->Fields("linktext")) {
+			$link = $R->Fields("linktext");
+		}
+		else {
+			$link = $R->Fields("title");
+		}
+		$html .= '<li><a href="article.php?id='.$R->Fields("id").'">'.$link.'</a></li>';
+		$R->MoveNext();
+	}
+	$html .= '</ul>';
+	return $html;
+}
+
+
 
 // function that makes a nav that shows sub sections if in that section
 
-function nav_subs($type,$list) {
+function nav_sub_section($type) {
 	global $dbcon;
 	$html .= '<ul class="nav_sub_list">';
 		$sql = "select type, id from articletype where parent = $type and usenav =1 order by textorder, id asc";
@@ -46,8 +66,14 @@ function nav_menu_dd($type){
 		    $html .= '<li><a href="section.php?id='.$R->Fields("id").'">'.$R->Fields("type").'</a></li>';
         }
 		if (($type == $R->Fields("id")) ||  ($R->Fields("id") == $subsections->Fields("parent")) ) {
-			$html .= nav_subs($R->Fields("id"),$R->Fields("listtype"));
-		}
+			if ($R->Fields("listtype")  == 5) {
+				$html .= nav_sub_section($R->Fields("id"));
+			} else if ($R->Fields("listtype")  == 1)  {
+				$html .= nav_sub_content($R->Fields("id"));
+			} else {
+				$html .= nav_sub_section($R->Fields("id"));
+				$html .= nav_sub_content($R->Fields("id"));
+			}
 		$R->MoveNext();
 	}
 	$html .= '</ul>';
