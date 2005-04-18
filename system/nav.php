ba14378cf77534430665a515db43d9858f504356
lattice <lattice@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-04-18 21:36:59 +0000

    Fix duplicate column bug.
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@641 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- system/nav.php
+++ system/nav.php
@@ -31,8 +31,8 @@ if ((($_POST['MM_update']) && ($_POST['MM_recordId'])) or ($_POST['MM_insert']) 
 	}
 	$MM_fieldsStr =
 	
-"name|value|sql|value|titleimg|value|titletext|value|titleti|value|linkfile|value|mfile|value|mcall1|value|mvar2|value|mcall2|value|repeat|value|linkextra|value|mvar1|value|linkfield|value|mvar1val|value|nosqlcode|value|nosql|value|templateid|value|modid2|value|rss|value|titleti|value";
-    $MM_columnsStr = "name|',none,''|sql|',none,''|titleimg|',none,''|titletext|',none,''|titleti|none,1,0|linkfile|',none,''|mfile|',none,''|mcall1|',none,''|mvar2|',none,''|mcall2|',none,''|repeat|',none,''|linkextra|',none,''|mvar1|',none,''|linkfield|',none,''|mvar1val|',none,''|nosqlcode|',none,''|nosql|',none,''|templateid|',none,''|modid|',none,''|rss|',none,''|titleti|',none,''";
+"name|value|sql|value|titleimg|value|titletext|value|titleti|value|linkfile|value|mfile|value|mcall1|value|mvar2|value|mcall2|value|repeat|value|linkextra|value|mvar1|value|linkfield|value|mvar1val|value|nosqlcode|value|nosql|value|templateid|value|modid2|value|rss|value|";
+    $MM_columnsStr = "name|',none,''|sql|',none,''|titleimg|',none,''|titletext|',none,''|titleti|none,1,0|linkfile|',none,''|mfile|',none,''|mcall1|',none,''|mvar2|',none,''|mcall2|',none,''|repeat|',none,''|linkextra|',none,''|mvar1|',none,''|linkfield|',none,''|mvar1val|',none,''|nosqlcode|',none,''|nosql|',none,''|templateid|',none,''|modid|',none,''|rss|',none,''";
 
 	require ("../Connections/insetstuff.php");
     require ("../Connections/dataactions.php");
