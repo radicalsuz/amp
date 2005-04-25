caafd4a1f9f52466c762dd666acd0b1f7d425073
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-04-25 15:54:25 +0000

    double /n/n
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@737 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- system/feeds_view.php
+++ system/feeds_view.php
@@ -170,7 +170,8 @@ $i++;
 	
 	<tr bgcolor="<?php echo $bgcolor ?>" class=name>
 		<td></td>
-		<td colspan="4" charset="utf-8"><?= utf8_decode( preg_replace( "/\\n/", "<br/>", $rs->Fields("content") ) ) ?></td>
+		<td colspan="4" charset="utf-8"><?php $text = utf8_decode( preg_replace( "/\\n/", "<br/>", $rs->Fields("content") ) ); 	echo preg_replace( "/\\n\\n/", "<br/><br/>", $text ) );
+ ?></td>
 	</tr>
 	<tr bgcolor="<?php echo $bgcolor ?>" class=name>
 	  <td colspan = '5'>
