93b3a1b2e213e871a421378c80e329a03b64ac80
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-04-25 20:55:25 +0000

    inital add
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@741 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ include/AMP/Nav/nav.artsect.php
@@ -0,0 +1,32 @@
+<?php
+//get subsections
+
+// get content
+
+//show current section name
+
+$artsql = "select id, title from articles where publish=1 and uselink =1 and type = $MM_type and class != 8  and class !=2 order by pageorder, date ";
+$secsql = "select id, type from articletype where usenav=1 and parent =$MM_type order by textorder asc";
+   	$art=$dbcon->Execute($artsql) or DIE($dbcon->ErrorMsg());
+   	$sec=$dbcon->Execute($secsql) or DIE($dbcon->ErrorMsg());
+
+
+
+
+while (!$sec->EOF) {
+	$shownav.=$lNAV_HTML_3 ; 
+	$shownav.='<a href="section.php?id='.$sec->Fields("id").'" class="sidelist">'.$sec->Fields("type").'</a>';
+	$shownav.=$lNAV_HTML_4 ;
+	$sec->MoveNext();
+}
+
+
+while (!$art->EOF) {
+	$shownav.=$lNAV_HTML_3 ; 
+	$shownav.='<a href="article.php?id='.$art->Fields("id").'" class="sidelist">'.$art->Fields("title").'</a>';
+	$shownav.=$lNAV_HTML_4 ; //start link text
+	$art->MoveNext();
+}
+echo $shownav;
+
+?>
