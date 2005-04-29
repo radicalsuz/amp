ebde9bf60da5cb7d2a1efbb442297f106738c02e
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-04-29 23:40:07 +0000

    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@793 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- template.php
+++ template.php
@@ -4,6 +4,6 @@ $mod_id = $_GET['temp'];
 include("AMP/BaseDB.php");
 include("AMP/BaseTemplate.php");
 include("AMP/BaseModuleIntro.php");  
-echo "[content]";
+echo "<xml><item>content</item></xml>";
 include("AMP/BaseFooter.php");  
 ?>
