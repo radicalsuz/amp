c7ab7ec9e4e1b495ba41c28158bcf8d95a81174d
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-04-29 22:21:39 +0000

    INITIAL ADD
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@791 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ template.php
@@ -0,0 +1,9 @@
+<?php
+
+$mod_id = $_GET['temp'];
+include("AMP/BaseDB.php");
+include("AMP/BaseTemplate.php");
+include("AMP/BaseModuleIntro.php");  
+echo "[content]";
+include("AMP/BaseFooter.php");  
+?>
