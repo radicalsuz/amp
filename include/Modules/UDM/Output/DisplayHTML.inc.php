4884788a09bc6c1c729d8e0009e71907c3ed4cd3
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-05-02 23:58:50 +0000

    Quick Layout Page Fix
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@794 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/Modules/UDM/Output/DisplayHTML.inc.php
+++ include/Modules/UDM/Output/DisplayHTML.inc.php
@@ -2,13 +2,14 @@
 require_once ('AMP/UserData/Plugin.inc.php');
 require_once ('AMP/UserData/Input.inc.php');
 require_once ('AMP/Region.inc.php');
+include_once ('custom.layouts.inc.php');
 
 class UserDataPlugin_DisplayHTML_Output extends UserDataPlugin {
     
     var $options= array( 
         'subheader'=>array('available'=>true, 
                             'description'=>'Show subheadings for',
-                            'default'=>'State',
+                            'default'=>null,
                             'type'=>'text'),
         'display_format'=>array('name'=>'List Display Function Name',
                                 'default'=>'groups_layout_display',
@@ -84,7 +85,7 @@ class UserDataPlugin_DisplayHTML_Output extends UserDataPlugin {
 
             //output display format
             foreach ($dataset as $dataitem) {
-                if (isset($this->options['subheader'])) $output.=$this->subheader($dataitem);
+                if (isset($options['subheader'])) $output.=$this->subheader($dataitem, $options['subheader']);
                 if($inclass) $output.=$this->$display_function($dataitem);
                 else $output.=$display_function($dataitem, $this->options);
             }
@@ -93,13 +94,13 @@ class UserDataPlugin_DisplayHTML_Output extends UserDataPlugin {
 		return $output;
     }
         
-    function subheader($dataitem) {
-        if ($this->current_subheader != trim($dataitem[$this->options['subheader']['value']])) {
-            $this->current_subheader = trim($dataitem[$this->options['subheader']['value']]);
+    function subheader($dataitem, $header_field) {
+        if ($this->current_subheader != trim($dataitem[$header_field])) {
+            $this->current_subheader = trim($dataitem[$header_field]);
             $output .= '<h1 style="font-size: small; background: #ccc; padding: 3px 3px;">' . $this->current_subheader;
-            if ($this->options['subheader']['value']=='City') {
-                if ($dataitem['State']) $output.= ', ' . $this->regionset->regions['US AND CANADA'][$dataitem['State']];
-                if ($dataitem['Country']!="USA") $output.= '&nbsp;&nbsp;' . $this->regionset->regions['WORLD'][$dataitem['Country']];
+            if ($header_field=='City') {
+                if ($dataitem['State']) $output.= ($dataitem['City']?', ':'') . $this->regionset->regions['US AND CANADA'][$dataitem['State']];
+                if ($dataitem['Country']!="USA") $output.= '&nbsp;' . $this->regionset->regions['WORLD'][$dataitem['Country']];
             }
             $output.= '</h1>';
         }
@@ -133,6 +134,8 @@ class UserDataPlugin_DisplayHTML_Output extends UserDataPlugin {
         $About=$data['custom1'];
         $Details=$data['custom18'];
         $image=$data['custom19'];
+        $start="";
+        $end="";
 		
 		if ($image) {
 			$start = "<table width= \"100%\"><tr><td width = 100><img src =\"img/thumb/$image\"></td><td valign=\"top\">";
@@ -141,13 +144,14 @@ class UserDataPlugin_DisplayHTML_Output extends UserDataPlugin {
 		$html .= $start;
 
 		$html .= "<span class =\"eventtitle\"> \n";
+        $endlink='';
 		if ($Web_Page && ($Web_Page != 'http://')) {
 			 $html .= '<a href="'.$Web_Page.'" target="_blank" class ="eventtitle" >';
 			 $endlink = "</a>";
 		}
 		//else if ($Details) {
-			 $html .= '<a href="groups.php?gid='.$id.'" class ="eventtitle" >';
-			 $endlink = "</a>";
+			 #$html .= '<a href="groups.php?gid='.$id.'" class ="eventtitle" >';
+			 #$endlink = "</a>";
 		//}
 		$html .= $Organization.$endlink."</span><br>";
 		
@@ -173,8 +177,8 @@ class UserDataPlugin_DisplayHTML_Output extends UserDataPlugin {
 		if ($About) {
 			$html .= "<span class=\"bodygrey\">". converttext($About) . "</span><br>\n";
 		}
+        $html.="<p>\n";
 		# $html .= events_groups($id);
-		$html .= "<br>\n";
 		$html .= $end;
 
 		return $html;
