d3c95f5834c587f015dce589329ac070c26ce32e
lattice <lattice@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-05-12 20:55:49 +0000

    cleanup some serious nasties in links.
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@878 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- links.php
+++ links.php
@@ -9,7 +9,7 @@ To Do:  user added option
 *********************/ 
 $modid=11;
 $mod_id = 12;
-if ($_GET["name"]) {
+if (isset($_GET["name"]) && $_GET['name']) {
 	 $name_link = $_GET["name"];
   	header ("Location: links.php#$name_link");  
   	
@@ -18,19 +18,20 @@ include("AMP/BaseDB.php");
 include("AMP/BaseTemplate.php");
 include("AMP/BaseModuleIntro.php");
 
-if ($_GET[linktype] ) {
-	$sql = "SELECT l. * , t.name FROM links l, linksreltype r, linktype t WHERE l.id = r.linkid AND l.linktype = t.id AND r.typeid =$_GET[linktype] AND l.publish = '1' order by t.name asc, l.linkname asc";
-	}
-	else  { 
+if (isset($_GET['linktype']) && $_GET['linktype']) {
+	$sql = "SELECT l. * , t.name FROM links l, linksreltype r, linktype t WHERE l.id = r.linkid AND l.linktype = t.id AND r.typeid=" . $dbcon->qstr($_GET['linktype']) . " AND l.publish = '1' order by t.name asc, l.linkname asc";
+} else { 
 	$sql = "SELECT l. * , t.name FROM links l, linktype t WHERE  l.linktype = t.id  AND l.publish = '1' AND t.publish = '1' order by t.name asc, l.linkname asc";
 }
 
 $links=$dbcon->CacheExecute($sql) or DIE($dbcon->ErrorMsg());
 
-if ($_GET[linktype]) { 
-	$linkt=$dbcon->CacheExecute("select type from articletype where id = $_GET[linktype]") or DIE($dbcon->ErrorMsg());
+if (isset($_GET['linktype']) && $_GET['linktype']) { 
+	$linkt=$dbcon->CacheExecute("select type from articletype where id=" . $dbcon->qstr($_GET['linktype'])) or DIE($dbcon->ErrorMsg());
 	echo "<p class=title>".$linkt->Fields("type")."</p><br>";
 }
+
+if (!isset($curType)) $curType = null;
  
 while  (!$links->EOF) { 
    if (strtolower(trim($links->Fields("name"))) != $curType) {
