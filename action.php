97fb0b005970adcba8f4fcd2805761a05d79afaf
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-05-20 18:44:52 +0000

    testing
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@957 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- action.php
+++ action.php
@@ -1,10 +1,10 @@
 <?php
 
-$mod_id = 62;
-$modid = 21;
+$mod_id = 7;
+//$modid = 21;
 include("AMP/BaseDB.php");
 include("AMP/BaseTemplate.php");
-//include("AMP/BaseModuleIntro.php");
+include("AMP/BaseModuleIntro.php");
 
 
 include("AMP/DBfunctions.inc.php"); 
