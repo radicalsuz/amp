f8f1fa01136f7a3a8ec6a3cd669c716fb35de95a
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-05-25 05:52:14 +0000

    Includes debugging, most annoying
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@985 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/AMP/BaseModuleIntro.php
+++ include/AMP/BaseModuleIntro.php
@@ -23,7 +23,7 @@ if (isset($mod_id) || isset($intro_id)) {
 		}
         $AMP_Module_Intro = $module->Fields("test");
 		if ($AMP_Module_Intro != (NULL)) {
-            print $AMP_Module_Intro;
+            print 'heh - intro no work';
             $AMP_Module_Intro = eval_includes ( $AMP_Module_Intro );
 			echo '<p class="text">';
 			if  ($module->Fields("html")) {
