c6796908270885644572d2ec8323447ad8ab2a5a
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-05-26 01:48:10 +0000

    testing auto-publish
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1000 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ include/Modules/UDM/AMPPublish/Save.inc.php
@@ -0,0 +1,28 @@
+<?php
+
+/* * * * * * *
+ *  AMP Publish Plugin
+ *  Automatically publishes a record when it is saved
+ *  only works from the user side
+ *
+ *  Author: austin@radicaldesigns.org
+ *  5/25/2005
+ */
+
+class UserDataPlugin_Save_AMPPublish extends UserDataPlugin {
+    
+    var $name = 'AutoPublish';
+    var $available = true;
+
+    function UserDataPlugin_Save_AMPPublish (&$udm, $plugin_instance=null) {
+        $this->init ($udm, $plugin_instance);
+    }
+
+    function execute() {
+        if (!isset ( $this->udm->uid ) return false;
+        if ($this->udm->admin) return false;
+        
+        $sql = "update userdata set publish=1 where id=".$this->udm-uid;
+        $this->dbcon->Execute($sql);
+    }
+}
