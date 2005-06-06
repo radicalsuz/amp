dfb1651ffec39df3d786a7c92d1d62646265048e
margot <margot@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-06-06 18:41:38 +0000

    and again .margot
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1046 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/AMP/Article/article.inc.php
+++ include/AMP/Article/article.inc.php
@@ -80,7 +80,7 @@ if ($Recordset1->Fields("picuse") == (1)) {
 	echo ($Recordset1->Fields("alignment") == "left") ? "left" : "right";
 	echo '" cellpadding="0" cellspacing="0"><tr><td>';
 	#set image
-	echo '<a href="'. $pathtoimg .'" target="_blank"> <img src="' . $pathtoimg . '" alt="' . $Recordset1->Fields("alttag") . '" hspace="4" vspace="4" border="0" class="img_main"></a>';
+	echo '<a href="'. $pathtoimg2 .'" target="_blank"> <img src="' . $pathtoimg . '" alt="' . $Recordset1->Fields("alttag") . '" hspace="4" vspace="4" border="0" class="img_main"></a>';
 	#set caption
 	echo '</td></tr><Tr align="center"><td width="' .  $pwidth  . '" class="photocaption">' . $Recordset1->Fields("piccap") . '</td>';
 	echo '</TR></table>';
