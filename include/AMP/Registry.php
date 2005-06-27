ef262b49fd87592f2202f273e785e499af83ceb1
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-06-27 23:38:17 +0000

    implementing the registry pattern as described at http://www.phppatterns.com/index.php/article/articleview/75/1/1/
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1109 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ include/AMP/Registry.php
@@ -0,0 +1,53 @@
+<?php
+
+define('AMP_REGISTRY_TEMPLATE','TEMPLATE');
+//define('AMP_REGISTRY_DB','DB'); 
+
+//based on the Registry pattern described at
+//http://www.phppatterns.com/index.php/article/articleview/75/1/1/
+class AMP_Registry {
+    var $_cache_stack;
+    
+    function AMP_Registry() {
+        $this->_cache_stack = array(array());
+    }
+    function setEntry($key, &$item) {
+        $this->_cache_stack[0][$key] = &$item;
+    }
+    function &getEntry($key) {
+        return $this->_cache_stack[0][$key];
+    }
+    function isEntry($key) {
+        return ($this->getEntry($key) !== null);
+    }
+    function &instance() {
+        static $registry = false;
+        if (!$registry) {
+            $registry = new AMP_Registry();
+        }
+        return $registry;
+    }
+
+	//save and restore are for testing purposes
+    function save() {
+        array_unshift($this->_cache_stack, array());
+        if (!count($this->_cache_stack)) {
+            trigger_error('Registry lost');
+        }
+    }
+    function restore() {
+        array_shift($this->_cache_stack);
+    }
+
+	//convenient accessor methods
+	function setTemplate(&$template) {
+		$registry =& $this->instance();
+		return $this->setEntry(AMP_REGISTRY_TEMPLATE, $template);
+	}
+
+	function &getTemplate() {
+		$registry =& $this->instance();
+		return $registry->getEntry(AMP_REGISTRY_TEMPLATE);
+	}
+}
+?>
