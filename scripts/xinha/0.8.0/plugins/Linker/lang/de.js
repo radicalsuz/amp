4993ab95700946b5a5d0e8b278273188ded3e2a8
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-07-15 01:39:00 +0000

    Xinha now copied from vendor folder
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1121 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ scripts/xinha/0.8.0/plugins/Linker/lang/de.js
@@ -0,0 +1,8 @@
+// I18N constants
+
+// LANG: "de", ENCODING: UTF-8
+// translated: Udo Schmal
+{
+  "You must select some text before making a new link.": "Sie müssen einen Text markieren um einen Link zu erstellen",
+  "Are you sure you wish to remove this link?": "Wollen Sie diesen Link wirklich entfernen?"
+}
