23d4292612fb51d39b176dd757b05cddb6e3042d
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-07-15 01:50:52 +0000

    i messed up
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1122 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- scripts/xinha/0.8.0/plugins/Linker/lang/de.js
+++
@@ -1,8 +0,0 @@
-// I18N constants
-
-// LANG: "de", ENCODING: UTF-8
-// translated: Udo Schmal
-{
-  "You must select some text before making a new link.": "Sie müssen einen Text markieren um einen Link zu erstellen",
-  "Are you sure you wish to remove this link?": "Wollen Sie diesen Link wirklich entfernen?"
-}
