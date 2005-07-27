35fed6d19d0bcb5ff942b20f8e5456db601a3a9d
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-07-27 05:49:59 +0000

    controllers for schedule
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1162 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- system/schedule_item.php
+++ system/schedule_item.php
@@ -6,14 +6,6 @@ require_once("Modules/Schedule/Item/ComponentMap.inc.php");
 $map = &new ComponentMap_ScheduleItem();
 
 $page = &new AMPSystem_Page ($dbcon, $map);
-/*
-$page->setIncludeFile( "Modules/Schedule/List.inc.php", "list" );
-$page->setIncludeFile( "Modules/Schedule/Form.inc.php", "form" );
-$page->setIncludeFile( "Modules/Schedule/Item.inc.php", "source" );
-$page->setComponentName( "ScheduleItem_Form", "form" );
-$page->setComponentName( "Schedule_List", "list" );
-$page->setComponentName( "ScheduleItem", "source" );
-*/
 
 if (isset($_GET['action']) && $_GET['action'] == "list")  $page->showList( true );
 
