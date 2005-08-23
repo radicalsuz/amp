e6a203b7e3a867b8e052bfcf07bf9b56b635caa8
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-08-23 19:07:47 +0000

    adodb Replace does, in fact, work like i thought it should new update method
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1236 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/Modules/Calendar/Feeds/CalendarFeeds.php
+++ include/Modules/Calendar/Feeds/CalendarFeeds.php
@@ -4,7 +4,6 @@ require_once ("AMP/System/Data/Item.inc.php");
 
 class CalendarFeeds extends AMPSystem_Data_Item {
 
-//	var $datatable = "px_feeds";
 	var $datatable = "calendar_feeds";
 	var $types;
 
@@ -26,17 +25,35 @@ fof_add_feed, then rss_fetch, cause it'll be cached, right?  how to test this...
 	*/
 	function save() {
 		$data = $this->getData();
+
 		$url = $data['url'];
 		if(substr($url, 0, 7) != 'http://' && substr($url, 0, 8) != 'https://')
 		{
 			$url = 'http://' . $url;
 		}
 
+		$id = $data['id']?$data['id']:'';
+
+		return $this->subscribe($url, $id);
+	}
+
+	function subscribe($url, $id=null) {
 		$rss = fetch_rss($url);
+
 		if(!$rss->channel && !$rss->items) {
-			$this->calendar->error="URL is not RSS or is invalid";
+			$this->error="URL is not RSS or is invalid";
 			return false;
 		}
+
+		$feed = array('id'=>$id,'url'=>$url,'title'=>$rss->channel['title'],
+											 'link' =>$rss->channel['link'],
+											 'description'=>$rss->channel['description']);
+		$result = $this->dbcon->Replace( 'calendar_feeds', $feed, 'id', true );
+		if(!$result) {
+			$this->error="Could not save feed";
+			return false;
+		}
+		if ($result == ADODB_REPLACE_INSERTED ) $id = $this->dbcon->Insert_ID();
 
 		$num_events = 0;
 		foreach($rss->items as $item) {
@@ -54,86 +71,41 @@ fof_add_feed, then rss_fetch, cause it'll be cached, right?  how to test this...
 							  'location'=>$event['location'],
 							  'date'=>$event['startdate'],
 							  'enddate'=>$event['enddate'],
-							  'typeid'=>$type);
+							  'typeid'=>$type,
+							  'feed_id'=>$id);
 
-			$result = $this->dbcon->Replace( 'calendar', $calendar, 'url', true );
-/* ok, so Replace doesn't do what it seems to say it does in the docs, so this
-   is how i think it should work, at least for this application.  stole this
-   straight outta adodb-lib.inc.php */
-
-/*
-			$first = true;
-			foreach($calendar as $key => $value) {
-				if (!is_numeric($value) and strncmp($value,"'",1) !== 0 and strcasecmp($value,'null')!=0) {
-					$value = $this->dbcon->qstr($value);
-					$calendar[$key] = $value;
-            }
-				if ($first) {
-					$first = false;
-					$uSet = "$key=$value";
-				} else
-					$uSet .= ",$key=$value";
-			}
-				
-			$update = "UPDATE calendar SET $uSet WHERE url=$calendar[url]";
-print "about to execute $update<br/>";
-			$rs = $this->dbcon->Execute($update);
-var_dump($rs);
-print $this->dbcon->ErrorMsg();
-
-			if($rs) continue;
-print "update failed, let's try an insert<br/>";
-
-			$first = true;
-			foreach($calendar as $key => $value) {
-				if($first) {
-					$first = false;
-					$iCols = "$key";
-					$iVals = "$value";
-				} else {
-					$iCols .= ",$key";
-					$iVals .= ",$value";
-				}
-			}
-			$insert = "INSERT INTO calendar ($iCols) VALUES ($iVals)";
-print "about to execute $insert<br/>";
-			$rs = $this->dbcon->Execute($insert);
-print $this->dbcon->ErrorMsg();
-*/
+			$result = $this->dbcon->Replace( 'calendar', $calendar, array('feed_id', 'url'), true );
 		}
 
 		if(!$num_events) {
-			$this->calendar->error="Feed did not contain event information!";
+			$this->error="Feed did not contain event information!";
 			return false;
 		}
 
-		$id = $data['id']?$data['id']:'';
-		$feed = array('id'=>$id,'url'=>$url,'title'=>$rss->channel['title'],
-											 'link' =>$rss->channel['link'],
-											 'description'=>$rss->channel['description']);
-		$result = $this->dbcon->Replace( 'calendar_feeds', $feed, 'id', true );
-		if ($result == ADODB_REPLACE_INSERTED ) $this->id = $this->dbcon->Insert_ID();
-
-//		fof_update_feed($url, 0);
 
 		return true;
 	}
-
-/*
-	function readData( $id ) {
-//		$feed = fof_feed_row( $id );
-		if ($feed) {
-			$this->setData( $feed );
-			$this->items = fof_get_items( $id );
-			return true;
-		}
-	}
-*/
 
 	function deleteData( $id ) {
 		$result = $this->dbcon->Execute("DELETE from calendar WHERE feed_id=$id");
 		$result = $this->dbcon->Execute("DELETE from calendar_feeds where id = $id");
 		return $result;
+	}
+
+	function update() {
+		$sql = "SELECT url, id FROM calendar_feeds";
+		$feeds = $this->dbcon->Execute($sql);
+		if(!$feeds) {
+			return false;
+		}
+		while(!$feeds->EOF) {
+			$this->subscribe($feeds->Fields('url'), $feeds->Fields('id'));
+			$feeds->MoveNext();
+		}
+
+		$this->dbcon->CacheFlush();
+
+		return true;
 	}
 }
 ?>
