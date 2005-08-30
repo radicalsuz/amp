5f5b3e1f5e560f7dc17c381b9e4f39b51f151086
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-08-30 02:45:42 +0000

    subclassed to allow for vCard nestedness
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1267 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ include/RSSWriter/AMP_RSSWriter.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once('RSSWriter/rss10.inc');
+
+class AMP_RSSWriter extends RSSWriter {
+
+	function AMP_RSSWriter($uri, $title, $description, $meta=array()) {
+		parent::RSSWriter($uri, $title, $description, $meta);
+	}
+
+	function items() {
+		foreach ($this->items as $item) {
+			print "  <item rdf:about=\"" .  htmlspecialchars($item["uri"]) . "\">\n";
+			foreach ($item as $key => $value) {
+				if ($key!="uri") {
+					if (is_array($value)) {
+						$keys = array_keys($value);
+						if (strpos($keys[0], ':' ) !== false) {
+							print "    <${key}>\n";
+							foreach ($value as $k1 => $v1) {
+								print "      <${k1}>" . htmlspecialchars($v1) . "</${k1}>\n";
+							}
+							print "    </${key}>\n";
+						} else {
+							foreach ($value as $v1) {
+								print "    <${key}>" . htmlspecialchars($v1) . "</${key}>\n";
+							}
+						}
+					} else {
+						print "    <${key}>" . htmlspecialchars($value) . "</${key}>\n";
+					}
+				}
+			}
+			print "  </item>\n\n";
+		}
+	}
+}
