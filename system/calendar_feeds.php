fc635980b416183b0b7200642696fc68aa77ffc0
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-09-07 23:23:42 +0000

    feed updating
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1287 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- system/calendar_feeds.php
+++ system/calendar_feeds.php
@@ -1,11 +1,24 @@
 <?php
 
 require_once('AMP/System/Page.inc.php');
+require_once('Modules/Calendar/Feeds/CalendarFeeds.php');
+//require_once("feedonfeeds-0.1.9/init.php");
+require_once('magpierss/rss_fetch.inc');
+
 require_once('Modules/Calendar/Feeds/ComponentMap.inc.php');
-require_once("feedonfeeds-0.1.9/init.php");
 
 $map =& new ComponentMap_CalendarFeeds();
 $page =& new AMPSystem_Page( $dbcon, $map );
+
+if (isset($_GET['action']) && $_GET['action'] == "update") {
+	$feeds =& new CalendarFeeds( $dbcon );
+	if($feeds->update()) {
+		$page->setMessage('Updated feeds');
+	} else {
+		$page->setMessage('Could not update feeds', $is_error = true);
+	}
+	$page->showList( true );
+}
 
 if (isset($_GET['action']) && $_GET['action'] == "list") {
 	$page->showList( true );
