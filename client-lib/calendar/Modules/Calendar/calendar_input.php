6d1cac8bb58a4b44d646c8192e3ddcd797bdcdc5
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-09-14 17:36:53 +0000

    added form default redirect
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1329 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- client-lib/calendar/Modules/Calendar/calendar_input.php
+++ client-lib/calendar/Modules/Calendar/calendar_input.php
@@ -1,3 +1,4 @@
 <?php
-ampredirect("modinput4.php?modin=198");
+if (!defined( AMP_CALENDAR_ENTRY_FORM_DEFAULT )) define( 'AMP_CALENDAR_ENTRY_FORM_DEFAULT', 50);
+ampredirect("modinput4.php?modin=".AMP_CALENDAR_ENTRY_FORM_DEFAULT);
 ?>
