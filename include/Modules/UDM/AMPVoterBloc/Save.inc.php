bd8b656a33588e326b47a63f8e0cc7e8400436af
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-10-03 01:49:11 +0000

    bloc save plugin
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1364 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ include/Modules/UDM/AMPVoterBloc/Save.inc.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once('Modules/VoterGuide/VoterGuide.php');
+require_once('Modules/UDM/DIA/SupporterSave.inc.php');
+
+class UserDataPlugin_Save_AMPVoterBloc extends UserDataPlugin_SupporterSave_DIA {
+
+	function UserDataPlugin_Save_AMPVoterBloc(&$udm, $plugin_instance) {
+		$this->init($udm, $plugin_instance);
+	}
+
+	function _register_fields_dynamic() {
+		if(isset($_REQUEST['guide'])) {
+			$guide =& new VoterGuide($this->dbcon, $_REQUEST['guide']);
+			$bloc_id = $guide->getBlocID();
+			$this->fields['bloc_id'] = array(
+				'type' => 'hidden',
+                'public' => true,
+                'enabled' => true,
+                'default' => $bloc_id,
+                'required' => true,
+                'values'  => $bloc_id
+			);
+		}
+	}
+
+	function getSaveFields() {
+		return array_merge(parent::getSaveFields(), array('bloc_id'));
+	}
+
+	function save($data) {
+		$voter = parent::save($data);
+		VoterGuide::addVoterToBloc($voter, $data['bloc_id']);
+	}
+}
+?>
+		
+		
+
+		
