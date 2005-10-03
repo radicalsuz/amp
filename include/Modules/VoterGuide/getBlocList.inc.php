e218fe4b7ee3868426909c573899c04bb6875484
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-10-03 02:32:13 +0000

    typo
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1367 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/Modules/VoterGuide/getBlocList.inc.php
+++ include/Modules/VoterGuide/getBlocList.inc.php
@@ -6,7 +6,7 @@ require_once('Modules/VoterGuide/VoterGuide.php');
 $guide_id = $_REQUEST['id'];
 $guide =& new VoterGuide(AMP_Registry::getDbcon(), $guide_id);
 
-print DIA_ORGANIZATION_SHORT_NAME.'+'$guide->getShortName().'-'.$guide->getBlocID().
+print DIA_ORGANIZATION_SHORT_NAME.'+'.$guide->getShortName().'-'.$guide->getBlocID().
 		'@lists.democracyinaction.org';
 
 ?>
