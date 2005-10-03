8f983b084c9620a0e52c648ed1c3e483d2945531
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-10-03 02:25:38 +0000

    bloc list mailto intro text accessor
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1366 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ include/Modules/VoterGuide/getBlocList.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('AMP/Registry.php');
+require_once('Modules/VoterGuide/VoterGuide.php');
+
+$guide_id = $_REQUEST['id'];
+$guide =& new VoterGuide(AMP_Registry::getDbcon(), $guide_id);
+
+print DIA_ORGANIZATION_SHORT_NAME.'+'$guide->getShortName().'-'.$guide->getBlocID().
+		'@lists.democracyinaction.org';
+
+?>
