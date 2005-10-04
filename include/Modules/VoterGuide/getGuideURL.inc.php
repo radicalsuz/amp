f6606576ae149b08b85a51e0c92449beaca1d49a
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-10-04 00:33:33 +0000

    try that again
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1387 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/Modules/VoterGuide/getGuideURL.inc.php
+++ include/Modules/VoterGuide/getGuideURL.inc.php
@@ -6,7 +6,7 @@ require_once('Modules/VoterGuide/VoterGuide.php');
 $currentPage =& AMPContent_Page::instance();
 $guide =& $currentPage->getObject(strtolower('UserDataPlugin_Save_AMPVoterGuide'));
 if($guide) {
-	print $_SERVER['PHP_SELF'] . $guide->getURL();
+	print $_SERVER['SERVER_NAME'] . $guide->getURL();
 } else {
 	trigger_error('no guide for getURL');
 }
