faa492de4d462f45e841cfd7f26e5513bad9e400
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-10-06 10:15:18 +0000

    rename getGuidePublicURL (stupid) lookup city, state, name, id
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1402 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/Modules/VoterGuide/Lookups.inc.php
+++ include/Modules/VoterGuide/Lookups.inc.php
@@ -16,19 +16,31 @@ class AMPSystemLookup_VoterGuideByBlocID extends AMPSystem_Lookup {
     var $result_field = "id";
 	var $id_field = "bloc_id";
 
-    function AMPSystemLookup_VoterGuideByBlocID() {
+    function AMPSystemLookup_VoterGuideByBlocID($bloc_id) {
         $this->init();
     }
 }
 
 class AMPSystemLookup_VoterGuideByBlocIDInPost extends AMPSystem_Lookup {
     var $datatable = "voterguides";
-    var $result_field = "id";
-	var $id_field = "bloc_id";
+    var $id_field = "id";
 
     function AMPSystemLookup_VoterGuideByBlocID() {
-		$this->criteria = "where bloc_id = " . $_REQUEST['bloc_id'];
+		$this->criteria = "bloc_id = " . $_REQUEST['bloc_id'];
         $this->init();
     }
+
+	function init() {
+		parent::init();
+		$this->dataset = $this->dataset[0];
+	}
 }
+
+class AMPSystemLookup_LocationsNames extends AMPSystem_Lookup {
+	var $datatable = "voterguides";
+	var $id_field = "id";
+	var $result_field = "state, city, name";
+	var $sortby = "state, city, name";
+}
+
 ?>
