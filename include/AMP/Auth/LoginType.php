6f3b899ff1964d8b5bb57ae22b7efeedb1817588
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-10-07 12:02:42 +0000

    Actual checkin of LoginType abstractions
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1416 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ include/AMP/Auth/LoginType.php
@@ -0,0 +1,104 @@
+<?php
+
+class AMP_Authentication_LoginType {
+
+    var $_dbcon;
+
+    var $_loginScreen = 'AMP/Auth/LoginScreen.inc.php';
+    var $_login_username_field = "AMPLogin_username";
+    var $_login_password_field = "AMPLogin_password";
+
+    var $_cookie_name;
+    var $_timeout = 43200;					// default timeout of 12 hours.
+
+    var $_loginScreenText;
+
+    var $_fieldTemplate = "<label for=\"%1\$s\">%2\$s</label> <input type=\"%3\$s\" name=\"%1\$s\" />";
+    var $_formFields = array(
+        'AMPLogin_username' => 
+            array( 'label' => 'Username:', 'type' => 'text'),
+        'AMPLogin_password' => 
+            array( 'label' => 'Password:', 'type' => 'password')
+            );
+
+
+    function AMP_Authentication_LoginType ( &$handler ) {
+        $this->init( $handler );
+    }
+
+    function init( &$handler ){
+        $this->_dbcon = &$handler->dbcon;
+        $this->_handler = &$handler;
+    }
+
+    function check_authen_credentials() {
+
+        // First check for an existing authentication token.
+        if (isset($_COOKIE[ $this->_cookie_name ]))
+            return $this->_handler->check_cookie($_COOKIE[ $this->_cookie_name ]);
+
+        if (isset($_REQUEST[ $this->_login_username_field ]) || isset($_SERVER['PHP_AUTH_USER']))
+            return $this->_handler->check_password();
+
+        return false;
+
+    }
+
+    function submittedUser( ) {
+        if (!isset($_REQUEST[ $this->_login_username_field ])) return false;
+        return $_REQUEST[ $this->_login_username_field ];
+    }
+
+    function submittedPassword( ) {
+        if (!isset($_REQUEST[ $this->_login_password_field ])) return false;
+        return $_REQUEST[ $this->_login_password_field ];
+    }
+
+    function validateUser( $username, $password ){
+        // abstract
+        trigger_error( 'No login type defined');
+    }
+
+    function showLoginScreen( ) {
+        include( $this->_loginScreen );
+    }
+
+    function getCookieName( ){
+        return $this->_cookie_name;
+    }
+
+    function getTimeout( ){
+        return $this->_timeout;
+    }
+
+    function setTimeout( $timeout_seconds ) {
+        if ( !is_int( $timeout_seconds)) return false;
+        $this->_timeout = $timeout_seconds;
+    }
+
+    function getHelpLinks() {
+        return sprintf( AMP_TEXT_LOGIN_HELP_ADMIN, AMP_SITE_ADMIN );
+    }
+
+    function getFormFields( ){
+        $output = "";
+        foreach( $this->_formFields as $fieldkey => $formField) {
+            $output .= sprintf( $this->_fieldTemplate, $fieldkey, $formField['label'], $formField['type']);
+        }
+        return $output;
+    }
+    function getFieldnamePassword( ) {
+        return $this->_login_password_field;
+    }
+    function getAuthFieldnames( ) {
+        return array( $this->_login_username_field, $this->_login_password_field);
+    }
+    function clearAuthFields( ) {
+        $postFields = $this->getAuthFieldnames( );
+        foreach( $postFields as $authfield ) {
+            unset( $_POST[ $authfield ]);
+        }
+    }
+
+}
+?>
---
+++ include/AMP/Auth/LoginType/Content.inc.php
@@ -0,0 +1,29 @@
+<?php
+if ( !defined( 'AMP_FORM_ID_CONTENT_LOGIN')) define( 'AMP_FORM_ID_CONTENT_LOGIN', 20 );
+require_once( 'AMP/Auth/LoginType.php');
+
+class AMP_Authentication_LoginType_Content extends AMP_Authentication_LoginType {
+    var $_cookie_name = "AMPUserLoginCredentials";
+    var $_loginScreenText = ' User Login';
+
+    function AMP_Authentication_LoginType_Content ( &$handler ){
+        $this->init( $handler );
+    }
+
+    function validateUser( $username, $password ){
+        $user_sql = "SELECT id, custom1 as username, custom2 as password, custom3 as permission FROM userdata WHERE modin = ". AMP_FORM_ID_CONTENT_LOGIN." and publish=1 and custom1=" . $this->_dbcon->qstr( $username ) . " ORDER BY custom3 DESC";
+        $authdata = $this->_dbcon->GetRow( $user_sql );
+
+        if ($this->_handler->validate_password( $password, $authdata['password'] )) {
+            $this->_handler->setPermissionLevel( $authdata['permission'] );
+            $this->_handler->setUserId( $authdata['id'] );
+            return true;
+        }
+    }        
+
+    function getHelpLinks( ) {
+        return '&nbsp;';
+    }
+
+}
+?>
