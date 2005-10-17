3a91e7812115927e004ca657fa3b8350c4c479ee
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-10-17 18:11:48 +0000

    quiz header sql
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1501 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ sql/3.5.6-quizmodules.sql
@@ -0,0 +1,10 @@
+INSERT INTO `modules` ( `id` , `name` , `userdatamod` , `userdatamodid` , `file` , `perid` , `navhtml` , `publish` , `module_type` )
+VALUES (
+'46', 'Quiz', NULL , NULL , NULL , NULL , NULL , '1', '1'
+);
+
+
+INSERT INTO `moduletext` ( `id` , `title` , `name` , `subtitile` , `test` , `html` , `searchtype` , `date` , `type` , `subtype` , `catagory` , `templateid` , `modid` )
+VALUES (
+'63', 'Quiz', 'Quiz', NULL , NULL , '0', 'quiz.php', '0000-00-00', '1' , '0' ,'0' ,'0' , '46'
+);
---
+++ sql/3.5.6-researchheader.sql
@@ -0,0 +1,4 @@
+INSERT INTO `moduletext` ( `id` , `title` , `name` , `subtitile` , `test` , `html` , `searchtype` , `date` , `type` , `subtype` , `catagory` , `templateid` , `modid` )
+VALUES (
+'62', 'Research', 'Research', NULL , NULL , '0', 'research.php', '0000-00-00', '1' , '0' ,'0' ,'0' , '19'
+);
