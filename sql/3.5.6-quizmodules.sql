0a5c1eadc47f93e440a430611ad0fda191039905
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-10-17 18:29:11 +0000

    quiz header fixes
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1502 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- sql/3.5.6-quizmodules.sql
+++ sql/3.5.6-quizmodules.sql
@@ -1,6 +1,6 @@
 INSERT INTO `modules` ( `id` , `name` , `userdatamod` , `userdatamodid` , `file` , `perid` , `navhtml` , `publish` , `module_type` )
 VALUES (
-'46', 'Quiz', NULL , NULL , NULL , NULL , NULL , '1', '1'
+'46', 'Quiz', NULL , NULL , 'quiz.php' , '103' , NULL , '1', '1'
 );
 
 
@@ -8,3 +8,7 @@ INSERT INTO `moduletext` ( `id` , `title` , `name` , `subtitile` , `test` , `htm
 VALUES (
 '63', 'Quiz', 'Quiz', NULL , NULL , '0', 'quiz.php', '0000-00-00', '1' , '0' ,'0' ,'0' , '46'
 );
+
+INSERT INTO `per_description` ( `id` , `name` , `description` , `publish` )VALUES ('103', 'Quiz', NULL , '1');
+
+INSERT INTO `permission` ( `id` , `groupid` , `perid` )VALUES ('', '1', '103'), ('', '3', '103');
