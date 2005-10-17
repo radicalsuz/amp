2c9ae44e2d90c6792b17c40ef8678db6b113096d
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-10-17 18:10:41 +0000

    quiz header
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1500 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- system/quiz.php
+++ system/quiz.php
@@ -1,15 +1,1 @@
-<?
-require_once("Modules/Quiz/ComponentMap.inc.php");
-require_once("AMP/System/Page.inc.php");
-
-$map = &new ComponentMap_Quiz();
-
-$page = &new AMPSystem_Page ($dbcon, $map);
-if (isset($_GET['action']) && $_GET['action'] == "list")  $page->showList( true );
-
-$page->execute();
-
-print $page->output( );
-
-?>
-
+<?require_once("Modules/Quiz/ComponentMap.inc.php");require_once("AMP/System/Page.inc.php");$modid = 46;$map = &new ComponentMap_Quiz();$page = &new AMPSystem_Page ($dbcon, $map);if (isset($_GET['action']) && $_GET['action'] == "list")  $page->showList( true );$page->execute();print $page->output( );?>
