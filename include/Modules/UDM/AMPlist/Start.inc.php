3c5eabac42ae6fc0fe8ca20d8f9224b0a7c91846
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-11-07 20:01:04 +0000

    modinput4 supports email as default field
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1610 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/Modules/UDM/AMPlist/Start.inc.php
+++ include/Modules/UDM/AMPlist/Start.inc.php
@@ -15,6 +15,9 @@ class UserDataPlugin_Start_AMPlist extends UserDataPlugin {
         PARENT::init( $udm, $plugin_instance );
         $init_blaster_method = 'init'. AMP_MODULE_BLAST;
         if (method_exists( $this, $init_blaster_method) ) $this->$init_blaster_method( $udm, $plugin_instance ) ;
+        if ( isset( $_GET['email']) && email_is_valid( $_GET['email']) && !$this->udm->uid) {
+            $this->udm->setData( array( 'Email'=>$_GET['email']));
+        }
     }
     function initPHPlist( &$udm, $plugin_instance ){
         $save = & $udm->registerPlugin( 'PHPlist', 'Save', $plugin_instance );
