dd7f3ed5a668ceb2bb5119b908ab04cb05e400d5
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-11-07 18:14:31 +0000

    phplist auth fixes
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1604 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- system/email_redirect.php
+++ system/email_redirect.php
@@ -3,9 +3,10 @@ require_once("Connections/freedomrising.php");
 
 
 if (AMP_MODULE_BLAST == 'PHPlist') {
-    $sql ="select password from phplist_admin where loginname ='admin' ";
-    $R=$dbcon->Execute($sql) or DIE($sql.$dbcon->ErrorMsg());
-    $location = "../phplist/admin/?login=admin&password=".$R->Fields("password");
+    //$sql ="select password from phplist_admin where loginname ='admin' ";
+    //$R=$dbcon->Execute($sql) or DIE($sql.$dbcon->ErrorMsg());
+   // $location = "../phplist/admin/?login=admin&password=".$R->Fields("password");
+    $location = "../phplist/admin/";
     ampredirect($location);
 } elseif (AMP_MODULE_BLAST == 'DIA') {
 
