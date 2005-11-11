9709c7f333874827c4959f555d8d5779428c1b80
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-11-11 21:46:52 +0000

    moving DIA tests to DIA library package
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1620 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- test/unit_tests/include/DIA/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('RUNNER', true);
-require_once('GroupTestDIA.php');
-
-$test =& new GroupTestDIA();
-$test->run(new HtmlReporter());
-?>
--- test/unit_tests/include/Modules/VoterGuide/TestVoterGuide.php
+++ test/unit_tests/include/Modules/VoterGuide/TestVoterGuide.php
@@ -3,11 +3,101 @@ require_once( 'unit_tests/config.php' );
 
 require_once( 'AMP/BaseDB.php' );
 require_once( 'Modules/VoterGuide/VoterGuide.php' );
+require_once( 'Modules/VoterGuide/Controller.php' );
 
 class TestVoterGuide extends UnitTestCase {
 
 	var $dbcon;
 	var $guide;
+	var $_request = array (
+  'action' => 'new',
+  'plugin_AMPVoterGuide_id' => '',
+  'plugin_AMPVoterGuide_picture_value' => '',
+  'MAX_FILE_SIZE' => '1048576',
+  'plugin_AMPVoterGuide_filelink_value' => '',
+  'modin' => '200',
+  'First_Name' => 'Staple',
+  'Last_Name' => 'Gunn',
+  'Email' => 'staple@gunn.com',
+  'Phone' => '123 456 7890',
+  'Cell_Phone' => '123 456 7891',
+  'Phone_Provider' => 'verizon',
+  'Work_Phone' => '123 456 7892',
+  'Street' => '123 Sesame St',
+  'Street_2' => 'Apt 1',
+  'City' => 'San Francisco',
+  'State' => 'CA',
+  'Zip' => '94110',
+  'plugin_AMPVoterGuide_name' => 'Testeroo Voter Guide',
+  'plugin_AMPVoterGuide_short_name' => 'testeroo',
+  'plugin_AMPVoterGuide_city' => 'San Francisco',
+  'plugin_AMPVoterGuide_state' => 'CA',
+  'plugin_AMPVoterGuide_election_date' => 
+  array (
+    'd' => '8',
+    'M' => '11',
+    'Y' => '2005',
+  ),
+  'plugin_AMPVoterGuide_blurb' => 'Testeroo Intro',
+  'plugin_AMPVoterGuide_footer' => 'Testeroo Outro',
+  'plugin_AMPVoterGuide_accurate_checkbox' => '1',
+  'plugin_AMPVoterGuide_trust_checkbox' => '1',
+  'btnUdmSubmit' => 'Submit My Voter Guide',
+  'plugin_AMPVoterGuide_voterguidePositions_id' => 
+  array (
+    1 => '',
+    2 => '',
+  ),
+  'plugin_AMPVoterGuide_voterguidePositions_headline' => 
+  array (
+    1 => 'Testeroo First Endorsement Category',
+    2 => 'Testeroo Second Endorsement Category',
+  ),
+  'plugin_AMPVoterGuide_voterguidePositions_item' => 
+  array (
+    1 => 'Testeroo First Endorsement Candidate/Issue',
+    2 => 'Testeroo Second Endorsement Candidate/Issue',
+  ),
+  'plugin_AMPVoterGuide_voterguidePositions_position' => 
+  array (
+    1 => '1',
+    2 => '2',
+  ),
+  'plugin_AMPVoterGuide_voterguidePositions_comments' => 
+  array (
+    1 => 'Testeroo First Endorsement Reason',
+    2 => 'Testeroo Second Endorsement Reason',
+  ),
+  'plugin_AMPVoterGuide_voterguidePositions_voterguide_id' => 
+  array (
+    1 => '',
+    2 => '',
+  ),
+  'plugin_AMPVoterGuide_voterguidePositions_textorder' => 
+  array (
+    1 => '',
+    2 => '',
+  ),
+);
+
+	var $_files = array (
+  'plugin_AMPVoterGuide_picture' => 
+  array (
+    'name' => 'experts_small.gif',
+    'type' => 'image/gif',
+    'tmp_name' => '/var/tmp/phplUJ8pe',
+    'error' => 0,
+    'size' => 5368,
+  ),
+  'plugin_AMPVoterGuide_filelink' => 
+  array (
+    'name' => 'voterguide05 Portland.pdf',
+    'type' => '',
+    'tmp_name' => '',
+    'error' => 2,
+    'size' => 0,
+  ),
+);
 
     function TestVoterGuide () {
         $this->UnitTestCase('AMP VoterGuide Test');
@@ -23,10 +113,17 @@ class TestVoterGuide extends UnitTestCase {
 		unset($this->guide);
 	}
 
+	function testAutoPublish() {
+
+	}
+/*
 	function testGetBlocID() {
 		$guide =& new VoterGuide($this->dbcon);
 		$id = $guide->getBlocGroupIDByName('Smackdown 2005');
 		$this->assertEqual($id, 22092);
+
+		$id = $guide->getBlocGroupIDByName('no such group');
+		$this->assertFalse($id);
 
 		$id = $guide->getBlocGroupIDByName('no such group');
 		$this->assertFalse($id);
@@ -38,6 +135,8 @@ class TestVoterGuide extends UnitTestCase {
 		$this->assertTrue($id);
 		$this->assertFalse($this->guide->isUniqueShortName('california', true));
 	}
+*/
+
 		
 }
 
