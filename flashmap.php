e803d3255a00ec1796b53a71155c7ce0a0f0884a
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-11-16 07:23:02 +0000

    fixes to busted flash maps
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1624 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- flashmap.php
+++ flashmap.php
@@ -3,12 +3,20 @@
 include("AMP/BaseDB.php"); 
 include("AMP/BaseTemplate.php"); 
 require_once("AMP/Geo/Maps.php");
+if ($_GET['map']) {
+	$map = new Maps($dbcon,$_GET['map']);
 
-$map = new Maps($dbcon,$_GET['map']);
-
-echo "<h3>".$map->P['title']."</h3>";
-echo "<p>".$map->P['description']."</p>";
-echo  $map->flash_map();
+	echo "<h3>".$map->P['title']."</h3>";
+	echo "<p>".$map->P['description']."</p>";
+	echo  $map->flash_map();
+} else {
+	$sql = 'select id, name, description from maps ';
+	$M=$dbcon->CacheExecute($sql) or DIE($sql.$dbcon->ErrorMsg());
+	while (!$M->EOF) {
+		echo "<p><a href = 'flashmap.php?map=".$M->Fields("id")."'>".$M->Fields('name')."</a>  ".$M->Fields("description")."</p>";
+		$M->MoveNext();
+	}				
+}
 
 require_once("AMP/BaseFooter.php");
 ?>
