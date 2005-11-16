67e162fcd2fe5c23c7baa40b3d77abc7a1206996
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-11-16 20:13:37 +0000

    report bug fixes
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1631 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- system/sendfax_report.php
+++ system/sendfax_report.php
@@ -11,12 +11,20 @@ $R= $dbcon->Execute($sql)or DIE($sql.$dbcon->ErrorMsg());
 $sql = "select title from action_text where id = ".$_REQUEST['report']	;
 $N= $dbcon->Execute($sql)or DIE($sql.$dbcon->ErrorMsg());
 
-#$sql = "select distinct MONTH(date) from action history where actionid = ".$_REQUEST['report']	
-#$M= $dbcon->Execute($sql)or DIE($sql.$dbcon->ErrorMsg());
+
 echo "<p>".$N->Fields("title")."</p>";
 echo "Actions Taken: ".$R->Fields("number");
 
+$sql = "select distinct MONTH(date) as month from action_history where actionid = ".$_REQUEST['report']	
+$M= $dbcon->Execute($sql)or DIE($sql.$dbcon->ErrorMsg());
+while (!$M->EOF) {
+	$sql = 'select count(id) as number from action_history where actionid = '.$_REQUEST['report'] .' and MONTH(date) = '. $M->Fields('month')	;
+	$C= $dbcon->Execute($sql)or DIE($sql.$dbcon->ErrorMsg());
+	echo date("M", mktime(0, 0, 0, $M->Fields('month'), 1, 2005)) .": ".$C->Fields("number");
 
+	
+	$M->MoveNext();
+}
 
 include ("footer.php");
 ?>
