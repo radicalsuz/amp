68ec913d549b1f35b240a7249020742713cc99f1
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-12-01 01:18:02 +0000

    fixed apm cannon problem
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1663 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- includes/emaillist_functions.php
+++ includes/emaillist_functions.php
@@ -328,6 +328,8 @@ function isEmail(email) {
 function friendsend($email,$fromemail,$fromname,$subject,$text) {
 	$headers="From: $fromname <$fromemail>\nReply-To: $fromemail";
 	//if (emailisvalid($email)) {
+	   $text = $text.'\n\n\n This message sent to you from '.AMP_SITE_NAME
+.'  '.AMP_SITE_URL;
 		mail ($email,$subject,$text,$headers);
 	//}
 }			
