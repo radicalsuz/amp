9e46fe5000f1aa1efdd8849d7d1ef6804633062e
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-12-01 01:05:33 +0000

    reverted pull quote
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1660 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/AMP/Content/Article/Display.inc.php
+++ include/AMP/Content/Article/Display.inc.php
@@ -60,11 +60,11 @@ class Article_Display extends AMPDisplay_HTML {
             $htmlbody = str_replace( array_keys($hw), array_values($hw), $htmlbody );
         }
 		//insert pull quote
-		if ($pq = &$this->_article->getPullquote() ) {
-			$find = '[-pullquote-]';
-			$replace = '<div class="pullquote">'.nl2br($pq).'</div>';
-			$htmlbody = str_replace( $find, $replace, $htmlbody );
-        }
+		//if ($pq = &$this->_article->getPullquote() ) {
+		//	$find = '[-pullquote-]';
+		//	$replace = '<div class="pullquote">'.nl2br($pq).'</div>';
+		//	$htmlbody = str_replace( $find, $replace, $htmlbody );
+        //}
         return $this->_HTML_bodyText( $htmlbody );
     }
 
