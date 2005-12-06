f8ac8e635408c85e37b502381fb28ce982fc4d00
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-12-06 00:36:50 +0000

    system login into punbb
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1678 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ system/punbb.php
@@ -0,0 +1,25 @@
+<?php
+require_once("AMP/BaseDB.php");
+
+function punbb_set_admin_cookie($user_id=2) {
+	global $dbcon;
+	$sql ='select password from punbb_users where id = '.$user_id;
+	$R=$dbcon->Execute($sql) or DIE($dbcon->ErrorMsg());
+	$password_hash = $R->Fields("password");
+
+	$now = time();
+	$expire = $now + 31536000;	// The cookie expires after a year
+
+	$cookie_name = AMP_DB_NAME.'_punbb_cookie';
+	$cookie_domain = '';
+	$cookie_path = '/';
+	$cookie_secure = 0;
+	$cookie_seed = '744c5f12';
+
+	setcookie($cookie_name, serialize(array($user_id, md5($cookie_seed.$password_hash))), $expire, $cookie_path, $cookie_domain, $cookie_secure);
+}
+punbb_set_admin_cookie();
+ampredirect('../punbb/admin_index.php');
+
+
+?>
