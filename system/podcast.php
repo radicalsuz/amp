f11c91e42250870281cc8f4d68e14c93b41dec65
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2005-12-13 23:10:00 +0000

    formatting change
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1698 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- system/podcast.php
+++ system/podcast.php
@@ -1,1 +1,14 @@
-<?require_once("Modules/Podcast/ComponentMap.inc.php");require_once("AMP/System/Page.inc.php");$modid = 46;$map = &new ComponentMap_Podcast();$page = &new AMPSystem_Page ($dbcon, $map);if (isset($_GET['action']) && $_GET['action'] == "list")  $page->showList( true );$page->execute();print $page->output( );?>
+<?
+require_once("Modules/Podcast/ComponentMap.inc.php");
+require_once("AMP/System/Page.inc.php");
+$modid = 46;
+$map = &new ComponentMap_Podcast();
+
+$page = &new AMPSystem_Page ($dbcon, $map);
+if (isset($_GET['action']) && $_GET['action'] == "list")  $page->showList( true );
+
+$page->execute();
+
+print $page->output( );
+
+?>
