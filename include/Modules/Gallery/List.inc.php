b4b9a53129a7f5c6e026e4111ae1ed6c3aac2363
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2006-01-09 23:42:20 +0000

    bad link for gallery type in admin
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1718 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/Modules/Gallery/List.inc.php
+++ include/Modules/Gallery/List.inc.php
@@ -7,7 +7,8 @@ class Gallery_List extends AMPSystem_List {
     var $name = 'Photo Galleries';
     var $col_headers = array( 'Gallery' => 'galleryname', 'ID'=>'id' );
     var $extra_columns = array( 'Add to Content System'=> 'module_contentadd.php?gallery=' );
-    
+	var $editlink = 'gallery_type.php';    
+
     function Gallery_List ( &$dbcon ){
         $source = &new GallerySet( $dbcon );
         $this->init( $source );
