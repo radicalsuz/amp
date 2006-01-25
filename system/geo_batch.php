620ba0fca9d15540d76c220e1069927775b868cd
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2006-01-25 22:47:44 +0000

    debugging
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1750 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- system/geo_batch.php
+++ system/geo_batch.php
@@ -14,7 +14,7 @@ function get_geo($modin,$geo_field=NULL,$update=NULL){
 		$up_sql = ' and '.$geo_field.' != "" ';
 	}
 	
-	$sql = "select * from userdata where modin =".$modin.$up_sql." limit 50";
+	$sql = "select * from userdata where modin =".$modin.$up_sql." limit 100";
 	$R= $dbcon->Execute($sql)or DIE("Error getting udm data ".$sql.$dbcon->ErrorMsg());
 			$t= 0;
 			$x= 0;
