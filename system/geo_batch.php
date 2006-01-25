b544403173f1e2e28adf38d7a63048a1b2def12b
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2006-01-25 22:00:28 +0000

    inital add
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1733 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ system/geo_batch.php
@@ -0,0 +1,32 @@
+<?php
+
+# function that gets the lat/lng of an address or location
+
+require_once("Connections/freedomrising.php");
+require_once("header.php");
+require_once("AMP/Geo/Geo.php");
+$geo_field = 'custom40';
+$sql = "select * from userdata where modin =".$_REQUEST['modin']." and ".$geo_field." != '' ";
+
+while (!$R->EOF) {	
+	$t++;
+	$geo = new Geo($dbcon);
+	$geo->City =  $R->Fields("City");
+	$geo->State =  $R->Fields("State")
+	$geo->Street = $R->Fields("Street");
+	$geo->Zip = $R->Fields("Zip");
+	$geo->geocoder_getdata();
+	
+	if ( ($geo->lat) && ($geo->lng) ){
+		//$sql = "update userdata set ". $geo_field ." = '".$geo->lat.",".$geo->lng."' where id = " . $R->Field("id");
+		echo $R->Field("id").": ".$geo->lat.$geo->lng."<br>";
+		$x++;
+	} else {
+		$html .= "<a href = 'modinput4_view.php?uid=".."&modin=".$_REQUEST['modin']."'>".$R->Fields("Street")." ".$R->Fields("City")." ".$R->Fields("State")." ".$R->Fields("Zip")." </a><br>";
+	}
+}
+$out = "Found $x address of $t <br><br>Failed Addresses<br> $html";
+echo $out;
+require_once("header.php");
+
+?>
