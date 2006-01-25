71763991db6c58f37a423b767f8aeb6f81f38b32
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2006-01-25 22:37:33 +0000

    debugging
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1747 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- system/geo_batch.php
+++ system/geo_batch.php
@@ -5,33 +5,44 @@
 require_once("Connections/freedomrising.php");
 require_once("header.php");
 require_once("AMP/Geo/Geo.php");
-$geo_field = 'custom40';
-$sql = "select * from userdata where modin =".$_REQUEST['modin'];
-$R= $dbcon->Execute($sql)or DIE("Error getting udm data ".$sql.$dbcon->ErrorMsg());
-		$t= 0;
-		$x= 0;
-while (!$R->EOF) {	
-	$t++;
-	$geo = new Geo($dbcon);
+set_time_limit(0);
+
+function get_geo($modin,$geo_field=NULL,$update=NULL){
+	global $dbcon;
+
+	if ($update) {
+		$up_sql = ' and '.$geo_field.' != "" ';
+	}
 	
-	if ( $R->Fields("Street")  ) {
-		$geo->City =  $R->Fields("City");
-		$geo->State =  $R->Fields("State");
-		$geo->Street = $R->Fields("Street");
-		$geo->Zip = $R->Fields("Zip");
-		$geo->geocoder_getdata();
+	$sql = "select * from userdata where modin ="$modin.$up_sql;
+	$R= $dbcon->Execute($sql)or DIE("Error getting udm data ".$sql.$dbcon->ErrorMsg());
+			$t= 0;
+			$x= 0;
+	while (!$R->EOF) {	
+		$t++;
+		$geo = new Geo($dbcon);
+		
+		if ( $R->Fields("Street")  ) {
+			$geo->City =  $R->Fields("City");
+			$geo->State =  $R->Fields("State");
+			$geo->Street = $R->Fields("Street");
+			$geo->Zip = $R->Fields("Zip");
+			$geo->geocoder_getdata();
+		}
+		if ( ($geo->lat) && ($geo->long) ){
+			//$sql = "update userdata set ". $geo_field ." = '".$geo->lat.",".$geo->lng."' where id = " . $R->Field("id");
+			echo $R->Fields("id").": ".$geo->lat.$geo->long."<br>";
+			$x++;
+		} else {
+			$html .= "<a href = 'modinput4_view.php?uid=".$R->Fields("id")."&modin=".$modin."'>".$R->Fields("Street")." ".$R->Fields("City")." ".$R->Fields("State")." ".$R->Fields("Zip")." </a><br>";
+		}
+		$R->MoveNext();	
 	}
-	if ( ($geo->lat) && ($geo->long) ){
-		//$sql = "update userdata set ". $geo_field ." = '".$geo->lat.",".$geo->lng."' where id = " . $R->Field("id");
-		echo $R->Fields("id").": ".$geo->lat.$geo->long."<br>";
-		$x++;
-	} else {
-		$html .= "<a href = 'modinput4_view.php?uid=".$R->Fields("id")."&modin=".$_REQUEST['modin']."'>".$R->Fields("Street")." ".$R->Fields("City")." ".$R->Fields("State")." ".$R->Fields("Zip")." </a><br>";
-	}
-	$R->MoveNext();	
+	$out = "Found $x address of $t <br><br>Failed Addresses<br> $html";
+	return $out;
 }
-$out = "Found $x address of $t <br><br>Failed Addresses<br> $html";
-echo $out;
+
+echo get_geo($_REQUEST['modin']);
 require_once("header.php");
 
 ?>
