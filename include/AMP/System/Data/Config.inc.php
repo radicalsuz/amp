c4b8314861cf24aa271e90764bdf28f5b9c1767f
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2006-02-22 18:11:24 +0000

    Data config file now required
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1878 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ include/AMP/System/Data/Config.inc.php
@@ -0,0 +1,5 @@
+<?php
+if ( !defined( 'AMP_SORT_ASC' )) define( 'AMP_SORT_ASC', ' ASC');
+if ( !defined( 'AMP_SORT_DESC' )) define( 'AMP_SORT_DESC', ' DESC');
+if ( !defined( 'AMP_SORT_END' )) define( 'AMP_SORT_END', 'zzzzzzzzzzzzzzzz');
+?>
