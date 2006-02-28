7d5feb6ca75b7976c2399b33cd881d88ee5a621b
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2006-02-28 23:43:01 +0000

    bugfix to language config file
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1884 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/AMP/System/Language/Config.php
+++ include/AMP/System/Language/Config.php
@@ -2,6 +2,6 @@
 
 if ( !defined( 'AMP_SITE_LANGUAGE')) define ( 'AMP_SITE_LANGUAGE', 'en' );
 require_once( 'AMP/System/Language/'. AMP_SITE_LANGUAGE . '/Error.php');
-require_once( 'AMP/System/Language/'. AMP_SITE_LANGUAGE . 'Text.php');
+require_once( 'AMP/System/Language/'. AMP_SITE_LANGUAGE . '/Text.php');
 
 ?>
