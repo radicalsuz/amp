c26b873ff748a4ebea2094ce52898ea1a3fdc1bd
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2006-03-27 23:45:59 +0000

    whatever, more includes is fine
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1899 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- voterguide.php
+++ voterguide.php
@@ -3,8 +3,18 @@
 $modid = 109;
 $mod_id = 74;
 
-
+require_once( "AMP/BaseDB.php" );
+require_once( "AMP/UserData/Input.inc.php" );
 require_once( "AMP/Content/Page.inc.php" );
+require_once( "AMP/Content/Map.inc.php" );
+require_once( "Modules/VoterGuide/ComponentMap.inc.php" );
+require_once( "Modules/VoterGuide/Lookups.inc.php" );
+require_once( "AMP/Form/ElementCopierScript.inc.php" );
+
+
+require_once( "Modules/VoterGuide/VoterGuide.php" );
+require_once( "Modules/VoterGuide/Search/Form.inc.php" );
+require_once( "Modules/VoterGuide/SetDisplay.inc.php" );
 require_once( "Modules/VoterGuide/Controller.inc.php" );
 
 $currentPage = &AMPContent_Page::instance();
