8bedd6dfab588b7db5615709e5908450dcaddc50
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2006-04-21 23:05:57 +0000

    make styles public or not
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1973 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- sql/3.5.9-voterguide_cycles.sql
+++ sql/3.5.9-voterguide_cycles.sql
@@ -1,2 +1,3 @@
 ALTER TABLE voterguides add column election_cycle varchar(128);
 ALTER TABLE voterguides add column group_name varchar(64);
+ALTER TABLE voterguide_styles add column `public` tinyint(1);
