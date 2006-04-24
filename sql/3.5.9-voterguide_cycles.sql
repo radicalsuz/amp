dc21f1a233edb3a76c4268f303874b1a5d0c8d74
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2006-04-24 19:52:08 +0000

    longer url's allowed
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1985 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- sql/3.5.9-voterguide_cycles.sql
+++ sql/3.5.9-voterguide_cycles.sql
@@ -1,3 +1,5 @@
 ALTER TABLE voterguides add column election_cycle varchar(128);
 ALTER TABLE voterguides add column group_name varchar(64);
 ALTER TABLE voterguide_styles add column `public` tinyint(1);
+alter table voterguide_styles change column url url varchar(128);
+alter table voterguide_styles change column fullscreen_url fullscreen_url varchar(128);
