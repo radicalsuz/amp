9b2665fb203c230c94d7627fe49617b765101a88
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2006-04-28 00:08:07 +0000

    making this a swiss army knife lib for DIA saving
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@1996 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/Modules/UDM/DIA/Save.inc.php
+++ include/Modules/UDM/DIA/Save.inc.php
@@ -14,20 +14,70 @@ class UserDataPlugin_Save_DIA extends UserDataPlugin_Save {
             ),
 		'user' => array(
             'type'=>'text',
-            'size'=>'5',
+            'size'=>'15',
             'default' => '',
             'available'=>true,
             'label'=>'DIA AMP User Name'
 			),
 		'password' => array(
             'type'=>'text',
-            'size'=>'5',
+            'size'=>'15',
             'default' => '',
             'available'=>true,
             'label'=>'DIA AMP User Password'
-			)
+			),
+        'table1' => array(
+            'type' => 'text',
+            'size' => '15',
+            'default' => 'supporter',
+            'available' => true,
+            'label' => 'DIA table to save to'),
+        'mapping1' => array(
+            'type' => 'textarea',
+            'size' => '3:15',
+            'label' => '<span class="photocaption">mapping.  ex: custom1=<br/>Email_Preference&custom2<br/>=Source_Tracking_Code...</span>',
+            'available' => true),
+        'returned_key1' => array(
+            'type' => 'text',
+            'size' => '15',
+            'label' => 'save returned key to:',
+            'available' => true),
+        'table2' => array(
+            'type' => 'text',
+            'size' => '15',
+            'default' => 'groups',
+            'available' => true,
+            'label' => 'DIA table to save to'),
+        'mapping2' => array(
+            'type' => 'textarea',
+            'size' => '3:15',
+            'label' => '<span class="photocaption">mapping.  ex: custom3=<br/>Group_Name&custom4<br/>=Description...</span>',
+            'available' => true),
+        'returned_key2' => array(
+            'type' => 'text',
+            'size' => '15',
+            'label' => 'save returned key to:',
+            'available' => true),
+        'table3' => array(
+            'type' => 'text',
+            'size' => '15',
+            'default' => 'event',
+            'available' => true,
+            'label' => 'DIA table to save to'),
+        'mapping3' => array(
+            'type' => 'textarea',
+            'size' => '3:15',
+            'label' => '<span class="photocaption">mapping.  ex: custom5=<br/>Event_Name&custom6<br/>=Description...</span>',
+            'available' => true),
+        'returned_key3' => array(
+            'type' => 'text',
+            'size' => '15',
+            'label' => 'save returned key to:',
+            'available' => true),
         );
     var $available = true;
+
+    var $mappings = array('mapping1', 'mapping2', 'mapping3');
 
     var $_field_prefix = 'DIA';
     var $_listfield_template = array(
@@ -50,6 +100,8 @@ class UserDataPlugin_Save_DIA extends UserDataPlugin_Save {
     }
 
 	function _register_fields_dynamic() {
+        if(defined('AMP_UDM_PLUGIN_DIA_SAVE_LISTS_REGISTERED') 
+            && AMP_UDM_PLUGIN_DIA_SAVE_LISTS_REGISTERED) return;
         if( !( $lists = $this->udm->getRegisteredLists( ))) return;
         $this->fields[ 'list_header' ] = $this->_listfield_header;
 
@@ -57,6 +109,7 @@ class UserDataPlugin_Save_DIA extends UserDataPlugin_Save {
             $listField = array( 'label'    => $list_name );
             $this->fields[ 'list_' . $list_id] = $listField + $this->_listfield_template;
         }
+        define('AMP_UDM_PLUGIN_DIA_SAVE_LISTS_REGISTERED', true);
     }
 
     function getSaveFields() {
@@ -67,26 +120,36 @@ class UserDataPlugin_Save_DIA extends UserDataPlugin_Save {
         return array_intersect( $db_fields, $qf_fields );
     }
 
-
     function save ( $data ) {
         $options=$this->getOptions();
 
-		if(!defined('DIA_API_ORGCODE') && isset($options[ 'orgKey' ])) {
-			define('DIA_API_ORGCODE', $options[ 'orgKey' ]);
+        if(!isset($options[ 'orgKey' ]) && defined('DIA_API_ORGCODE')) {
+            $options['orgKey'] = DIA_API_ORGCODE;
+        }
+		if(!isset($options[ 'user' ]) && defined('DIA_API_USERNAME')) {
+            $options['user'] = DIA_API_USERNAME;
 		}
-		if(!defined('DIA_API_USERNAME') && isset($options[ 'user' ])) {
-			define('DIA_API_USERNAME', $options[ 'user' ]);
+		if(!isset($options[ 'password' ]) && defined('DIA_API_PASSWORD')) {
+            $options['password'] = DIA_API_PASSWORD;
 		}
-		if(!defined('DIA_API_PASSWORD') && isset($options[ 'password' ])) {
-			define('DIA_API_PASSWORD', $options[ 'password' ]);
-		}
-
-		$data = $this->translate($data);
-		$data['uid'] = $this->udm->uid;
 
 		$api =& DIA_API::create();
-		$supporter_id = $api->addSupporter( $data[ 'Email'], $data );
-		$this->_supporter_key = $supporter_id;
+        $api->init(array('user'     => $options['user'],
+                         'password' => $options['password'],
+                         'organization_key' => $options['orgKey']));
+
+/*
+
+foreach table: save
+must save supporter key first SO must check to see if supporter table is specified and do that
+
+        foreach($this->mappings as $mapping) {
+            if(isset($options[$mapping])) {
+            }
+        }
+
+*/
+        $supporter_id = $this->addDIASupporter($api, $data);
 
 /*XXX: there is an api for linking with one step in the addSupporter method
   TODO: make that api clearer and use that
@@ -103,10 +166,20 @@ class UserDataPlugin_Save_DIA extends UserDataPlugin_Save {
         return $supporter_id;
     }
 
-	function translate( $data ) {
+    function addDIASupporter(&$api, $data) {
+        $supporter = $this->translate($data);
+        $supporter['uid'] = $this->udm->uid;
+        $this->_supporter_key = $api->addSupporter($supporter);
+        $this->updateReturnKey('supporter', $this->_supporter_key);
+        return $this->_supporter_key;
+    }
+
+	function translate( $data, $table='supporter' ) {
 		$translation = array('region' => 'Region',
 							'occupation' => 'Occupation',
 							'Company' => 'Organization');
+
+//        $translation = array_merge($translation, $this->getMapping($table));
 
 		foreach($data as $key => $value) {
 			if(isset($translation[$key])) {
@@ -118,6 +191,23 @@ class UserDataPlugin_Save_DIA extends UserDataPlugin_Save {
 
 		return $return;
 	}
+
+    function getMapping($table) {
+        $options = $this->getOptions();
+        //XXX:NOT RIGHT
+        foreach($options as $option) {
+            if(!in_array($option, $this->table_options)) continue;
+            if(!($table == $option['value'])) continue;
+        }
+    }
+
+    function extractMapping($string) {
+        $mappings = explode('&',$string);
+        foreach($mappings as $map) {
+            list($key, $value) = explode('=',$map);
+            $return[$key] = $value;
+        }
+    }
 
 	function getSupporterKey() {
 		return $this->_supporter_key;
