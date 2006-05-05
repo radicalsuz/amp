15319281aea5f24f8c96bd81e1b42ea32ffc15ee
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2006-05-05 22:30:50 +0000

    controllers for RSS
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@2026 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- system/module.php
+++ system/module.php
@@ -5,84 +5,8 @@ require_once( 'AMP/System/Page.inc.php');
 
 $map = &new ComponentMap_Tool( );
 $systemPage = &new AMPSystem_Page( $dbcon, $map );
-if (isset($_GET['action']) && $_GET['action'] == "list")  $systemPage->showList( true );
 
 $systemPage->execute( );
 print $systemPage->output( );
 
-/*
-$mod_name="module";
-
-require_once("Connections/freedomrising.php");
-require_once("Connections/sysmenu.class.php");
-$obj = new SysMenu; 
-$buildform = new BuildForm;
-
-$table = "modules";
-$listtitle ="Modules";
-$listsql ="select id, name  from $table  ";
-$orderby =" order by name asc  ";
-$fieldsarray=array( 'Module'=>'name','ID'=>'id'
-					);
-$extra=array('Settings'=>'module_control_list.php?modid=');
-$filename="module.php";
-
-ob_start();
-// insert, update, delete
-if ((($_POST['MM_update']) && ($_POST['MM_recordId'])) or ($_POST['MM_insert']) or (($_POST['MM_delete']) && ($_POST['MM_recordId']))) {
-
-#check auto inc value
-	if ($_POST['MM_insert']) {
-		$id = lowerlimitInsertId('modules',100);
-	}
-
-    $MM_editTable  = $table;
-    $MM_recordId = $_POST['MM_recordId'];
-    $MM_editRedirectUrl = $filename."?action=list";
-	$MM_editColumn = "id";
-    $MM_fieldsStr =
-"id|value|name|value|userdatamod|value|userdatamodid|value|file|value|perid|value|navhtml|value|publish|value|module_type|value";
-    $MM_columnsStr = "id|',none,''|name|',none,''|userdatamod|',none,''|userdatamodid|',none,''|file|',none,''|perid|',none,''|navhtml|',none,''|publish|',none,''|module_type|',none,''";
-	require ("../Connections/insetstuff.php");
-    require ("../Connections/dataactions.php");
-    ob_end_flush();	
-}
-
-if (isset($_GET['id'])) {	$R__MMColParam = $_GET['id']; }
-else {$R__MMColParam = "8000000";}
-
-$R=$dbcon->Execute("SELECT * FROM $table WHERE id = $R__MMColParam") or DIE($dbcon->ErrorMsg());
-$T=$dbcon->Execute("SELECT * FROM module_type ") or DIE($dbcon->ErrorMsg());
-
-
-$rec_id = & new Input('hidden', 'MM_recordId', $_GET['id']);
-//build form
-$html  = $buildform->start_table('name');
-$html .= $buildform->add_header('Add/Edit '.$listtitle, 'banner');
-$html .= addfield('name','Module','text',$R->Fields("name"));
-$tem_options = makelistarray($T,'id','name','Select Module Type');
-$Tem = & new Select('module_type',$tem_options,$R->Fields("module_type"));
-$html .=  $buildform->add_row('Module Type', $Tem);
-$html .= addfield('perid','Permission','text',$R->Fields("perid"));
-$html .= addfield('publish','Publish','checkbox',$R->Fields("publish"));
-$html .= addfield('navhtml','Navigation HTML','textarea',$R->Fields("navhtml"));
-$html .= addfield('file','Default File','text',$R->Fields("file"));
-$html .= addfield('userdatamod','User Data Module','checkbox',$R->Fields("userdatamod"));
-$html .= addfield('userdatamodid','UDM ID','text',$R->Fields("userdatamodid"));
-$html .= addfield('id','ID','text',$R->Fields("id"));
-
-$html .= $buildform->add_content($buildform->add_btn() .'&nbsp;'. $buildform->del_btn().$rec_id->fetch());
-$html .= $buildform->end_table();
-$form = & new Form();
-$form->set_contents($html);
-
-include ("header.php");
-if ($_GET['action'] == "list") {
-	listpage($listtitle,$listsql,$fieldsarray,$filename,$orderby,$sort,$extra);
-}
-else {
-	echo $form->fetch();
-}	
-include ("footer.php");
-*/
 ?>
--- system/rssfeed.php
+++ system/rssfeed.php
@@ -1,6 +1,13 @@
 <?php
-#generic update page
 $modid = "45";
+
+require_once( 'AMP/Content/RSS/Feed/ComponentMap.inc.php');
+
+$map = &new ComponentMap_RSS_Feed( );
+$controller = &$map->get_controller( );
+print $controller->execute( );
+/*
+
 $mod_name = "rss";
 
 require_once("Connections/freedomrising.php");
@@ -93,4 +100,5 @@ else {
 }	
 
 include ("footer.php");
+*/
 ?>
