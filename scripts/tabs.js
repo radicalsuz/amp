df207e832db4aae07d1dc3cde2f05cb99ce48b02
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2006-08-02 05:34:31 +0000

    tabs on the bottom of the article form
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@2206 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -9,9 +9,20 @@ function Tabs_register( item ) {
 	
 }
 
+function Tabs_mirror( item ) {
+	item_mirror = document.getElementById( item.id + '_mirror' );
+	if (item_mirror) {
+		return item_mirror;
+	}
+}
+
 function Tabs_clear() {
 	for (n=0;n<window.Tabs_registeredItems.length;n++){
 		window.Tabs_registeredItems[n].className= window.Tabs_registeredItems_ClassNames[n];
+		item_mirror = Tabs_mirror( window.Tabs_registeredItems[n] );
+		if (item_mirror) {
+			item_mirror.className = window.Tabs_registeredItems_ClassNames[n];
+		}
 	}
 }
 
@@ -20,5 +31,15 @@ function Tabs_highlight( item, highlight_class ) {
 	Tabs_clear();
 	Tabs_register(item);
  	item.className = item.className + ' ' + highlight_class;	
+	item_mirror = Tabs_mirror( window.Tabs_registeredItems[n] );
+	if (item_mirror) {
+		item_mirror.className = item_mirror.className + ' ' + highlight_class;
+	}
+	
+}
+
+function Tabs_highlight_mirror( item_mirror ){
+	target_item = document.getElementById( item_mirror.id.substring(0,5));
+	return Tabs_highlight( target_item );
 }
 
