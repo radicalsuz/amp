8fb2d2699f9fc4d4731abb09957ada6136981960
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2006-10-05 06:02:29 +0000

    speed tests
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@2286 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- system/introtext.php
+++ system/introtext.php
@@ -2,6 +2,12 @@
 require_once("AMP/System/IntroText/ComponentMap.inc.php");
 
 $map = &new ComponentMap_IntroText();
+
+//require_once( 'AMP/System/Page.inc.php');
+//$page = &new AMPSystem_Page( AMP_Registry::getDbcon( ), $map );
+//$page->execute( );
+//print $page->output( );
+
 $controller = &$map->get_controller( );
 
 print $controller->execute( );
