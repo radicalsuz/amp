3b0b9729269867e377640633e8d78c6276aed023
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2007-06-15 12:44:42 +0000

    fixing userdata state lookup default
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@2499 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- sql/3.7.1-userdata_defaults_state_country.sql
+++
@@ -1,2 +0,0 @@
-alter table userdata_fields change column lookup_State lookup_State varchar( 255 ) null DEFAULT 'AMPSystemLookup_Regions_US';
-alter table userdata_fields change column lookup_Country lookup_Country varchar( 255 ) null DEFAULT 'AMPSystemLookup_Regions_World';
---
+++ sql/3.7.2-delimiter_char.sql
@@ -0,0 +1,1 @@
+update userdata_plugins_options set name='delimiter_char' where name='delimiter_type';
