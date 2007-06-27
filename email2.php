35c0955c5ef834a445a6dd0bd0767f7a27bf8ce8
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2007-06-27 19:35:19 +0000

    update to email2.php, redirects to form.php?modin=3 or to redirect set for /email
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@2543 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- email2.php
+++ email2.php
@@ -1,4 +1,21 @@
 <?php 
+
+$mod_id = 20;
+$modid=9;
+include("AMP/BaseDB.php"); 
+
+require_once( 'AMP/Content/Redirect/Redirect.php');
+$redirect = new AMP_Content_Redirect( AMP_Registry::getDbcon( ));
+$target_set = $redirect->find( array( 'alias' => 'email'));
+if ( $target_set ) {
+    $source = current( $target_set  );
+    $new_url = $source->getTarget( );
+} else {
+    $new_url = AMP_url_add_vars( AMP_CONTENT_URL_FORM, array( 'modin=3')) ;
+}
+
+ampredirect( $new_url );
+
 /*********************
 07-02-2003  v3.01
 Module:  email
@@ -11,6 +28,7 @@ To Do:  declare  post vars
 
 *********************/ 
 
+/*
 $mod_id = 20;
 $modid=9;
 include("AMP/BaseDB.php"); 
@@ -130,4 +148,5 @@ mail ( "$sendemail","" ,"" , "From: ".AMPSystem_Email::sanitize($email)."\nX-Mai
 	  include("AMP/BaseModuleIntro.php"); ?>
 <?php } //end thank you
  include("AMP/BaseFooter.php"); 
+ */
 ?>
