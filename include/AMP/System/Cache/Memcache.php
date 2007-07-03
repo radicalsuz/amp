62de58b7ad2080ce4eb332dfa36306c66fd8f335
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2007-07-03 20:19:56 +0000

    move the garbage restart and log code to the bottom, where it can die for all i care
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@2578 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/AMP/System/Cache/Memcache.php
+++ include/AMP/System/Cache/Memcache.php
@@ -53,62 +53,6 @@ class AMP_System_Cache_Memcache extends AMP_System_Cache {
         $this->_memcache_connection = &$connection;
     }
 
-    function _restart_memcached() {
-        if(!defined('MEMCACHED_RC_SCRIPT')) define('MEMCACHED_RC_SCRIPT', '/usr/local/etc/rc.d/memcached.sh');
-        if(!file_exists(MEMCACHED_RC_SCRIPT)) return false;
-
-        $memcache_connection = &new Memcache;
-
-        //just try again
-        $result = $memcache_connection->connect( AMP_SYSTEM_MEMCACHE_SERVER, AMP_SYSTEM_MEMCACHE_PORT );
-        if ( $result ) {
-            $this->set_connection( $memcache_connection );
-            return true;
-        }
-
-        $lock = fopen("/tmp/restart-memcached.lock", "w");
-        flock($lock, LOCK_EX|LOCK_NB, $currently_restarting);
-        if ($currently_restarting) {
-            //if $currently_restarting, then another process has the lock
-            trigger_error(getmypid()." - could not get lock, hopefully someone else is restarting memcached");
-            return false;
-        } else {
-            //we're the first one to try to restart
-            trigger_error('connection to memcached failed, restarting');
-            $stats = $memcache_connection->getStats();
-            $pid = $stats['pid'];
-            trigger_error(getmypid()." - acquired lock, restarting memcached with pid $pid");
-
-            //use the rc.d script to force a restart
-            $ret = exec(MEMCACHED_RC_SCRIPT.' forcerestart',$message,$code);
-            if($code) {
-                trigger_error("memcached restart script ".MEMCACHED_RC_SCRIPT." returned error code: $code");
-                return false;
-            }
-
-            //give it 30 seconds to reconnect
-            $start = 0; 
-            while(!($result = $memcache_connection->connect( AMP_SYSTEM_MEMCACHE_SERVER, AMP_SYSTEM_MEMCACHE_PORT ))) {
-                if(++$start > 30) {
-                    break;
-                }
-                sleep(1);
-            }
-            if ( $result ) {
-                $new_stats = $memcache_connection->getStats();
-                $new_pid = $new_stats['pid'];
-                trigger_error("memcached restarted successfully, new pid is $new_pid");
-            } else {
-                trigger_error('could not reestablish connection after 30 seconds');
-            }
-   
-            //give up the lock
-            flock($lock, LOCK_UN);
-            mail('seth@radicaldesigns.org, austin@radicaldesigns.org', '[AMP] memcached restarted', "stats before restart:\n".print_r($stats, true), 'From: amp@radicaldesigns.org', '-fautomated@radicaldesigns.org');
-        }
-        return $result;
-    }
-
     function has_connection( ){
         return isset( $this->_memcache_connection );
     }
@@ -128,49 +72,6 @@ class AMP_System_Cache_Memcache extends AMP_System_Cache {
         return $cache;
     }
     */
-
-    function log_memcache_failure() {
-        $log = '/tmp/amp-memcache-fails';
-        $memcache_fail_limit = 100;   // 100 failures in
-        $memcache_fail_window = 60*5; // 5 minutes
-        //try to create the file if it doesn't exist
-        if($fh = file_exists($log) ? fopen($log, 'r+') : fopen($log, 'x+')) {
-            if (flock($fh, LOCK_EX|LOCK_NB)) { // do an exclusive lock
-                $AMP_MEMCACHE_FAILS = explode("\n",file_get_contents($log));
-                $AMP_MEMCACHE_FAILS[] = time();
-
-                if(count($AMP_MEMCACHE_FAILS) > $memcache_fail_limit) {
-                    //if there's more than the limit of failures, restart and reset the log
-                    trigger_error( 'requested memcache restart');
-                    $this->_restart_memcached();
-                    $AMP_MEMCACHE_FAILS = array();
-                } else {
-                    //only keep failures that have happened within the window
-                    $current_failures = $AMP_MEMCACHE_FAILS;
-                    $AMP_MEMCACHE_FAILS = array();
-                    foreach( $current_failures as $fail_time ) {
-                        if ( $fail_time < ( time( ) - $memcache_fail_window ) ) {
-                            continue;
-                        }
-                        $AMP_MEMCACHE_FAILS[] = $fail_time;
-                    }
-                    trigger_error( 'returning ' . count( $AMP_MEMCACHE_FAILS ) . ' of ' . count( $current_failures ) . ' recent failures to the log');
-                    //$AMP_MEMCACHE_FAILS = array_filter($AMP_MEMCACHE_FAILS, create_function('$var', 'return $var > time() - '.$memcache_fail_window.';'));
-                }
-
-                //write out the log
-                ftruncate($fh, 0);
-                fwrite($fh, implode("\n",$AMP_MEMCACHE_FAILS));
-                flock($fh, LOCK_UN); // release the lock
-            } else {
-                trigger_error("Couldn't lock memcache fail log at " . $log ); 
-            }
-
-            fclose($fh);
-        } else {
-            trigger_error('could not open memcache fail log at '.$log.' for reading and writing');
-        }
-    }
 
     function add( &$item, $key ){
         $authorized_key = $this->authorize( $key );
@@ -279,6 +180,105 @@ class AMP_System_Cache_Memcache extends AMP_System_Cache {
 
     function failover( ){
         return 'file';
+    }
+
+    function _restart_memcached() {
+        if(!defined('MEMCACHED_RC_SCRIPT')) define('MEMCACHED_RC_SCRIPT', '/usr/local/etc/rc.d/memcached.sh');
+        if(!file_exists(MEMCACHED_RC_SCRIPT)) return false;
+
+        $memcache_connection = &new Memcache;
+
+        //just try again
+        $result = $memcache_connection->connect( AMP_SYSTEM_MEMCACHE_SERVER, AMP_SYSTEM_MEMCACHE_PORT );
+        if ( $result ) {
+            $this->set_connection( $memcache_connection );
+            return true;
+        }
+
+        $lock = fopen("/tmp/restart-memcached.lock", "w");
+        flock($lock, LOCK_EX|LOCK_NB, $currently_restarting);
+        if ($currently_restarting) {
+            //if $currently_restarting, then another process has the lock
+            trigger_error(getmypid()." - could not get lock, hopefully someone else is restarting memcached");
+            return false;
+        } else {
+            //we're the first one to try to restart
+            trigger_error('connection to memcached failed, restarting');
+            $stats = $memcache_connection->getStats();
+            $pid = $stats['pid'];
+            trigger_error(getmypid()." - acquired lock, restarting memcached with pid $pid");
+
+            //use the rc.d script to force a restart
+            $ret = exec(MEMCACHED_RC_SCRIPT.' forcerestart',$message,$code);
+            if($code) {
+                trigger_error("memcached restart script ".MEMCACHED_RC_SCRIPT." returned error code: $code");
+                return false;
+            }
+
+            //give it 30 seconds to reconnect
+            $start = 0; 
+            while(!($result = $memcache_connection->connect( AMP_SYSTEM_MEMCACHE_SERVER, AMP_SYSTEM_MEMCACHE_PORT ))) {
+                if(++$start > 30) {
+                    break;
+                }
+                sleep(1);
+            }
+            if ( $result ) {
+                $new_stats = $memcache_connection->getStats();
+                $new_pid = $new_stats['pid'];
+                trigger_error("memcached restarted successfully, new pid is $new_pid");
+            } else {
+                trigger_error('could not reestablish connection after 30 seconds');
+            }
+   
+            //give up the lock
+            flock($lock, LOCK_UN);
+            mail('seth@radicaldesigns.org, austin@radicaldesigns.org', '[AMP] memcached restarted', "stats before restart:\n".print_r($stats, true), 'From: amp@radicaldesigns.org', '-fautomated@radicaldesigns.org');
+        }
+        return $result;
+    }
+
+    function log_memcache_failure() {
+        $log = '/tmp/amp-memcache-fails';
+        $memcache_fail_limit = 100;   // 100 failures in
+        $memcache_fail_window = 60*5; // 5 minutes
+        //try to create the file if it doesn't exist
+        if($fh = file_exists($log) ? fopen($log, 'r+') : fopen($log, 'x+')) {
+            if (flock($fh, LOCK_EX|LOCK_NB)) { // do an exclusive lock
+                $AMP_MEMCACHE_FAILS = explode("\n",file_get_contents($log));
+                $AMP_MEMCACHE_FAILS[] = time();
+
+                if(count($AMP_MEMCACHE_FAILS) > $memcache_fail_limit) {
+                    //if there's more than the limit of failures, restart and reset the log
+                    trigger_error( 'requested memcache restart');
+                    $this->_restart_memcached();
+                    $AMP_MEMCACHE_FAILS = array();
+                } else {
+                    //only keep failures that have happened within the window
+                    $current_failures = $AMP_MEMCACHE_FAILS;
+                    $AMP_MEMCACHE_FAILS = array();
+                    foreach( $current_failures as $fail_time ) {
+                        if ( $fail_time < ( time( ) - $memcache_fail_window ) ) {
+                            continue;
+                        }
+                        $AMP_MEMCACHE_FAILS[] = $fail_time;
+                    }
+                    trigger_error( 'returning ' . count( $AMP_MEMCACHE_FAILS ) . ' of ' . count( $current_failures ) . ' recent failures to the log');
+                    //$AMP_MEMCACHE_FAILS = array_filter($AMP_MEMCACHE_FAILS, create_function('$var', 'return $var > time() - '.$memcache_fail_window.';'));
+                }
+
+                //write out the log
+                ftruncate($fh, 0);
+                fwrite($fh, implode("\n",$AMP_MEMCACHE_FAILS));
+                flock($fh, LOCK_UN); // release the lock
+            } else {
+                trigger_error("Couldn't lock memcache fail log at " . $log ); 
+            }
+
+            fclose($fh);
+        } else {
+            trigger_error('could not open memcache fail log at '.$log.' for reading and writing');
+        }
     }
 }
 
