b18c80bd9798c4701696b2dd23313f1b309fc7e3
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2007-09-19 18:22:30 +0000

    added support for custom fields to sections
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@2641 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- sql/3.7.2-nav_badges.sql
+++ sql/3.7.2-nav_badges.sql
@@ -1,4 +1,4 @@
+alter table navtbl add column badge_id int( 9 ) null;
+replace into badges ( id, name, include, include_function, publish ) values (  30, "Articles", "AMP/Badge/Articles.php", "amp_badge_articles", 1 );
+replace into badges ( id, name, include, include_function, publish ) values (  29, "Related Forms", "AMP/Badge/RelatedForms.php", "amp_badge_related_form", 1 );
 alter table badges add column include_function_args text null;
-alter table nav add column badge_id int( 9 ) null;
-replace into badges ( id, name, include, include_function, include_function_args, publish ) values (  30, "Articles", "AMP/Badge/Articles.php", "amp_badge_articles", "", 1 );
-replace into badges ( id, name, include, include_function, include_function_args, publish ) values (  29, "Related Forms", "AMP/Badge/RelatedForms.php", "amp_badge_related_form", "", 1 );
---
+++ sql/3.7.3-section_custom.sql
@@ -0,0 +1,4 @@
+alter table articletype add column custom1 text null;
+alter table articletype add column custom2 text null;
+alter table articletype add column custom3 text null;
+alter table articletype add column custom4 text null;
