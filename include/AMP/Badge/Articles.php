f224aea54edea6ed09f3975872656876191c0664
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2007-09-19 18:29:01 +0000

    more flexibility for articles badge
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@2646 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/AMP/Badge/Articles.php
+++ include/AMP/Badge/Articles.php
@@ -1,25 +1,28 @@
 <?php
 
-function amp_badge_articles( $options ) {
+function amp_badge_articles( $options, $display = 'AMP_Content_Badge_Public_Article_List' ) {
     $section = ( isset( $options['section']) && $options['section'] ) ? $options['section'] : false;
     $class = ( isset( $options['class']) && $options['class'] ) ? $options['class'] : false;
     if ( !( $section || $class )) return false; 
 
     $header_output = false;
     $header = ( isset( $options['header'] )) ? $options['header'] : null;
+    $display = ( isset( $options['display']) && $options['display'] ) ? $options['display'] : false;
     $limit = ( isset( $options['limit']) && $options['limit'] ) ? $options['limit'] : false;
+    $morelink = ( isset( $options['morelink']) ) ? $options['morelink'] : null;
+    $suppress_morelink = ( isset( $morelink ) && ( $morelink == 0 ));
 
     $criteria = array( );
     if ( $class ) {
         $criteria['class'] = $class;
-        if ( !isset( $header )) {
+        if ( !isset( $header ) && !is_array( $class )) {
             $class_names = AMP_lookup( 'classes');
             $header =  isset( $class_names[$class]) ? AMP_pluralize( $class_names[$class] ) : false;
         }
     }
     if ( $section ) {
         $criteria['section'] = $section;
-        if ( !isset( $header )) {
+        if ( !isset( $header ) && !is_array( $section )) {
             $section_names = AMP_lookup( 'sectionsLive');
             $header =  isset( $section_names[$section]) ? $section_names[$section] : false;
         }
@@ -30,9 +33,10 @@ function amp_badge_articles( $options ) {
         $header_output = $renderer->span( $header, array( 'class' => AMP_CONTENT_CSS_CLASS_BADGE_HEADER ));
     }
 
-    require_once( 'AMP/Content/Article/Public/List.php');
     require_once( 'AMP/Content/Badge/Public/Article/List.php');
     $list = &new AMP_Content_Badge_Public_Article_List( false, $criteria, $limit );
+    if ( $display ) $list->set_display_method( $display );
+    if ( $suppress_morelink ) $list->suppress( 'pager' );
     return $header_output . $list->execute( );
 
 }
