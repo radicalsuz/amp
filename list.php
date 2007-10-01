2cb7f3c3b0f9100b26e5f56b2a0134d50a9448f2
lattice <lattice@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2007-10-01 20:04:12 +0000

    list works with header and templating
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@2673 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- list.php
+++ list.php
@@ -5,11 +5,15 @@ require_once( 'AMP/Content/Article/Public/ComponentMap.inc.php');
 require_once( 'AMP/Content/Page.inc.php');
 
 $_REQUEST['action'] = 'search';
-$page = AMPContent_Page::instance( );
-$template_section = ( isset( $_REQUEST['template_section']) && $_REQUEST['template_section'] ) ? $_REQUEST['template_section'] : AMP_CONTENT_SECTION_ID_ROOT;
+$page = &AMPContent_Page::instance( );
+$template_section = ( isset( $_REQUEST['template_section']) && $_REQUEST['template_section'] ) ? $_REQUEST['template_section'] : false;
+if (!$template_section) {
+	$template_section = ( isset( $_REQUEST['section']) && $_REQUEST['section'] && is_numeric($_REQUEST['section']) ) ? $_REQUEST['section'] : AMP_CONTENT_SECTION_ID_ROOT;
+}
 $page->setSection( $template_section );
+$map = new ComponentMap_Article_Public();
+$list = $map->getComponent('list');
 
-$map = new ComponentMap_Article_Public( );
 $list = $map->getComponent( 'list');
 $list->suppress( 'search_form' );
 $page->contentManager->add( $list );
