366785da75d4c42a7a090ffe3d54fd9fc4dfd37a
dtaylor <dtaylor@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2007-10-24 22:10:09 +0000

    added international to geo and support for world flash map
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@2713 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/AMP/Geo/Maps.php
+++ include/AMP/Geo/Maps.php
@@ -22,6 +22,7 @@ Class Maps {
 		'line_color'=>'0000ff',
 		'state_info_icon'=>'',
 		'label_field'=>'City',
+		'type'=>'us',
 		'hover_field'=>'',
 		'point_url'=>'',
 		'state_url'=>'',
@@ -84,6 +85,7 @@ Class Maps {
 		$this->prop('span_lat',$R);
 		$this->prop('geo_field',$R);
 		$this->prop('span_long',$R);
+		$this->prop('type',$R);
 		$this->P['table'] = $R->Fields("map_table");
 		if ($R->Fields("type")) {
 			if ($this->P['table'] == 'userdata') {
@@ -187,9 +189,824 @@ Class Maps {
 		#if stale rewrites xml file
 		
 	}
+
+
+
+	function world_xml() {	
+		header('Content-type: text/xml');
+
+		$cache_key = __FILE__.'-'.__FUNCTION__.'-map_ID='.$this->map_ID;
+		$cached_map = AMP_cache_get( $cache_key );
+
+		if ($cached_map ) return $cached_map;
+
+	
+		if (!$this->points) {
+			$this->build_points();
+		}
+	
+		
+        $out = "";
+		$out .= '<?xml version="1.0" encoding="iso-8859-1"?>';
+		$out .= '<countrydata>';
+		$out .= '<state id="default_color"><color>'.$this->P['default_color'].'</color></state>';
+		$out .= '<state id="background_color"><color>'.$this->P['background_color'].'</color></state>';
+		$out .= '<state id="outline_color"><color>'.$this->P['outline_color'].'</color></state>';
+		$out .= '<state id="default_point"><color>'.$this->P['default_point_color'].'</color><size>'.$this->P['default_point_size'].'</size><src>'.$this->P['default_point_src'].'</src></state>';
+		$out .= '<state id="font_size"><data>'.$this->P['font_size'].'</data></state>';
+		$out .= '<state id="state_info_icon"><src>'.$this->P['state_info_icon'].'</src></state>';
+		$out .= '<state id="line_color"><color>'.$this->P['line_color'].'</color></state>';
+		$out .= '<state id="arc_color"><color>'.$this->P['arc_color'].'</color></state>';
+		$out .= '<state id="scale_points"><data>50</data></state>';	
+	
+		if ($this->points) {
+			
+			foreach($this->points as $p) {
+				$out .= "\n<state id=\"point\">";
+				$out .= '<name>' . $p['name'] . '</name>';
+				$out .= '<loc>' . $p['loc'] .'</loc>';
+                if ( isset( $this->P['opacity'])){
+                    $out .= '<opacity>' . $this->P['opacity'] .'</opacity>';
+                }
+				$out .= '<target>' . $this->P['target'] .'</target>';
+
+				if ($p['id']) {
+					$out .= '<url>' .$this->P['point_url']. $p['id'] .'</url>';
+				}
+				if ($p['hover']) {
+					$out .= '<hover>' .$p['hover'] .'</hover>';
+				}
+				$out .= '</state>';
+				
+			}
+		}
+		$out .='
+<state id="AF">
+	<name>Afghanistan</name>
+</state>
+<state id="AL">
+	<name>Albania</name>
+</state>
+<state id="AG">
+	<name>Algeria</name>
+</state>
+<state id="AN">
+	<name>Andorra</name>
+</state>
+<state id="AO">
+	<name>Angola</name>
+</state>
+<state id="AC">
+	<name>Antigua and Barbuda</name>
+</state>
+<state id="AR">
+	<name>Argentina</name>
+</state>
+<state id="AM">
+	<name>Armenia</name>
+</state>
+<state id="AA">
+	<name>Aruba</name>
+</state>
+<state id="AS">
+	<name>Australia</name>
+</state>
+<state id="AU">
+	<name>Austria</name>
+</state>
+<state id="AJ">
+	<name>Azerbaijan</name>
+</state>
+<state id="BF">
+	<name>The Bahamas</name>
+</state>
+<state id="BA">
+	<name>Bahrain</name>
+</state>
+<state id="FQ">
+	<name>Baker Island</name>
+</state>
+<state id="BG">
+	<name>Bangladesh</name>
+</state>
+<state id="BB">
+	<name>Barbados</name>
+</state>
+<state id="BO">
+	<name>Belarus</name>
+</state>
+<state id="BE">
+	<name>Belgium</name>
+</state>
+<state id="BH">
+	<name>Belize</name>
+</state>
+<state id="BN">
+	<name>Benin</name>
+</state>
+<state id="BD">
+	<name>Bermuda</name>
+</state>
+<state id="BT">
+	<name>Bhutan</name>
+</state>
+<state id="BL">
+	<name>Bolivia</name>
+	<data>-17.00000,-65.00000</data>
+</state>
+<state id="BK">
+	<name>Bosnia and Herzegovina</name>
+</state>
+<state id="BC">
+	<name>Botswana</name>
+</state>
+<state id="BV">
+	<name>Bouvet Island</name>
+</state>
+<state id="BR">
+	<name>Brazil</name>
+</state>
+<state id="IO">
+	<name>British Indian Ocean Territory</name>
+</state>
+<state id="VI">
+	<name>British Virgin Islands</name>
+</state>
+<state id="BX">
+	<name>Brunei</name>
+</state>
+<state id="BU">
+	<name>Bulgaria</name>
+</state>
+<state id="UV">
+	<name>Burkina Faso</name>
+</state>
+<state id="BY">
+	<name>Burundi</name>
+</state>
+<state id="CB">
+	<name>Cambodia</name>
+</state>
+<state id="CM">
+	<name>Cameroon</name>
+</state>
+<state id="CA">
+	<name>Canada</name>
+</state>
+<state id="CV">
+	<name>Cape Verde</name>
+</state>
+<state id="CJ">
+	<name>Cayman Islands</name>
+</state>
+<state id="CT">
+	<name>Central African Republic</name>
+</state>
+<state id="CD">
+	<name>Chad</name>
+</state>
+<state id="CI">
+	<name>Chile</name>
+</state>
+<state id="CH">
+	<name>China</name>
+</state>
+<state id="CO">
+	<name>Colombia</name>
+</state>
+<state id="CN">
+	<name>Comoros</name>
+</state>
+<state id="CF">
+	<name>Congo</name>
+</state>
+<state id="CW">
+	<name>Cook Islands</name>
+</state>
+<state id="CS">
+	<name>Costa Rica</name>
+</state>
+<state id="IV">
+	<name>Cote dIvoire</name>
+</state>
+<state id="HR">
+	<name>Croatia</name>
+</state>
+<state id="CU">
+	<name>Cuba</name>
+</state>
+<state id="CY">
+	<name>Cyprus</name>
+	<data>35.00000,33.00000</data>
+</state>
+<state id="EZ">
+	<name>Czech Republic</name>
+</state>
+<state id="CG">
+	<name>Democratic Republic of the Congo</name>
+</state>
+<state id="DA">
+	<name>Denmark</name>
+</state>
+<state id="DJ">
+	<name>Djibouti</name>
+</state>
+<state id="DO">
+	<name>Dominica</name>
+</state>
+<state id="DR">
+	<name>Dominican Republic</name>
+</state>
+<state id="EC">
+	<name>Ecuador</name>
+</state>
+<state id="EG">
+	<name>Egypt</name>
+</state>
+<state id="ES">
+	<name>El Salvador</name>
+</state>
+<state id="EK">
+	<name>Equatorial Guinea</name>
+</state>
+<state id="ER">
+	<name>Eritrea</name>
+</state>
+<state id="EN">
+	<name>Estonia</name>
+</state>
+<state id="ET">
+	<name>Ethiopia</name>
+</state>
+<state id="FK">
+	<name>Falkland Islands (Islas Malvinas)</name>
+</state>
+<state id="FO">
+	<name>Faroe Islands</name>
+</state>
+<state id="FM">
+	<name>Federated States of Micronesia</name>
+</state>
+<state id="FJ">
+	<name>Fiji</name>
+</state>
+<state id="FI">
+	<name>Finland</name>
+</state>
+<state id="FR">
+	<name>France</name>
+</state>
+<state id="FG">
+	<name>French Guiana</name>
+</state>
+<state id="FP">
+	<name>French Polynesia</name>
+</state>
+<state id="GB">
+	<name>Gabon</name>
+</state>
+<state id="GA">
+	<name>The Gambia</name>
+</state>
+<state id="GG">
+	<name>Georgia</name>
+</state>
+<state id="GM">
+	<name>Germany</name>
+</state>
+<state id="GH">
+	<name>Ghana</name>
+</state>
+<state id="GI">
+	<name>Gibraltar</name>
+</state>
+<state id="GO">
+	<name>Glorioso Islands</name>
+</state>
+<state id="GR">
+	<name>Greece</name>
+</state>
+<state id="GL">
+	<name>Greenland</name>
+</state>
+<state id="GJ">
+	<name>Grenada</name>
+</state>
+<state id="GP">
+	<name>Guadeloupe</name>
+</state>
+<state id="GQ">
+	<name>Guam</name>
+</state>
+<state id="GT">
+	<name>Guatemala</name>
+</state>
+<state id="GK">
+	<name>Guernsey</name>
+</state>
+<state id="PU">
+	<name>Guinea-Bissau</name>
+</state>
+<state id="GV">
+	<name>Guinea</name>
+</state>
+<state id="GY">
+	<name>Guyana</name>
+</state>
+<state id="HA">
+	<name>Haiti</name>
+</state>
+<state id="HM">
+	<name>Heard Island & McDonald Islands</name>
+</state>
+<state id="HO">
+	<name>Honduras</name>
+</state>
+<state id="HU">
+	<name>Hungary</name>
+</state>
+<state id="IC">
+	<name>Iceland</name>
+</state>
+<state id="IN">
+	<name>India</name>
+</state>
+<state id="IO">
+	<name>British Indian Ocean Territory</name>
+</state>
+<state id="ID">
+	<name>Indonesia</name>
+</state>
+<state id="IR">
+	<name>Iran</name>
+</state>
+<state id="IZ">
+	<name>Iraq</name>
+</state>
+<state id="EI">
+	<name>Ireland</name>
+</state>
+<state id="IM">
+	<name>Isle of Man</name>
+</state>
+<state id="IS">
+	<name>Israel</name>
+</state>
+<state id="GZ">
+	<name>Palestinian Authority</name>
+</state>
+<state id="IT">
+	<name>Italy</name>
+</state>
+<state id="JM">
+	<name>Jamaica</name>
+</state>
+<state id="JN">
+	<name>Jan Mayen</name>
+</state>
+<state id="JA">
+	<name>Japan</name>
+</state>
+<state id="DQ">
+	<name>Jarvis Island</name>
+</state>
+<state id="JE">
+	<name>Jersey</name>
+</state>
+<state id="JQ">
+	<name>Johnston Atoll</name>
+</state>
+<state id="JO">
+	<name>Jordan</name>
+</state>
+<state id="JU">
+	<name>Juan De Nova Island</name>
+</state>
+<state id="KZ">
+	<name>Kazakhstan</name>
+</state>
+<state id="KE">
+	<name>Kenya</name>
+</state>
+<state id="KR">
+	<name>Kiribati</name>
+</state>
+<state id="KS">
+	<name>South Korea</name>
+</state>
+<state id="KU">
+	<name>Kuwait</name>
+</state>
+<state id="KG">
+	<name>Kyrgyzstan</name>
+</state>
+<state id="LA">
+	<name>Laos</name>
+</state>
+<state id="LG">
+	<name>Latvia</name>
+</state>
+<state id="LE">
+	<name>Lebanon</name>
+</state>
+<state id="LT">
+	<name>Lesotho</name>
+</state>
+<state id="LI">
+	<name>Liberia</name>
+</state>
+<state id="LY">
+	<name>Libya</name>
+</state>
+<state id="LS">
+	<name>Liechtenstein</name>
+</state>
+<state id="LH">
+	<name>Lithuania</name>
+</state>
+<state id="LU">
+	<name>Luxembourg</name>
+</state>
+<state id="MC">
+	<name>Macau</name>
+</state>
+<state id="MK">
+	<name>Macedonia</name>
+</state>
+<state id="MA">
+	<name>Madagascar</name>
+</state>
+<state id="MI">
+	<name>Malawi</name>
+</state>
+<state id="MY">
+	<name>Malaysia</name>
+</state>
+<state id="MV">
+	<name>Maldives</name>
+</state>
+<state id="ML">
+	<name>Mali</name>
+</state>
+<state id="MT">
+	<name>Malta</name>
+</state>
+<state id="RM">
+	<name>Marshall Islands</name>
+</state>
+<state id="MB">
+	<name>Martinique</name>
+</state>
+<state id="MR">
+	<name>Mauritania</name>
+</state>
+<state id="MP">
+	<name>Mauritius</name>
+</state>
+<state id="MF">
+	<name>Mayotte</name>
+</state>
+<state id="MX">
+	<name>Mexico</name>
+</state>
+<state id="MQ">
+	<name>Midway Islands</name>
+</state>
+<state id="MD">
+	<name>Moldova</name>
+</state>
+<state id="MN">
+	<name>Monaco</name>
+</state>
+<state id="MG">
+	<name>Mongolia</name>
+</state>
+<state id="MH">
+	<name>Montserrat</name>
+</state>
+<state id="MO">
+	<name>Morocco</name>
+</state>
+<state id="MZ">
+	<name>Mozambique</name>
+</state>
+<state id="BM">
+	<name>Myanmar (Burma)</name>
+</state>
+<state id="WA">
+	<name>Namibia</name>
+</state>
+<state id="NR">
+	<name>Nauru</name>
+</state>
+<state id="NP">
+	<name>Nepal</name>
+</state>
+<state id="NT">
+	<name>Netherlands Antilles</name>
+</state>
+<state id="NL">
+	<name>Netherlands</name>
+</state>
+<state id="NC">
+	<name>New Caledonia</name>
+</state>
+<state id="NZ">
+	<name>New Zealand</name>
+</state>
+<state id="NU">
+	<name>Nicaragua</name>
+</state>
+<state id="NG">
+	<name>Niger</name>
+</state>
+<state id="NI">
+	<name>Nigeria</name>
+</state>
+<state id="NE">
+	<name>Niue</name>
+</state>
+<state id="NF">
+	<name>Norfolk Island</name>
+</state>
+<state id="KN">
+	<name>North Korea</name>
+</state>
+<state id="CQ">
+	<name>Northern Mariana Islands</name>
+</state>
+<state id="NO">
+	<name>Norway</name>
+</state>
+<state id="MU">
+	<name>Oman</name>
+</state>
+<state id="PS">
+	<name>Pacific Islands (Palau)</name>
+</state>
+<state id="PK">
+	<name>Pakistan</name>
+</state>
+<state id="PM">
+	<name>Panama</name>
+</state>
+<state id="PP">
+	<name>Papua New Guinea</name>
+</state>
+<state id="PA">
+	<name>Paraguay</name>
+</state>
+<state id="PE">
+	<name>Peru</name>
+	<data>-10.00000,-76.00000</data>
+</state>
+<state id="RP">
+	<name>Philippines</name>
+</state>
+<state id="PL">
+	<name>Poland</name>
+</state>
+<state id="PO">
+	<name>Portugal</name>
+</state>
+<state id="RQ">
+	<name>Puerto Rico</name>
+</state>
+<state id="QA">
+	<name>Qatar</name>
+</state>
+<state id="RE">
+	<name>Reunion</name>
+</state>
+<state id="RO">
+	<name>Romania</name>
+</state>
+<state id="RS">
+	<name>Russia</name>
+</state>
+<state id="RW">
+	<name>Rwanda</name>
+</state>
+<state id="SM">
+	<name>San Marino</name>
+</state>
+<state id="TP">
+	<name>Sao Tome and Principe</name>
+</state>
+<state id="SA">
+	<name>Saudi Arabia</name>
+</state>
+<state id="SG">
+	<name>Senegal</name>
+</state>
+<state id="SR">
+	<name>Serbia</name>
+</state>
+<state id="MW">
+	<name>Montenegro</name>
+</state>
+<state id="SE">
+	<name>Seychelles</name>
+</state>
+<state id="SL">
+	<name>Sierra Leone</name>
+</state>
+<state id="SN">
+	<name>Singapore</name>
+</state>
+<state id="LO">
+	<name>Slovakia</name>
+</state>
+<state id="SI">
+	<name>Slovenia</name>
+</state>
+<state id="BP">
+	<name>Solomon Islands</name>
+</state>
+<state id="SO">
+	<name>Somalia</name>
+</state>
+<state id="SF">
+	<name>South Africa</name>
+</state>
+<state id="SX">
+	<name>South Georgia and the South Sandwich Is</name>
+</state>
+<state id="SP">
+	<name>Spain</name>
+</state>
+<state id="PG">
+	<name>Spratly Islands</name>
+</state>
+<state id="CE">
+	<name>Sri Lanka</name>
+</state>
+<state id="SC">
+	<name>St. Kitts and Nevis</name>
+</state>
+<state id="ST">
+	<name>St. Lucia</name>
+</state>
+<state id="SB">
+	<name>St. Pierre and Miquelon</name>
+</state>
+<state id="VC">
+	<name>St. Vincent and the Grenadines</name>
+</state>
+<state id="SU">
+	<name>Sudan</name>
+</state>
+<state id="NS">
+	<name>Suriname</name>
+</state>
+<state id="SV">
+	<name>Svalbard</name>
+</state>
+<state id="WZ">
+	<name>Swaziland</name>
+</state>
+<state id="SW">
+	<name>Sweden</name>
+</state>
+<state id="SZ">
+	<name>Switzerland</name>
+</state>
+<state id="SY">
+	<name>Syria</name>
+</state>
+<state id="TE">
+	<name>East Timor</name>
+</state>
+<state id="TW">
+	<name>Taiwan</name>
+</state>
+<state id="TI">
+	<name>Tajikistan</name>
+</state>
+<state id="TZ">
+	<name>United Republic of Tanzania</name>
+</state>
+<state id="TH">
+	<name>Thailand</name>
+</state>
+<state id="TO">
+	<name>Togo</name>
+</state>
+<state id="TL">
+	<name>Tokelau</name>
+</state>
+<state id="TN">
+	<name>Tonga</name>
+</state>
+<state id="TD">
+	<name>Trinidad and Tobago</name>
+</state>
+<state id="TS">
+	<name>Tunisia</name>
+</state>
+<state id="TU">
+	<name>Turkey</name>
+</state>
+<state id="TX">
+	<name>Turkmenistan</name>
+</state>
+<state id="TK">
+	<name>Turks and Caicos Islands</name>
+</state>
+<state id="TV">
+	<name>Tuvalu</name>
+</state>
+<state id="UG">
+	<name>Uganda</name>
+</state>
+<state id="UP">
+	<name>Ukraine</name>
+</state>
+<state id="TC">
+	<name>United Arab Emirates</name>
+</state>
+<state id="UK">
+	<name>United Kingdom</name>
+</state>
+<state id="US">
+	<name>United States</name>
+</state>
+<state id="UY">
+	<name>Uruguay</name>
+</state>
+<state id="UZ">
+	<name>Uzbekistan</name>
+</state>
+<state id="NH">
+	<name>Vanuatu</name>
+</state>
+<state id="VE">
+	<name>Venezuela</name>
+</state>
+<state id="VM">
+	<name>Vietnam</name>
+</state>
+<state id="WQ">
+	<name>Wake Island</name>
+</state>
+<state id="WI">
+	<name>Western Sahara</name>
+</state>
+<state id="WS">
+	<name>Western Samoa</name>
+</state>
+<state id="YM">
+	<name>Yemen</name>
+</state>
+<state id="ZA">
+	<name>Zambia</name>
+</state>
+<state id="ZI">
+	<name>Zimbabwe</name>
+</state>';
+
+		}
+		$out .=  '</<countrydata>';
+
+		AMP_cache_set( $cache_key, $out );
+		return $out;
+	}
+	
+	function google_xml() {
+		if (!$this->points) {
+			$this->build_points();
+		}
+
+		header('Content-type: text/xml');
+		$out .= '<?xml version="1.0" ?>';
+		$out .= "<markers>";
+		if ($this->points) {
+			
+			foreach($this->points as $p) {
+				$out .= "\n<marker lat=\"".$p['lat']."\" lng=\"".$p['long']."\"";
+				$out .= " html=\"";	
+				$out .= '&lt;a href=&quot;'.htmlentities( $this->P['point_url']). $p['id'].'&quot;&gt;';	
+				$out .= htmlentities( $p['name']).'&lt;/a&gt;';
+				$out .= "&lt;br&gt;".$p['City'].", ".$p['State'];
+				$out .= '" />';
+			}
+		}
+		$out .= "</markers>";
+		return $out;
+	}
+	 
+
+
 	
 	
 	function us_xml() {	
+	
+		
+		if ($this->P['type']]== 'world'){
+			return $this->world_xml();
+			end
+		}
+		
 		header('Content-type: text/xml');
 
 		$cache_key = __FILE__.'-'.__FUNCTION__.'-map_ID='.$this->map_ID;
