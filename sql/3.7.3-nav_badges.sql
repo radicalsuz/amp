6523e22b8dc9f49ef1e3033be9f9208caa690eae
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2007-11-30 02:38:21 +0000

    arguments for nav includes
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@2829 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- sql/3.7.3-nav_badges.sql
+++ sql/3.7.3-nav_badges.sql
@@ -1,1 +1,4 @@
 alter table navtbl add column badge_id int( 9 ) null;
+alter table navtbl add column include_function_args text null;
+alter table navtbl change column include_file include_file varchar(100);
+alter table navtbl change column include_function include_function varchar(100);
