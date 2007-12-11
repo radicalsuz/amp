8cd1d4a276f0e916b2ec14973d84123832b9e737
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2007-12-11 07:20:51 +0000

    width : not clear enough
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@2846 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/AMP/Content/Image/Public/Detail.php
+++ include/AMP/Content/Image/Public/Detail.php
@@ -55,7 +55,7 @@ STYLESHEET;
         $container_css = $float_type ? AMP_CONTENT_CSS_CLASS_ARTICLE_IMAGE . ' ' . AMP_CONTENT_CSS_CLASS_ARTICLE_IMAGE . '-' . $float_type : AMP_CONTENT_CSS_CLASS_ARTICLE_IMAGE;
         $container_attr = array( 'class' => $container_css );
         if ( $width = $source->width) $container_attr['style'] = "width: {$width}px";
-        if ( isset( $attributes['width'])) $container_attr['style'] = "width: {$attributes['width']}px";
+        if ( isset( $attributes['container_width'])) $container_attr['style'] = "width: {$attributes['container_width']}px";
         return $container_attr;
     }
 
