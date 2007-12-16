8b612ccf061b27b31fd6de5f634196995a68d28f
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2007-12-16 23:29:33 +0000

    db updates
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@2856 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ sql/3.7.3-password_hashing.sql
@@ -0,0 +1,1 @@
+alter table users add column hash char( 128 ) not null;
--- sql/one_time_update_3.7.3-new_navs.sql
+++ sql/one_time_update_3.7.3-new_navs.sql
@@ -1,5 +1,5 @@
 replace into badges ( id, publish, name, include, include_function ) values ( 10, 1, "Rating Update", "AMP/Badge/Rating.php", "amp_badge_rating" );
-replace into navtbl( modid, name, titletext, include, include_function ) values ( 19, "Rate Articles", "Rate this Article", "AMP/Badge/Rating.php", "amp_badge_rating_block" );
-replace into navtbl( modid, name, titletext, include, include_function, include_function_args ) values ( 19, "Articles By Author", "Articles By This Author", "AMP/Badge/ArticlesByAuthor.php", "amp_badge_articles_by_author", "limit=5&&morelink=list.php" );
-replace into navtbl( id, modid, name, titletext, include, include_function, include_function_args ) values ( 140, "Tag Cloud", "Popular Tags", "AMP/Badge/Tagcloud.php", "amp_badge_tag_cloud", "section=" );
-replace into navtbl( modid, name, titletext, include, include_function, include_function_args ) values ( 19, "Blog Archives", "Archives", "AMP/Badge/ArticlesArchive.php", "amp_badge_articles_archive", "class=20" );
+replace into navtbl( modid, name, titletext, include_file, include_function ) values ( 19, "Rate Articles", "Rate this Article", "AMP/Badge/Rating.php", "amp_badge_rating_block" );
+replace into navtbl( modid, name, titletext, include_file, include_function, include_function_args ) values ( 19, "Articles By Author", "Articles By This Author", "AMP/Badge/ArticlesByAuthor.php", "amp_badge_articles_by_author", "limit=5&&morelink=list.php" );
+replace into navtbl( id, modid, name, titletext, include_file, include_function, include_function_args ) values ( 140, 19, "Tag Cloud", "Popular Tags", "AMP/Badge/Tagcloud.php", "amp_badge_tag_cloud", "section=" );
+replace into navtbl( modid, name, titletext, include_file, include_function, include_function_args ) values ( 19, "Blog Archives", "Archives", "AMP/Badge/ArticlesArchive.php", "amp_badge_articles_archive", "class=20" );
