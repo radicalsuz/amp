d835362590f1f14256d4046d9c8e5a32fa7682b6
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2008-01-23 22:01:57 +0000

    remove old debug info
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@2920 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- system/module_header_list.php
+++ system/module_header_list.php
@@ -13,17 +13,14 @@ require_once( 'AMP/System/Component/Controller.php');
 require_once( 'AMP/System/IntroText/List.inc.php');
 
 $controller = &new AMP_System_Component_Controller( );
-trigger_error( 'MHL: got controller');
 $controller->set_banner( AMP_TEXT_LIST , 'Public Page');
 $display = &$controller->get_display( );
-trigger_error( 'MHL: got display');
 $display->add_nav( 'content' );
 
 $list = &new AMPSystem_Introtext_List( $dbcon );
 $list->setTool( $tool_id );
 $display->add( $list );
 print $display->execute( );
-trigger_error( 'MHL: display ran');
 
     
 ?>
