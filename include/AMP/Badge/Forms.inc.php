6ca585b2af839189203f8c0b117953357405e417
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2008-01-24 01:22:36 +0000

    create subheader method
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@2925 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- include/AMP/Badge/Forms.inc.php
+++ include/AMP/Badge/Forms.inc.php
@@ -1,4 +1,6 @@
 <?php
+require_once( 'AMP/User/Data/Data.php');
+require_once( 'AMP/User/Data/List.php');
 
 function amp_badge_forms( $options = array( )) {
     $modin = ( isset( $options['modin']) && $options['modin']) ? $options['modin'] : false;
@@ -13,15 +15,17 @@ function amp_badge_forms( $options = array( )) {
     $limit = ( isset( $options['limit']) && $options['limit']) ? $options['limit'] : AMP_CONTENT_LIST_DISPLAY_MAX;
     $display = ( isset( $options['display']) && $options['display']) ? $options['display'] : false;
     $display_header = ( isset( $options['display_header']) && $options['display_header']) ? $options['display_header'] : false;
+    $display_subheader = ( isset( $options['display_subheader']) && $options['display_subheader'] ) ? $options['display_subheader'] : false;
 
     $finder = new AMP_User_Data( AMP_Registry::getDbcon( ));
     $search = &$finder->getSearchSource( $criteria );
     $search->addSort( $sort );
-    $search->addLimit( $limit );
+    $search->setLimit( $limit );
     $source = $finder->find( );
     $list = new AMP_User_Data_List( $source );
     if ( $display ) $list->set_display_method( $display );
     if ( $display_header ) $list->set_display_header_method( $display_header );
+    if ( $display_subheader ) $list->set_display_subheader_method( $display_subheader );
     return $list->execute( );
 }
 ?>
