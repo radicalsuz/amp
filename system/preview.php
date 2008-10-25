5cc25f33704964e314b09f2076556a177fd0dcc3
austin <austin@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2008-10-25 02:27:05 +0000

    change the path to dispatch.php in the system side preview
    git-svn-id: https://svn.radicaldesigns.org/amp/trunk@3030 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- system/preview.php
+++ system/preview.php
@@ -2,5 +2,5 @@
 
 require_once( 'AMP/Base/Config.php');
 define( 'AMP_DISPLAYMODE_PREVIEW', true );
-require_once( AMP_BASE_PATH . 'dispatch.php' );
+require_once( AMP_BASE_PATH . '/dispatch.php' );
 ?>
