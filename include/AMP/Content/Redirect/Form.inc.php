ecfd46100f1b738e6806eb5c10d2e508d1fa0e19
Austin Putman <austin@radicaldesigns.org>
2009-08-14 16:13:05 -0700

    new redirects check pretty url table for conflicts

--- include/AMP/Content/Redirect/Form.inc.php
+++ include/AMP/Content/Redirect/Form.inc.php
@@ -15,5 +15,42 @@ class AMP_Content_Redirect_Form extends AMPSystem_Form_XML {
     function setDynamicValues( ){
         /*auto scaffolded items here  auto scaffold items end */
     }
+
+    function _initJavascriptActions( ) {
+        $header = &AMP_getHeader( );
+        $this->_initPrettyUrlCreation( $header );
+    }
+
+    function _initPrettyUrlCreation( &$header ){
+        if( !AMP_CONTENT_HUMANIZE_URLS ) return;
+        $conflict_checker = <<<SCRIPT
+               jq( 'form#redirect input[name=old]').change( check_route_ajax );  
+               jq( '#manual_route_check').click( check_route_ajax );  
+			   function check_route_ajax( ev ) {
+                    var system_domain = '%s';
+					var target = jq( 'form#redirect input[name=old]' );
+                    jq.getJSON('/system/route_slug_ajax.php?slug_name=' + jq( target ).val(), function( result ) {
+                        if ( result.conflicts !== undefined && result.conflicts.length == 0 ) {
+                            jq( '#route_slug_details' ).html( "URL: " + system_domain + result.clean_url );
+                        } else {
+                            jq('#route_slug_details').html( "Warning: ");
+                            jq.each( result.conflicts, function() {
+                                jq('#route_slug_details').append( "This pretty url is already in use on <a href='" + this.owner_edit_url + "'>" + this.owner_type + " #"+ this.owner_id + "</a>" );
+                            } );
+                            jq('#route_slug_details').append( "<br/>Suggested Available URL: " + system_domain + result.clean_url );
+                        }
+                    } );
+					return false;
+               }
+SCRIPT;
+        $page_load_wrapper = <<<SCRIPT
+            jq( function( ) {
+                %s
+            });
+SCRIPT;
+        $values = $this->getValues();
+        $conflict_check = sprintf( $conflict_checker, AMP_SITE_URL );
+        $header->addJavascriptDynamic( sprintf( $page_load_wrapper, $pretty_url_builder . $conflict_check ));
+    }
 }
 ?>
